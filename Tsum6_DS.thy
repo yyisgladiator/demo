5ffa38def5e1f9cac9a1079e2bb141842b4d7fc3
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2017-03-13 10:59:11 +0000

    Proof tsum6=tsum5 completed.

--- Tsum6_DS.thy
+++ Tsum6_DS.thy
@@ -12,19 +12,29 @@ begin
 definition tsum6 :: "nat tstream \<rightarrow> nat tstream" where
 "tsum6 \<equiv> tsscanl plus 0"
 
-lemma tsum6_h2tsum5_h: "Fin n < #(tsscanl_h op + 0\<cdot>s) \<longrightarrow> snth n (tsscanl_h op + 0\<cdot>s) = snth n (tsum5_h 0\<cdot>s)"
-apply (induction n arbitrary: s)
-apply (smt shd1 snth_shd surj_scons tsscanl_h_empty tsscanl_h_scons_tick tsscanl_h_shd tsum5_h_shd_2 tsum5_shd)
-proof -
-  fix  n :: "nat" and s :: "nat event stream"
-  assume a1: "\<And>s. Fin n < #(tsscanl_h op + 0\<cdot>s) \<longrightarrow> snth n (tsscanl_h op + 0\<cdot>s) = snth n (tsum5_h 0\<cdot>s)"
-  thus "Fin (Suc n) < #(tsscanl_h op + 0\<cdot>s) \<longrightarrow> snth (Suc n) (tsscanl_h op + 0\<cdot>s) = snth (Suc n) (tsum5_h 0\<cdot>s)"
-    apply (cases "shd s=\<surd>")
-    apply (metis (no_types, lifting) not_less slen_rt_ile_eq snth_rt tsscanl_h_unfold_srt_tick tsum5_h_srt_tick)
-    apply (simp add: snth_rt tsscanl_h_unfold_srt tsum5_h_srt)
-    sorry
-qed
+lemma tsum5_h_unfold_tick: "shd s=\<surd> \<and> s\<noteq>\<epsilon> \<Longrightarrow> tsum5_h q\<cdot>s = \<up>\<surd> \<bullet> tsum5_h q\<cdot>(srt\<cdot>s)"
+by (metis surj_scons tsum5_h_scons_tick)
 
+lemma tsum6_h2tsum5_h: "Fin n < #(tsscanl_h op + q\<cdot>s) \<longrightarrow> snth n (tsscanl_h op + q\<cdot>s) = snth n (tsum5_h q\<cdot>s)"
+apply (induction n arbitrary: q s, auto)
+proof -
+  fix  n :: "nat" and q :: "nat" and s :: "nat event stream" and k :: "lnat"
+  assume a1: "#s = lnsuc\<cdot>k"
+  hence h1: "s\<noteq>\<epsilon>"
+    using lnsuc_neq_0 strict_slen by auto
+  thus "shd (tsscanl_h op + q\<cdot>s) = shd (tsum5_h q\<cdot>s)"
+    apply (cases "shd s=\<surd>")
+    apply (simp add: tsscanl_h_unfold_shd_tick tsum5_h_unfold_tick)
+    by (simp add: tsscanl_h_unfold_shd)
+next
+  fix  n :: "nat" and q :: "nat" and s :: "nat event stream"
+  assume a2: "\<And>q s. Fin n < #s \<longrightarrow> snth n (tsscanl_h op + q\<cdot>s) = snth n (tsum5_h q\<cdot>s)"
+  assume a3: "Fin (Suc n) < #s"
+  thus "snth (Suc n) (tsscanl_h op + q\<cdot>s) = snth (Suc n) (tsum5_h q\<cdot>s)"
+  by (smt Suc_n_not_le_n a2 less2nat_lemma less_imp_not_eq2 lnle_Fin_0 not_less slen_empty_eq 
+      slen_rt_ile_eq snth_scons surj_scons trans_lnless tsscanl_h_scons tsscanl_h_scons_tick 
+      tsum5_suc_snth tsum5_suc_snth_tick)
+qed
 
 lemma tsum62tsum5: "tsscanl_h plus 0\<cdot>s = tsum5_h 0\<cdot>s"
 apply (rule snths_eq)
