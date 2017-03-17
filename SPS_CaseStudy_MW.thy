14b403e3b1c1d60f567f1539e1457e656e1f084a
Marc Wiartalla <marc.wiartalla@rwth-aachen.de>
2017-03-17 11:41:44 +0000

    changed definition of ariane

--- SPS_CaseStudy_MW.thy
+++ SPS_CaseStudy_MW.thy
@@ -3,6 +3,63 @@ imports Feedback_MW SPF_MW SPF_Templates
   
 begin
 
+lemma [simp]: "spfDom\<cdot>(addSPF (c1,c2,c3)) = {c1, c2}"
+  using addC_def by auto
+
+lemma [simp]: "spfRan\<cdot>(addSPF (c1,c2,c3)) = {c3}"
+  using addC_def by auto
+  
+lift_definition arianeComp1 :: "nat SPS" is
+  "{addSPF (c1,c2,c3)}"
+  by(simp add: sps_well_def)
+
+lemma [simp]: "spfDom\<cdot>(appendSPF (c3,c2) 0) = {c3}"
+  sorry
+
+lemma [simp]: "spfRan\<cdot>(appendSPF (c3,c2) 0) = {c2}"
+  sorry
+    
+lift_definition arianeComp2 :: "nat SPS" is
+  "{appendSPF (c3,c2) 0}"
+  by(simp add: sps_well_def)
+
+lift_definition arianeComp3 :: "nat SPS" is
+  "{addSPF (c3,c4,c5)}"
+  by(simp add: sps_well_def)
+    
+lemma [simp]: "\<forall>f. (\<exists>s. f = Abs_CSPF (\<lambda> sb. (sbDom\<cdot>sb = {c5}) \<leadsto> ([c4\<mapsto>s :: nat stream]\<Omega>)) \<and> #s = \<infinity>) \<longrightarrow> spfDom\<cdot>(f) = {c5}"
+  sorry
+
+lemma [simp]: "\<forall>f. (\<exists>s. f = Abs_CSPF (\<lambda> sb. (sbDom\<cdot>sb = {c5}) \<leadsto> ([c4\<mapsto>s :: nat stream]\<Omega>)) \<and> #s = \<infinity>) \<longrightarrow> spfRan\<cdot>(f) = {c4}"
+  sorry
+    
+lift_definition arianeComp4 :: "nat SPS" is
+  "{ Abs_CSPF (\<lambda> sb. (sbDom\<cdot>sb = {c5}) \<leadsto> ([c4\<mapsto>s]\<Omega>)) | s :: nat stream. #s = \<infinity>}"
+  by(simp add: sps_well_def)
+   
+lift_definition ariane :: "nat SPS" is "arianeComp1 \<Otimes> arianeComp2 \<Otimes> arianeComp3 \<Otimes> arianeComp4"
+  sorry  
+
+lemma final: assumes "f \<in> Rep_SPS ariane" and "sbDom\<cdot>sb = spsDom ariane"
+  shows "snth n ((f\<rightleftharpoons>sb).c4) \<ge> sum_snth n (sb . c1)"
+  sorry
+  
+  
+  
+lemma [simp]: "\<forall>f. (\<exists>s. f = Abs_CSPF (\<lambda>sb. (sbDom\<cdot>sb = {c1})\<leadsto>[c5 \<mapsto> add\<cdot>(sum3\<cdot>(sb . c1))\<cdot>s]\<Omega>) \<and> #s = \<infinity>) \<longrightarrow> spfDom\<cdot>f = {c1}"
+  sorry
+    
+lemma [simp]: "\<forall>f. (\<exists>s. f = Abs_CSPF (\<lambda>sb. (sbDom\<cdot>sb = {c1})\<leadsto>[c5 \<mapsto> add\<cdot>(sum3\<cdot>(sb . c1))\<cdot>s]\<Omega>) \<and> #s = \<infinity>) \<longrightarrow> spfRan\<cdot>f = {c5}"
+  sorry
+  
+lift_definition ariane2 :: "nat SPS" is
+  "{ Abs_CSPF (\<lambda> sb. (sbDom\<cdot>sb = {c1}) \<leadsto> ([c5\<mapsto>add\<cdot>(sum3\<cdot>(sb . c1))\<cdot>s]\<Omega>)) | s :: nat stream. #s = \<infinity>}"
+  by(simp add: sps_well_def)
+
+lemma arianeEq: "ariane \<equiv> ariane2"
+  sorry
+  
+(*
 definition sumAriane :: "nat SPF" where
 "sumAriane \<equiv> sum1SPF"
 
@@ -55,5 +112,5 @@ lemma arianeEq: "ariane \<equiv> ariane2"
   apply(simp add: ariane_def ariane2_def spsComp_def)
   apply(simp add: arianeHelper1_def arianeHelper2_def)
   by (smt Collect_cong Rep_SPS_inverse arianeHelper2.rep_eq mem_Collect_eq sumAriane_def)
-  
+  *)
 end
