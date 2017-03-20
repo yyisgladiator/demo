d9b802cc973e85686463cfd0a8a7741ffbdbbd62
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2017-03-20 16:48:49 +0000

    ttimes verified with ttimes_nth.

--- Tsum6_DS.thy
+++ Tsum6_DS.thy
@@ -49,14 +49,68 @@ by (simp add: tsum6_h2tsum5_h_snth)
 lemma tsum62tsum5: "tsum6=tsum5"
 by (simp add: tsscanl_def tsum5_def tsum6_def tsum6_h2tsum5_h)
 
-(* Definition of ttimes1 and ttimes_nth *)
-definition ttimes1 :: "nat tstream \<rightarrow> nat tstream" where
-"ttimes1 \<equiv> tsscanl times 1"
+(* Definition of ttimes and ttimes_nth *)
+definition ttimes :: "nat tstream \<rightarrow> nat tstream" where
+"ttimes \<equiv> tsscanl times 1"
 
 primrec ttimes_nth:: "nat \<Rightarrow> nat event stream \<Rightarrow> nat" where
-"ttimes_nth 0 s = (if shd s=\<surd> then 1 else \<M>\<inverse> shd s)" |
+"ttimes_nth 0 s = (if shd s=\<surd> then 1 else 1 * \<M>\<inverse> shd s)" |
 "ttimes_nth (Suc n) s = (if shd s=\<surd> then 1 * ttimes_nth n (srt\<cdot>s) 
                          else (\<M>\<inverse>(shd s)) * ttimes_nth n (srt\<cdot>s))"
+
+lemma tsscanl_h_times_switch_initial:
+  "Fin n<#(s :: nat event stream) \<and> snth n s\<noteq>\<surd> 
+    \<Longrightarrow> snth n (tsscanl_h times q\<cdot>s) = \<M> q * \<M>\<inverse> snth n (tsscanl_h times 1\<cdot>s)"
+proof (induction n arbitrary: q s, auto)
+  fix q :: "nat" and s :: "nat event stream" and k :: "lnat"
+  assume a1: "shd s \<noteq> \<surd>"
+  assume a2: "#s = lnsuc\<cdot>k"
+  hence h1: "s\<noteq>\<epsilon>"
+    using bot_is_0 lnat.con_rews strict_slen by auto
+  thus "shd (tsscanl_h op * q\<cdot>s) = \<M> q * \<M>\<inverse> shd (tsscanl_h op * 1\<cdot>s)"
+    by (simp add: a1 tsscanl_h_unfold_shd)
+next
+  fix n :: "nat" and q :: "nat" and s :: "nat event stream"
+  assume a3: "\<And>q s. Fin n < #(s :: nat event stream) \<and> snth n s \<noteq> \<surd> 
+                \<Longrightarrow> snth n (tsscanl_h op * q\<cdot>s) = \<M> q * \<M>\<inverse> snth n (tsscanl_h op * 1\<cdot>s)"
+  assume a4: "Fin (Suc n) < #s"
+  assume a5: "snth (Suc n) s \<noteq> \<surd>"
+  thus "snth (Suc n) (tsscanl_h op * q\<cdot>s) = \<M> q * \<M>\<inverse> snth (Suc n) (tsscanl_h op * 1\<cdot>s)"
+    by (smt Suc_neq_Zero a3 a4 event.simps(4) less_imp_not_eq2 lnle_Fin_0 mult.assoc 
+        mult.comm_neutral not_less slen_rt_ile_eq snth_scons strict_slen surj_scons trans_lnless
+        tsscanl_h_scons tsscanl_h_scons_tick)  
+qed
+
+lemma tsscanl_h2ttimes_nth_snth: 
+  "Fin n<#s \<and> snth n s\<noteq>\<surd> \<Longrightarrow> snth n (tsscanl_h times 1\<cdot>s) = \<M> ttimes_nth n s"
+proof (induction n arbitrary: s, auto)
+  fix s :: "nat event stream" and k :: "lnat"
+  assume a1: "shd s \<noteq> \<surd>"
+  assume a2: "#s = lnsuc\<cdot>k"
+  thus "shd (tsscanl_h op * 1\<cdot>s) = \<M> \<M>\<inverse> shd s"
+    by (simp add: a1 nat_mult_1 only_empty_has_length_0 semiring_normalization_rules(11)
+        tsscanl_h_unfold_shd)
+next  
+  fix n :: "nat" and s :: "nat event stream"
+  assume a3: "\<And>s. Fin n < #s \<and> snth n s \<noteq> \<surd> \<Longrightarrow> snth n (tsscanl_h op * 1\<cdot>s) = \<M> ttimes_nth n s"
+  assume a4: " Fin (Suc n) < #s"
+  assume a5: "snth (Suc n) s \<noteq> \<surd>"
+  thus "shd s = \<surd> \<Longrightarrow> snth (Suc n) (tsscanl_h op * 1\<cdot>s) = \<M> ttimes_nth n (srt\<cdot>s)"
+    by (metis a3 a4 not_less slen_rt_ile_eq snth_rt tsscanl_h_unfold_srt_tick)
+  thus "shd s \<noteq> \<surd> \<Longrightarrow> snth (Suc n) (tsscanl_h op * 1\<cdot>s) = \<M> \<M>\<inverse> shd s * ttimes_nth n (srt\<cdot>s)"
+    by (smt a3 a4 a5 event.simps(4) mult.left_neutral not_less slen_rt_ile_eq snth_rt 
+        tsscanl_h_times_switch_initial tsscanl_h_unfold_srt)
+qed
+
+lemma tsscanl_h2ttimes_nth: 
+  "Fin n<#s \<Longrightarrow> snth n (tsscanl_h times 1\<cdot>s) = (case (snth n s) of Msg a \<Rightarrow> \<M> ttimes_nth n s | \<surd> \<Rightarrow> \<surd>)"
+apply (cases "snth n s =\<surd>", simp add: tsscanl_h_snth_tick2tick)
+by (metis event.exhaust event.simps(4) tsscanl_h2ttimes_nth_snth)
+
+lemma ttimes2ttimes_nth: 
+  "Fin n<#(Rep_tstream ts) \<Longrightarrow> snth n (Rep_tstream (ttimes\<cdot>ts)) =
+      (case (snth n (Rep_tstream ts)) of Msg a \<Rightarrow> \<M> ttimes_nth n (Rep_tstream ts) | \<surd> \<Rightarrow> \<surd>)"
+by (simp add: ttimes_def tsscanl_unfold ts_well_tsscanl_h tsscanl_h2ttimes_nth)
 
 (* Definition of tsscanl_nth_nq (neutral element q) and unfold lemmata *)
 
