347bce515cec8d08a620a125c1dc57f193f8cce1
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2017-04-18 08:50:05 +0000

    Tried a few things.

--- TStreamCaseStudy_DS.thy
+++ TStreamCaseStudy_DS.thy
@@ -47,13 +47,19 @@ by (smt Rep_Abs Rep_tstream_inject list2s.simps(1) list2s.simps(2) list2s_inj ls
     not_Cons_self po_eq_conv sup'_def tick_msg ts_tsconc_prefix ts_well_conc1 tsconc_rep_eq1)
 
 (* Constructed function on tstreams is monotone and weak causal but not continous? *)
-definition f1 :: "nat tstream \<Rightarrow> nat tstream" where
-"f1 ts \<equiv> if #(Rep_tstream ts)<\<infinity> then ts \<bullet> Abs_tstream (\<up>\<surd>) else ts \<bullet> Abs_tstream (<[\<surd>, \<surd>]>)"
+definition f1 :: "'a tstream \<Rightarrow> 'a tstream" where
+"f1 ts \<equiv> if #(Rep_tstream ts)<\<infinity> then ts else ts \<bullet> Abs_tstream (\<up>\<surd>)"
+
+definition finite_tick_chain :: "nat \<Rightarrow> 'a tstream" where
+"finite_tick_chain = (\<lambda>i. (tsntimes 100 (Abs_tstream(\<up>\<surd>))) \<down> i)"
+
+lemma "f1 (finite_tick_chain i) = finite_tick_chain i"
+by (simp add: f1_def not_less tsconc_insert)
 
 lemma mono_f1: "monofun f1"
 apply (rule monofunI)
 apply (simp add: f1_def, auto)
-sorry
+by (metis inf_less_eq leI tsInfTicks tsconc_id)+
 
 lemma weak_f1: "tsWeakCausal f1"
 apply (rule tsMono2Weak2)
@@ -61,14 +67,17 @@ apply (simp add: mono_f1)
 apply (simp add: f1_def)
 using lnle_def monofun_cfun_arg ts_tsconc_prefix by blast
 
+lemma cont_f1_1: "cont f1"
+by (metis Fin_02bot f1_def leI lnzero_def notinfI3 strict_slen tsWeak2cont ts_well_Rep ts_well_def
+    tsconc_id tstickcount_insert weak_f1)
+
 lemma f1_inftick:
-  "f1 (tsinftimes (Abs_tstream (\<up>\<surd>))) = tsinftimes (Abs_tstream (\<up>\<surd>)) \<bullet> Abs_tstream (<[\<surd>, \<surd>]>)"
+  "f1 (tsinftimes (Abs_tstream (\<up>\<surd>))) = tsinftimes (Abs_tstream (\<up>\<surd>)) \<bullet> Abs_tstream (\<up>\<surd>)"
 by (simp add: f1_def slen_inftick)
 
 lemma f1_inftick_is_ub:
   "range (\<lambda>i. f1 tsinftimes (Abs_tstream (\<up>\<surd>)) \<down> i ) <| tsinftimes (Abs_tstream (\<up>\<surd>))"
-apply (simp add: is_ub_def f1_def slen_tstakenofinftick)
-sorry
+by (simp add: is_ub_def f1_def slen_tstakenofinftick)
 
 lemma cont_f1: "\<not>cont f1"
 apply (simp add: cont_def)
@@ -76,6 +85,33 @@ apply (rule_tac x="(\<lambda>i. (tsinftimes (Abs_tstream (\<up>\<surd>))) \<down
 apply (simp add: f1_inftick is_lub_def, auto)
 apply (rule_tac x="tsinftimes (Abs_tstream (\<up>\<surd>))" in exI)
 apply (simp add: f1_inftick_is_ub)
+sorry
+
+definition smedium :: "bool stream \<Rightarrow> 'a stream \<Rightarrow> 'a stream" where
+"smedium \<equiv> fix\<cdot>(\<Lambda> h. (\<lambda>bs ms. if (bs=\<epsilon> \<or> ms=\<epsilon>) then \<epsilon> else 
+                              (if shd bs=False then (h (srt\<cdot>bs) (srt\<cdot>ms)) else 
+                                  (\<up>(shd ms) \<bullet> (h (srt\<cdot>bs) (srt\<cdot>ms))))))"
+
+lemma cont_smedium: "\<And>bs ms. cont (\<lambda>h. if bs = \<epsilon> \<or> ms = \<epsilon> then \<epsilon> else 
+                                     if shd bs = False then h (srt\<cdot>bs) (srt\<cdot>ms) else
+                                        \<up>(shd ms) \<bullet> h (srt\<cdot>bs) (srt\<cdot>ms))"
+apply (rule contI)
+apply (rule_tac y=bs in scases', auto)
+apply (rule is_lub_const)+
+apply (subst lub_fun, assumption)
+apply (rule_tac y=ms in scases', auto)
+apply (metis ch2ch_fun lub_fun thelubE)
+apply (rule is_lub_const)
+by (smt ch2ch_fun lub_fun thelubE contlub_cfun_arg lub_eq monofun_cfun_arg chain_def)
+
+lemma "#(smedium bs ms) = #({True} \<ominus> bs)"
+apply (subst smedium_def [THEN fix_eq2])
+apply (simp add: cont_smedium, auto)
+sorry
+
+lemma "smedium bs ms \<sqsubseteq> ms"
+apply (subst smedium_def [THEN fix_eq2])
+apply (simp add: cont_smedium, auto)
 sorry
 
 end
