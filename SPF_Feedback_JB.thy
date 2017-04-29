7707e61c41585a275a738a2cf8758900de958077
JB <jens.buerger@rwth-aachen.de>
2017-04-29 15:58:25 +0200

    proved feedhelper is cont

--- SPF_Feedback_JB.thy
+++ SPF_Feedback_JB.thy
@@ -24,16 +24,51 @@ let I  = spfDom\<cdot>f - spfRan\<cdot>f;
 in Abs_CSPF (\<lambda> sb. (sbDom\<cdot>sb = I) \<leadsto>
     (\<Squnion>i. iterate i\<cdot>(\<Lambda> z. (f\<rightleftharpoons>((sb \<uplus> z)\<bar> I1)))\<cdot>(C^\<bottom>)))" 
   
-definition spfFeedHelp:: "'m SPF \<Rightarrow> 'm SB \<Rightarrow> 'm SB  \<rightarrow> 'm SB" where
-"spfFeedHelp f1 x \<equiv> (\<Lambda> z. (f1\<rightleftharpoons>((x \<uplus> z)\<bar> (spfDom\<cdot>f1))))"
+definition spfFeedH:: "'m SPF \<Rightarrow> 'm SB \<Rightarrow> 'm SB  \<rightarrow> 'm SB" where
+"spfFeedH f x \<equiv> (\<Lambda> z. (f\<rightleftharpoons>((x \<uplus> z)\<bar> (spfDom\<cdot>f))))"
 
 abbreviation iter_spfFeedHelp:: "'m SPF \<Rightarrow> nat \<Rightarrow> 'm SB \<Rightarrow> 'm SB" where
-"iter_spfFeedHelp f1 i \<equiv> (\<lambda> x. iterate i\<cdot>(spfFeedHelp f1 x)\<cdot>((spfRan\<cdot>f1)^\<bottom>))"
+"iter_spfFeedHelp f i \<equiv> (\<lambda> x. iterate i\<cdot>(spfFeedH f x)\<cdot>((spfRan\<cdot>f)^\<bottom>))"
 
     
 (* ----------------------------------------------------------------------- *)
 section \<open>spfFeedHelp\<close>
 (* ----------------------------------------------------------------------- *)
+  
+subsection \<open>cont\<close>
+
+
+lemma spfFeedH_cont[simp]: "cont (\<lambda> z. (f\<rightleftharpoons>((x \<uplus> z)\<bar> (spfDom\<cdot>f))))"
+proof -
+  have f1:"cont (\<lambda>z. (Rep_cfun (Rep_SPF f))\<rightharpoonup>((x \<uplus> z)\<bar>spfDom\<cdot>f))"
+   by (metis (no_types) cont_Rep_cfun2 cont_compose op_the_cont)
+  thus ?thesis
+    by(simp add: Rep_CSPF_def)
+qed
+  
+
+lemma spfFeedH_cont2[simp]: "cont (\<lambda> x. (f\<rightleftharpoons>((x \<uplus> z)\<bar> (spfDom\<cdot>f))))"
+proof -
+  have f1: "cont (\<lambda>x. (x \<uplus> z))" (* really important line *)
+    by simp
+  hence f2:"cont (\<lambda>x. (Rep_cfun (Rep_SPF f))\<rightharpoonup>((x \<uplus> z)\<bar>spfDom\<cdot>f))"
+   by (metis (no_types) cont_Rep_cfun2 cont_compose op_the_cont)
+  thus ?thesis
+    by(simp add: Rep_CSPF_def)
+qed
+  
+lemma spfFeedH_mono[simp]: "monofun (\<lambda> z. (f\<rightleftharpoons>((x \<uplus> z)\<bar> (spfDom\<cdot>f))))"
+  using cont2mono spfFeedH_cont by blast
+
+    
+lemma spfFeedH_continX[simp]: "cont (\<lambda> x. spfFeedH f x)"
+proof -
+  have "cont (\<lambda>x. \<Lambda> z. (f\<rightleftharpoons>((x \<uplus> z)\<bar> (spfDom\<cdot>f))))"
+    by(simp add: cont2cont_LAM)
+  thus ?thesis
+    by(simp add: spfFeedH_def)
+qed
+  
 
   
 end
