c7cc50a3114afb40e64aa891a0d0f068f5b0f16b
Sebastian Wolfram Stüber <sebastian.stueber@rwth-aachen.de>
2017-05-27 12:09:33 +0200

    changes tsZip definition and minor changes

--- TStream_DS.thy
+++ TStream_DS.thy
@@ -10,8 +10,11 @@ theory TStream_DS
 imports fixrec_tstream
 
 begin  
+  
+(* Do not modify here. It is already defined in fixrex_tstream.thy
+  I would copy it to "Prelude.thy" and show cont there *)
 
-definition upApply :: "('a \<Rightarrow> 'b) \<Rightarrow> 'a discr u \<rightarrow> 'b discr u" where
+(* definition upApply :: "('a \<Rightarrow> 'b) \<Rightarrow> 'a discr u \<rightarrow> 'b discr u" where
 "upApply f \<equiv> \<Lambda> a. (if a=\<bottom> then \<bottom> else updis (f (THE b. a = updis b)))"
 
 lemma upApply_mono [simp]: "monofun (\<lambda> a. (if a=\<bottom> then \<bottom> else updis (f (THE b. a = updis b))))"
@@ -29,16 +32,16 @@ definition upApply2 :: "('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow> 'a 
 "upApply2 f \<equiv> \<Lambda> a b. (if a=\<bottom>\<or>b=\<bottom> then \<bottom> else updis (f (THE x. a = updis x) (THE x. b = updis x)))"
  (* Is it possible to define upApply2 using upApply? *)
 (* ToDo: mono&cont *)
-
+*)
 
    (* only the general idea *)
 fixrec tsZip:: "'a tstream \<rightarrow> 'b stream \<rightarrow> ('a \<times> 'b) tstream" where
-"tsZip\<cdot>ts\<cdot>\<bottom> = \<bottom>" |
+"tsZip\<cdot>ts\<cdot>\<bottom> = \<bottom>" |  
 "x\<noteq>\<bottom> \<Longrightarrow> ts\<noteq>\<bottom> \<Longrightarrow>
   tsZip\<cdot>(tsLscons\<cdot>(up\<cdot>(uMsg\<cdot>t))\<cdot>ts)\<cdot>(lscons\<cdot>x\<cdot>xs)  = tsMLscons\<cdot>(upApply2 Pair\<cdot>(up\<cdot>t)\<cdot>x)\<cdot>(tsZip\<cdot>ts\<cdot>xs)" | 
   (* zip if both first elements are defined *)
-"x\<noteq>\<bottom> \<Longrightarrow> 
-  tsZip\<cdot>(tsLscons\<cdot>(up\<cdot>DiscrTick)\<cdot>ts)\<cdot>(lscons\<cdot>x\<cdot>xs) = delayFun\<cdot>(tsZip\<cdot>ts\<cdot>xs)"  
+"xs\<noteq>\<bottom> \<Longrightarrow> 
+  tsZip\<cdot>(tsLscons\<cdot>(up\<cdot>DiscrTick)\<cdot>ts)\<cdot>xs = delayFun\<cdot>(tsZip\<cdot>ts\<cdot>xs)"  
   (* ignore ticks *)
 (* No other cases required, stuff that does not match will go to bottom *)
 
@@ -52,16 +55,32 @@ lemma tszip_scons_fixrec: "x\<noteq>\<bottom> \<Longrightarrow> ts\<noteq>\<bott
   tsZip\<cdot>(tsLscons\<cdot>(up\<cdot>(uMsg\<cdot>t))\<cdot>ts)\<cdot>(lscons\<cdot>x\<cdot>xs)  = tsMLscons\<cdot>(upApply2 Pair\<cdot>(up\<cdot>t)\<cdot>x)\<cdot>(tsZip\<cdot>ts\<cdot>xs)"
 by (fixrec_simp)
 
-lemma tszip_scons_tick_fixrec: "x\<noteq>\<bottom> \<Longrightarrow> 
-  tsZip\<cdot>(tsLscons\<cdot>(up\<cdot>DiscrTick)\<cdot>ts)\<cdot>(lscons\<cdot>x\<cdot>xs) = delayFun\<cdot>(tsZip\<cdot>ts\<cdot>xs)"
+lemma tszip_scons_tick_fixrec: "xs\<noteq>\<bottom> \<Longrightarrow> 
+  tsZip\<cdot>(tsLscons\<cdot>(up\<cdot>DiscrTick)\<cdot>ts)\<cdot>xs = delayFun\<cdot>(tsZip\<cdot>ts\<cdot>xs)"
 by (fixrec_simp)
+
+lemma delayfun_simp: "(Abs_tstream (\<up>\<surd>) \<bullet> ts ) = delayFun\<cdot>ts"  
+  apply(simp add: delayFun_def)
+  done
+
+lemma delayfun_lscons: "delayFun\<cdot>ts= tsLscons\<cdot>(up\<cdot>DiscrTick)\<cdot>ts"
+  by(auto simp add: delayFun_def tslscons_insert tsconc_insert DiscrTick_def espf2tspf_def lscons_conv)  
     
 lemma tszip_scons_tick: "xs\<noteq>\<epsilon> \<Longrightarrow> tsZip\<cdot>(Abs_tstream (\<up>\<surd>)\<bullet>ts)\<cdot>xs = Abs_tstream(\<up>\<surd>) \<bullet> tsZip\<cdot>ts\<cdot>xs"
-oops
+  apply(simp add: delayfun_simp)
+    apply(subst delayfun_lscons)
+  by (fixrec_simp)
 
+
+    (* SWS: Um gottes willen! *)
 lemma tszip_scons: "t\<noteq>\<surd> \<Longrightarrow> ts_well (\<up>(\<M> (\<M>\<inverse> t,x)) \<bullet> Rep_tstream (tsZip\<cdot>ts\<cdot>xs)) 
   \<Longrightarrow> tsZip\<cdot>(Abs_tstream (\<up>t)\<bullet>ts)\<cdot>(\<up>x\<bullet>xs) = Abs_tstream(\<up>(\<M> (\<M>\<inverse> t,x)) \<bullet> Rep_tstream (tsZip\<cdot>ts\<cdot>xs))"
 oops
+
+
+  
+
+  (* SWS: Die funktion brauchen wir jetzt nicht unbedingt, fixrec klappt ja *)
 
 
 (* Wenn das 1. Element ein tick ist, dann bilde auf tick ab (und wende funktion auf rest an). Ansonsten wende funktion an *)
