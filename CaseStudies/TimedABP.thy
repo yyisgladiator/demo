c6ced487cd1fbfbee66d8548c79c98b89e0fe15b
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2017-06-05 19:11:17 +0200

    Added descriptions for lemmata. Some little modifications.

--- CaseStudies/TimedABP.thy
+++ CaseStudies/TimedABP.thy
@@ -20,51 +20,55 @@ section {* Medium *}
 definition tsMed :: "'a tstream \<rightarrow> bool stream \<rightarrow> 'a tstream" where
 "tsMed \<equiv> \<Lambda> msg ora. tsProjFst\<cdot>(tsFilter {x. snd x}\<cdot>(tsZip\<cdot>msg\<cdot>ora))"
 
-(* Assumption for lemmata: #({True} \<ominus> ora)=\<infinity> *)
-
-(* ToDo: Description for lemmata *)
+(* Assumption for medium  lemmata: #({True} \<ominus> ora)=\<infinity> *)
 
 lemma tsmed_insert: "tsMed\<cdot>msg\<cdot>ora = tsProjFst\<cdot>(tsFilter {x. snd x}\<cdot>(tsZip\<cdot>msg\<cdot>ora))"
 by (simp add: tsMed_def)
 
-lemma tsmed_delayfun: "ora \<noteq> \<bottom>\<Longrightarrow> tsMed\<cdot>(delayFun\<cdot>ts)\<cdot>ora = delayFun\<cdot>(tsMed\<cdot>ts\<cdot>ora)"
-  oops
+(* ToDo: Properties lemmata for medium *)
+
+(* Assumtions are missing. Add msg\<noteq>\<bottom> and ora\<noteq>\<bottom> as needed. *)
+lemma tsmed_mlscons_true: "tsMed\<cdot>(tsMLscons\<cdot>m\<cdot>msg)\<cdot>((updis True) && ora) = tsMLscons\<cdot>m\<cdot>(tsMed\<cdot>msg\<cdot>ora)"
+oops
     
-    (* assumtions are missing. add them as needed. ts\<noteq>\<bottom> and ora\<noteq>\<bottom>*)
-lemma tsmed_mlscons_true: "tsMed\<cdot>(tsMLscons\<cdot>t\<cdot>ts)\<cdot>((updis True) && ora) = tsMLscons\<cdot>t\<cdot>(tsMed\<cdot>ts\<cdot>ora)"
-  oops
-    
-lemma tsmed_mlscons_false: "tsMed\<cdot>(tsMLscons\<cdot>t\<cdot>ts)\<cdot>((updis False) && ora) = tsMed\<cdot>ts\<cdot>ora"
-  oops
-    
+lemma tsmed_mlscons_false: "tsMed\<cdot>(tsMLscons\<cdot>m\<cdot>msg)\<cdot>((updis False) && ora) = tsMed\<cdot>msg\<cdot>ora"
+oops
+
+lemma tsmed_delayfun: "ora \<noteq>\<bottom> \<Longrightarrow> tsMed\<cdot>(delayFun\<cdot>msg)\<cdot>ora = delayFun\<cdot>(tsMed\<cdot>msg\<cdot>ora)"
+oops
+
+text {* Not every message will be transmitted. *}    
 lemma tsmed_slen_leq:
   shows "#(Rep_tstream (tsMed\<cdot>msg\<cdot>ora)) \<le> #(Rep_tstream msg)"
 oops
 
 text {* If infinity messages will be sent infinity messages will be transmitted. *}
-lemma tsmed_slen[simp]: assumes "#({True} \<ominus> ora)=\<infinity>" and "#(tsAbs\<cdot>msg)=\<infinity>" 
+lemma tsmed_slen [simp]: assumes "#({True} \<ominus> ora)=\<infinity>" and "#(tsAbs\<cdot>msg)=\<infinity>" 
   shows "#(tsAbs\<cdot>(tsMed\<cdot>msg\<cdot>ora)) = \<infinity>"
 oops
 
-lemma tsmed_inftick [simp]: "#s=\<infinity> \<Longrightarrow> tsMed\<cdot>tsInfTick\<cdot>s = tsInfTick"
+text {* If infinity ticks will be sent infinity ticks will be transmitted. *}
+lemma tsmed_inftick [simp]: "#ora=\<infinity> \<Longrightarrow> tsMed\<cdot>tsInfTick\<cdot>ora = tsInfTick"
 oops
 
-lemma tsmed_trues [simp]: "tsMed\<cdot>ts\<cdot>((\<up>True) \<infinity>) = ts"
+text {* Medium without oracle will transmit all messages and ticks. *}
+lemma tsmed_inftrue [simp]: "tsMed\<cdot>msg\<cdot>((\<up>True) \<infinity>) = msg"
 oops
 
+(* ToDo: More properties lemmata for medium, first show testings below *)
 
-(* Some more complicated stuff, first show the testings below *)
+text {* Two medium can be reduced to one medium. *}
+lemma tsmed2med: obtains ora3 where "tsMed\<cdot>(tsMed\<cdot>msg\<cdot>ora1)\<cdot>ora2 = tsMed\<cdot>msg\<cdot>ora3"
+oops
 
-  (* You can reduce 2 mediums to one medium *)
-lemma tsmeds2med: obtains o3 where "tsMed\<cdot>(tsMed\<cdot>ts\<cdot>o1)\<cdot>o2= tsMed\<cdot>ts\<cdot>o3"
-  oops
+text {* Two medium with fairness requirement can be reduced to one medium with 
+        fairness requirement. *}
+lemma tsmed2infmed: assumes "#({True} \<ominus> ora1)=\<infinity>" and "#({True} \<ominus> ora2)=\<infinity>" 
+  obtains ora3 where "tsMed\<cdot>(tsMed\<cdot>msg\<cdot>ora1)\<cdot>ora2 = tsMed\<cdot>msg\<cdot>ora3" and "#({True} \<ominus> ora3)=\<infinity>"
+oops    
 
-lemma tsmed2infmed: assumes "#({True} \<ominus> o1)=\<infinity>" and "#({True} \<ominus> o2)=\<infinity>" 
-    obtains o3 where "tsMed\<cdot>(tsMed\<cdot>ts\<cdot>o1)\<cdot>o2= tsMed\<cdot>ts\<cdot>o3" and "#({True} \<ominus> o3)=\<infinity>"
-  oops    
-
-lemma tsmed_map: "tsMed\<cdot>(tsMap f\<cdot>ts)\<cdot>oracle = tsMap f\<cdot>(tsMed\<cdot>ts\<cdot>oracle)"
-  oops
+lemma tsmed_map: "tsMed\<cdot>(tsMap f\<cdot>msg)\<cdot>ora = tsMap f\<cdot>(tsMed\<cdot>msg\<cdot>ora)"
+oops
     
 (* ----------------------------------------------------------------------- *)
 section {* Receiver *}
@@ -91,23 +95,8 @@ by (metis (no_types, lifting) assoc_sconc list2s.simps(1) list2s.simps(2) lscons
 lemma "tsMed\<cdot>\<bottom>\<cdot>((\<up>True) \<infinity>) = \<bottom>"
 by (simp add: tsmed_insert)
 
-(* ToDo: Lemmata for Testing the medium. Proof sketch for first lemma.
-         Other lemmata analogously. Alternatively use tsMLscons representation *)
+(* ToDo: Testing lemmata *)
 
-  (* SWS: these lemmata might be helpful to test the medium. Might...
-lemma h1_f1: "tsZip\<cdot>(Abs_tstream (<[Msg 1, \<surd>, Msg 2, \<surd>, Msg 3, \<surd>]>))\<cdot>(\<up>True \<infinity>) 
-           = Abs_tstream (<[Msg (1, True), \<surd>, Msg (2, True), \<surd>, Msg (3, True), \<surd>]>)"
-oops
-
-lemma h2_f1: 
-  "tsFilter {x. snd x}\<cdot>(Abs_tstream (<[\<M> (1, True), \<surd>, \<M> (2, True), \<surd>, \<M> (3, True), \<surd>]>))
-                    = (Abs_tstream (<[\<M> (1, True), \<surd>, \<M> (2, True), \<surd>, \<M> (3, True), \<surd>]>))"
-oops
-
-lemma h3_f1: "tsProjFst\<cdot>(Abs_tstream (<[\<M> (1, True), \<surd>, \<M> (2, True), \<surd>, \<M> (3, True), \<surd>]>)) 
-                   = Abs_tstream (<[\<M> 1, \<surd>, \<M> 2, \<surd>, \<M> 3, \<surd>]>)"
-oops
-*)
 lemma "tsMed\<cdot>OneTwoThree\<cdot>((\<up>True) \<infinity>) = OneTwoThree"
 oops
 
