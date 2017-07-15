9776ae580c2b83e021678f887faaebb8db2b4828
Sebastian Wolfram Stüber <sebastian.stueber@rwth-aachen.de>
2017-07-15 19:56:36 +0200

    sender type changed

--- CaseStudies/ABP/TimedABP_Comp.thy
+++ CaseStudies/ABP/TimedABP_Comp.thy
@@ -13,30 +13,38 @@ imports "../TimedABP"
 begin
 default_sort countable
 
-type_synonym 'a sender = "('a tstream \<rightarrow> bool tstream \<rightarrow> bool discr \<rightarrow> ('a \<times> bool) tstream)"
+type_synonym 'a sender = "('a tstream \<rightarrow> bool tstream  \<rightarrow> ('a \<times> bool) tstream)"
 
 definition tsSnd2Rec :: "'a sender \<rightarrow> 'a tstream \<rightarrow> 'a tstream" where
-"tsSnd2Rec \<equiv> \<Lambda> tssnd msg. snd (fix\<cdot>(\<Lambda> (acks, out). tsRec\<cdot>(delayFun\<cdot>(tssnd\<cdot>msg\<cdot>acks\<cdot>(Discr True)))))"
+"tsSnd2Rec \<equiv> \<Lambda> tssnd msg. snd (fix\<cdot>(\<Lambda> (acks, out). tsRec\<cdot>(delayFun\<cdot>(tssnd\<cdot>msg\<cdot>acks))))"
 
-definition "tsSender = {tsSnd :: 'a tstream \<rightarrow> bool tstream \<rightarrow> bool discr \<rightarrow> ('a \<times> bool) tstream. 
-  \<forall>i as ack. 
-  tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(tsSnd\<cdot>i\<cdot>as\<cdot>ack))) \<sqsubseteq> tsAbs\<cdot>i \<and> 
-  tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(tsSnd\<cdot>i\<cdot>as\<cdot>ack)))) 
-    = tsAbs\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(tsSnd\<cdot>i\<cdot>as\<cdot>ack))) \<and>
-  #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(tsSnd\<cdot>i\<cdot>as\<cdot>ack)))) 
+definition tsSender :: "('a sender) set" where
+"tsSender = {tsSnd :: 'a tstream \<rightarrow> bool tstream \<rightarrow> ('a \<times> bool) tstream. 
+  \<forall>i as. 
+  tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(tsSnd\<cdot>i\<cdot>as))) \<sqsubseteq> tsAbs\<cdot>i \<and> 
+  tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(tsSnd\<cdot>i\<cdot>as)))) 
+    = tsAbs\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(tsSnd\<cdot>i\<cdot>as))) \<and>
+  #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(tsSnd\<cdot>i\<cdot>as)))) 
     = min (#(tsAbs\<cdot>i)) (lnsuc\<cdot>(#(tsAbs\<cdot>(tsRemDups\<cdot>as)))) \<and>
-  #(tsAbs\<cdot>i) > #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<longrightarrow> #(tsAbs\<cdot>(tsSnd\<cdot>i\<cdot>as\<cdot>ack)) = \<infinity>
+  (#(tsAbs\<cdot>i) > #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<longrightarrow> #(tsAbs\<cdot>(tsSnd\<cdot>i\<cdot>as)) = \<infinity>)
 }"
 
 lemma tssnd2rec_inp2out: assumes "tssnd \<in> tsSender"
   shows "tsAbs\<cdot>(tsSnd2Rec\<cdot>tssnd\<cdot>msg) = tsAbs\<cdot>msg"
   apply (simp add: tsSnd2Rec_def)
   oops
-    
+
+    (* TADA *)
+lemma 
+  fixes send:: "'a sender"
+  assumes "send\<in>tsSender"
+  shows "tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as))) \<sqsubseteq> tsAbs\<cdot>i"
+  using assms tsSender_def by auto
+
   (* goal lemma *)    
 lemma assumes 
       "tssnd \<in> tsSender"
-  and "ds = tssnd\<cdot>msg\<cdot>as\<cdot>ack"
+  and "ds = tssnd\<cdot>msg\<cdot>as"
   and "as = tsProjSnd\<cdot>ds"
   shows "tsAbs\<cdot>(tsRecSnd\<cdot>ds) = tsAbs\<cdot>msg"
   oops
