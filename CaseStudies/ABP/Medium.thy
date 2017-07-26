f0d2b5c527bdb42fad84ea616d93724cff054608
Sebastian Wolfram Stüber <sebastian.stueber@rwth-aachen.de>
2017-07-26 15:38:32 +0200

    #121 fixrec definition to get the new oracle

--- CaseStudies/ABP/Medium.thy
+++ CaseStudies/ABP/Medium.thy
@@ -24,6 +24,22 @@ definition tsMed :: "'a tstream \<rightarrow> bool stream \<rightarrow> 'a tstre
 section {* basic properties *}
 (* ----------------------------------------------------------------------- *)
 
+  
+fixrec newOracle :: "bool stream \<rightarrow> bool stream \<rightarrow> bool stream" where
+"newOracle\<cdot>\<bottom>\<cdot>bs = \<bottom> " |
+"newOracle\<cdot>as\<cdot>\<bottom> = \<bottom> " |
+"newOracle\<cdot>((up\<cdot>a)&&as)\<cdot>((up\<cdot>b)&&bs) = 
+  (* First oracle is not transmitting the message *)
+  (if(a = Discr False) then (updis False)&&newOracle\<cdot>as\<cdot>((up\<cdot>b)&&bs)
+  
+  (* First oracle is transmitting *)
+   else  up\<cdot>b && newOracle\<cdot>as\<cdot>bs)"
+
+(* Testing that it works *)
+lemma "newOracle\<cdot>(<[True, True, False, True]>)\<cdot>(<[True, False, True]>) = <[True, False, False, True]>"
+apply (simp only: list2s_0 list2s_Suc)
+  by fixrec_simp
+
 text {* Assumption for medium lemmata: #({True} \<ominus> ora)=\<infinity> *}
 
 lemma tsmed_insert: "tsMed\<cdot>msg\<cdot>ora = tsProjFst\<cdot>(tsFilter {x. snd x}\<cdot>(tsZip\<cdot>msg\<cdot>ora))"
