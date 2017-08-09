49e727fcbbf284fb335a89b437f60fc3e986da6e
Jan Müllers <jan.muellers@rwth-aachen.de>
2017-08-09 17:03:30 +0200

    issue #98 solved tests for medium, 1 test for for reciever still open

--- CaseStudies/ABP/Testing.thy
+++ CaseStudies/ABP/Testing.thy
@@ -26,25 +26,26 @@ subsection {* sender *}
 definition tsSndExampInp_1 :: "nat tstream" where
   "tsSndExampInp_1 = <[Msg 1, Msg 2, \<surd>, Msg 1, \<surd>]>\<surd>"
 
-
 definition tsSndExampInp_2 :: "bool tstream" where
   "tsSndExampInp_2 = <[\<surd>, Msg True, Msg True, \<surd>, Msg False, \<surd>, Msg True, \<surd>]>\<surd>"
 
 definition tsSndExampOut :: "(nat \<times> bool) tstream" where
   "tsSndExampOut = <[Msg (1, True), \<surd>,  Msg (2, False), Msg (2, False), \<surd>, \<surd>, Msg (1, True), \<surd>, \<surd>]>\<surd>"
 
-(* ToDo: testing lemmata for sender *)
+(* ToDo: testing lemmata for sender *) (* wait for redo of Sender*)
 
-lemma tsmed_test_bot: "tsSnd\<cdot>\<bottom>\<cdot>tsInfTick = \<bottom>"
-oops
+lemma tssnd_test_bot: "tsSnd\<cdot>\<bottom>\<cdot>tsInfTick = \<bottom>" 
+  by (fixrec_simp)
 
 lemma tssnd_test_fin: 
   "tsSnd\<cdot>tsSndExampInp_1\<cdot>(tsSndExampInp_2 \<bullet> tsInfTick)\<cdot>(Discr True) = tsSndExampOut"
-oops
+  apply (simp add: tsSndExampInp_1_def tsSndExampInp_2_def tsSndExampOut_def)
+  oops
 
 lemma tssnd_test_inf:
   "tsSnd\<cdot>(tsSndExampInp_1 \<bullet> tsInfTick)\<cdot>(tsSndExampInp_2 \<bullet> tsInfTick)\<cdot>(Discr True) 
        = tsSndExampOut \<bullet> tsInfTick"
+  apply (simp add: tsSndExampInp_1_def tsSndExampInp_2_def tsSndExampOut_def )
 oops
 
 (* ----------------------------------------------------------------------- *)
@@ -54,25 +55,36 @@ subsection {* medium *}
 definition tsMedExampInp :: "nat tstream" where
   "tsMedExampInp = <[Msg 1, \<surd>, Msg 2, \<surd>, Msg 3, \<surd>]>\<surd>"
 
-
 definition tsMedExampOut :: "nat tstream" where
   "tsMedExampOut = <[Msg 1, \<surd>, \<surd>, Msg 3, \<surd>]>\<surd>"
 
-
 lemma tsmed_test_bot: "tsMed\<cdot>\<bottom>\<cdot>((\<up>True) \<infinity>) = \<bottom>"
-by (simp add: tsmed_insert)
-
-(* ToDo: testing lemmata for medium *)
+  by simp
 
 lemma tsmed_test_fin: "tsMed\<cdot>tsMedExampInp\<cdot>((\<up>True) \<infinity>) = tsMedExampInp"
-oops
+  by simp
 
 lemma tsmed_test_fin2: "tsMed\<cdot>tsMedExampInp\<cdot>(<[True, False]> \<infinity>) = tsMedExampOut"
-oops
-
+proof -
+  have h1: "#(updis False && updis True && updis False && ((updis True && \<up>False)\<infinity>)) = \<infinity>"
+    by (simp add: lscons_conv slen_sinftimes)
+  have h2: "#(updis True && updis False && ((updis True && \<up>False)\<infinity>)) = \<infinity>"
+    by (simp add: lscons_conv slen_sinftimes)
+  have h3: "#(updis False && ((updis True && \<up>False)\<infinity>)) = \<infinity>"
+    by (simp add: lscons_conv slen_sinftimes)  
+  show ?thesis    
+  apply (simp add: tsMedExampInp_def tsMedExampOut_def)
+  apply (subst sinftimes_unfold)
+  apply (subst sinftimes_unfold,auto)  
+  apply (fold lscons_conv)    
+  apply (simp add: h1 tsmed_mlscons_true tsmed_delayfun)  
+  apply (simp add: h2 tsmed_mlscons_false tsmed_delayfun)  
+  by (simp add: h3 tsmed_mlscons_true tsmed_delayfun)  
+qed
+  
 lemma tsmed_test_inf:
   "tsMed\<cdot>(tsMedExampInp \<bullet> tsInfTick)\<cdot>((\<up>True) \<infinity>) = (tsMedExampInp \<bullet> tsInfTick)"
-oops
+  by simp
 
 (* ----------------------------------------------------------------------- *)
 subsection {* receiver *}
@@ -90,13 +102,49 @@ definition tsRecExampOut_2 :: "nat tstream" where
 (* ToDo: testing lemmata for receiver *)
 
 lemma tsrec_test_bot: "tsRec\<cdot>\<bottom> = \<bottom>"
-oops
+  by simp
 
 lemma tsrec_test_fin: "tsRec\<cdot>tsRecExampInp = (tsRecExampOut_1, tsRecExampOut_2)"
-oops
+  by (simp add: tsrec_insert tsrecsnd_insert tsRecExampInp_def tsRecExampOut_1_def 
+      tsRecExampOut_2_def tsprojsnd_delayfun tsprojsnd_mlscons tsprojfst_delayfun 
+      tsprojfst_mlscons tsremdups_h_delayfun tsremdups_h_mlscons tsremdups_h_mlscons_dup 
+      tsremdups_h_mlscons_ndup tsremdups_insert)  
 
+lemma tsprojsnd_con: "tsProjSnd\<cdot>(a\<bullet>b) = (tsProjSnd\<cdot>a)\<bullet>(tsProjSnd\<cdot>b)"
+  apply (simp add: tsprojsnd_insert)
+  apply (simp add: tsmap_insert)
+  by (simp add: smap_split tsconc_insert tsmap_h_well)  
+ 
+(*    
+lemma tsprojfst_con: "tsProjFst\<cdot>(a\<bullet>b) = (tsProjFst\<cdot>a)\<bullet>(tsProjFst\<cdot>b)"
+  apply (simp add: tsprojfst_insert)
+  apply (simp add: tsmap_insert)
+  by (simp add: smap_split tsconc_insert tsmap_h_well)  
+*)  
+lemma h34: "(updis (Suc 0, True) &&\<surd> delay (updis (Suc 0, True) &&\<surd> delay (updis (Suc 0, False) &&\<surd> delay \<bottom>))) \<bullet>
+  tsInfTick \<noteq> \<bottom>"
+  sorry
+    
 lemma tsrec_test_inf:
   "tsRec\<cdot>(tsRecExampInp \<bullet> tsInfTick) = (tsRecExampOut_1 \<bullet> tsInfTick, tsRecExampOut_2 \<bullet> tsInfTick)"
+  apply (simp add: tsrec_insert)
+  apply (simp add: tsRecExampInp_def tsRecExampOut_1_def tsRecExampOut_2_def)  
+  apply (simp add: tsprojsnd_con)
+  apply (simp add: tsprojsnd_mlscons tsprojsnd_delayfun)  
+  apply (simp add: tsrecsnd_insert tsremdups_insert)
+  using [[simp_trace=true]] 
+  apply (unfold tsremdups_h_mlscons [of 
+ "((updis (Suc 0, True) &&\<surd> delay (updis (Suc 0, True) &&\<surd> delay (updis (Suc 0, False) &&\<surd> delay \<bottom>))) \<bullet>
+      tsInfTick)" "(Suc 0, True)"])
+  
+  apply (simp add: tsremdups_h_mlscons_dup)
+  apply (simp add: tsremdups_h_delayfun)
+  apply (simp add: tsremdups_h_mlscons_dup)  
+  apply (simp add: tsremdups_h_delayfun)
+  apply (simp add: tsremdups_h_mlscons_ndup) 
+  apply (simp add: tsremdups_h_delayfun)
+  apply (simp add: tsprojfst_delayfun tsprojfst_mlscons)  
+*)  
 oops
     
 end
