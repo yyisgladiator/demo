e44c32578021afd18cced6fe257aa68a190ad7cb
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2017-08-18 16:25:29 +0200

    Continued correctness proof of  ABP. Only four sorry left.

--- CaseStudies/ABP/Composition_DS.thy
+++ CaseStudies/ABP/Composition_DS.thy
@@ -55,10 +55,15 @@ lemma set2tssnd_nack2inftrans: assumes "send \<in> tsSender"
 subsection {* additional lemmata *}
 (* ----------------------------------------------------------------------- *)
 
-(* 3rd axiom *)
-lemma set2tssnd_ack2trans2: assumes "send\<in>tsSender"
+(* 2nd axiom *)
+lemma axiom2: assumes "send \<in> tsSender"
   shows "#(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))) \<le> lnsuc\<cdot>(#(tsAbs\<cdot>(tsRemDups\<cdot>as)))"
   using assms(1) set2tssnd_ack2trans by fastforce
+
+(* 3rd axiom modification *)
+lemma axiom3: assumes "send \<in> tsSender" and a1: "#(tsAbs\<cdot>(tsRemDups\<cdot>as)) = \<infinity>"
+  shows "#(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as))"
+  by (simp add: a1)
 
 (* property 0 *)
 lemma prop0: assumes "#({True} \<ominus> p) = \<infinity>"
@@ -70,9 +75,17 @@ lemma prop1: assumes "#({True} \<ominus> p1) = \<infinity>" and "#({True} \<omin
   shows "#(tsAbs\<cdot>(tsRemDups\<cdot>(tsMed\<cdot>(tsProjSnd\<cdot>(tsMed\<cdot>ts\<cdot>p1))\<cdot>p2))) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ts)))"
   sorry
 
-(* property 2 *) (* provable with types? *)
-lemma prop2: "#(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ts))) \<le> #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ts)))"
-  sorry
+lemma set2tssnd_alt_bit_tabs: assumes "send \<in> tsSender"
+  shows "srcdups\<cdot>(sprojsnd\<cdot>(srcdups\<cdot>(tsAbs\<cdot>(send\<cdot>i\<cdot>as)))) 
+    = (sprojsnd\<cdot>(srcdups\<cdot>(tsAbs\<cdot>(send\<cdot>i\<cdot>as))))"
+  by (metis assms set2tssnd_alt_bit tsprojsnd_tsabs tsremdups_tsabs)
+
+(* replaced property 2 #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ts))) \<le> #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ts))) *)
+lemma tssnd_tsprojsnd_tsremdups:
+  assumes send_def: "send \<in> tsSender"
+  shows "tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>(send\<cdot>i\<cdot>as))) = tsAbs\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))"
+  apply (simp add: tsprojsnd_tsabs tsremdups_tsabs sprojsnd_def)
+  by (metis eta_cfun send_def set2tssnd_alt_bit_tabs sprojsnd_def srcdups_smap_com)
 
 (* property 3 *)
 lemma prop3: "#(tsAbs\<cdot>ts) = \<infinity> \<Longrightarrow> #(tsAbs\<cdot>(tsMed\<cdot>(tsProjSnd\<cdot>(tsMed\<cdot>ts\<cdot>p1))\<cdot>p2)) = \<infinity>"
@@ -114,7 +127,7 @@ lemma tsaltbitpro_inp2out:
     have h1: "#(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ds)))"
       by (simp add: ar_def as_def dr_def p1_def p2_def prop1)
     hence "#(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<le> #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ds)))"
-      using prop2 trans_lnle by blast
+      by (simp add: ds_def send_def tssnd_tsprojsnd_tsremdups)
     hence leq: "#(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<le> #(tsAbs\<cdot>i)"
       using ds_def send_def set2tssnd_ack2trans by fastforce
     (* #(tsAbs\<cdot>i) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as)) *)
@@ -122,7 +135,8 @@ lemma tsaltbitpro_inp2out:
           \<or> #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ds))) = lnsuc\<cdot>(#(tsAbs\<cdot>(tsRemDups\<cdot>as)))"
       by (metis ds_def le_less_linear min_absorb2 send_def set2tssnd_ack2trans)
     hence "#(tsAbs\<cdot>i) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<or> (#(tsAbs\<cdot>(tsRemDups\<cdot>as)) = \<infinity> \<or> #(tsAbs\<cdot>as) < \<infinity>)"
-      sorry
+      by (metis Inf'_neq_0 leI prop3 send_def set2tssnd_nack2inftrans strict_slen tsabs_bot 
+          tsmed_strict(2))
     hence "#(tsAbs\<cdot>i) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<or> #(tsAbs\<cdot>(tsRemDups\<cdot>as)) = \<infinity>"
       by (metis ar_def as_def dr_def ds_def leI neq_iff prop3 send_def set2tssnd_nack2inftrans)
     hence geq: "#(tsAbs\<cdot>i) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as))"
@@ -135,7 +149,7 @@ lemma tsaltbitpro_inp2out:
       by (metis ds_def eq less_lnsuc min_absorb1 send_def set2tssnd_ack2trans)
     (* property 7 *)
     have prop7: "#(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ds))) = #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ds)))"
-      by (metis eq h1 order_class.order.antisym prop2 prop6)
+      by (simp add: ds_def send_def tssnd_tsprojsnd_tsremdups)
     (* property 8 *)
     have prop8: "#(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ds))) = #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>dr)))"
       by (metis ar_def as_def dr_def dual_order.antisym eq p1_def p2_def prop0 prop6 prop7
