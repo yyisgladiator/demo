0e46aeff5b43139870fd53fa9b167ab8ff46bff5
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2017-08-18 15:48:04 +0200

    Started correctness proof of  ABP in workspace file.

---
+++ CaseStudies/ABP/Composition_DS.thy
@@ -0,0 +1,151 @@
+(*  Title:        Composition.thy
+    Author:       Dennis Slotboom
+    e-mail:       dennis.slotboom@rwth-aachen.de
+
+    Description:  Definition of Alternating Bit Protocol on Timed Streams
+*)
+
+chapter {* Alternating Bit Protocol *}       
+                                                            
+theory Composition_DS
+imports Medium Receiver
+
+begin
+default_sort countable
+
+(* ----------------------------------------------------------------------- *)
+section {* definition of the set of sender *}
+(* ----------------------------------------------------------------------- *)
+
+type_synonym 'a sender = "('a tstream \<rightarrow> bool tstream  \<rightarrow> ('a \<times> bool) tstream)"
+
+definition tsSender :: "('a sender) set" where
+"tsSender = {send :: 'a tstream \<rightarrow> bool tstream \<rightarrow> ('a \<times> bool) tstream. 
+  \<forall>i as. 
+  tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as))) \<sqsubseteq> tsAbs\<cdot>i \<and> 
+  tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))) 
+    = tsAbs\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as))) \<and>
+  #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))) 
+    = min (#(tsAbs\<cdot>i)) (lnsuc\<cdot>(#(tsAbs\<cdot>(tsRemDups\<cdot>as)))) \<and>
+  (#(tsAbs\<cdot>i) > #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<longrightarrow> #(tsAbs\<cdot>(send\<cdot>i\<cdot>as)) = \<infinity>)
+}"
+
+(* 1st axiom *)
+lemma set2tssnd_prefix_inp: assumes "send \<in> tsSender"
+  shows "tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as))) \<sqsubseteq> tsAbs\<cdot>i"
+  using assms tsSender_def by auto
+
+lemma set2tssnd_alt_bit: assumes "send \<in> tsSender"
+  shows "tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))) 
+    = tsAbs\<cdot>(tsProjSnd\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))"
+  using assms tsSender_def by auto
+
+(* 5th axiom *)  
+lemma set2tssnd_ack2trans: assumes "send \<in> tsSender"
+  shows "#(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))) 
+    = min (#(tsAbs\<cdot>i)) (lnsuc\<cdot>(#(tsAbs\<cdot>(tsRemDups\<cdot>as))))"
+  using assms tsSender_def by auto
+
+(* 4th axiom *)
+lemma set2tssnd_nack2inftrans: assumes "send \<in> tsSender"
+  shows "(#(tsAbs\<cdot>i) > #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<longrightarrow> #(tsAbs\<cdot>(send\<cdot>i\<cdot>as)) = \<infinity>)"
+  using assms tsSender_def by auto
+
+(* ----------------------------------------------------------------------- *)
+subsection {* additional lemmata *}
+(* ----------------------------------------------------------------------- *)
+
+(* 3rd axiom *)
+lemma set2tssnd_ack2trans2: assumes "send\<in>tsSender"
+  shows "#(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(send\<cdot>i\<cdot>as)))) \<le> lnsuc\<cdot>(#(tsAbs\<cdot>(tsRemDups\<cdot>as)))"
+  using assms(1) set2tssnd_ack2trans by fastforce
+
+(* property 0 *)
+lemma prop0: assumes "#({True} \<ominus> p) = \<infinity>"
+  shows "#(tsAbs\<cdot>(tsRemDups\<cdot>(tsMed\<cdot>ts\<cdot>p))) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>ts))"
+  sorry
+
+(* property 1 *)
+lemma prop1: assumes "#({True} \<ominus> p1) = \<infinity>" and "#({True} \<ominus> p2) = \<infinity>"
+  shows "#(tsAbs\<cdot>(tsRemDups\<cdot>(tsMed\<cdot>(tsProjSnd\<cdot>(tsMed\<cdot>ts\<cdot>p1))\<cdot>p2))) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ts)))"
+  sorry
+
+(* property 2 *) (* provable with types? *)
+lemma prop2: "#(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ts))) \<le> #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ts)))"
+  sorry
+
+(* property 3 *)
+lemma prop3: "#(tsAbs\<cdot>ts) = \<infinity> \<Longrightarrow> #(tsAbs\<cdot>(tsMed\<cdot>(tsProjSnd\<cdot>(tsMed\<cdot>ts\<cdot>p1))\<cdot>p2)) = \<infinity>"
+  sorry
+
+(* property 4 *)
+lemma prop4: assumes "#({True} \<ominus> p) = \<infinity>"
+  shows"#(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ts))) < \<infinity> 
+  \<and> #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ts))) = #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ts)))
+  \<and> #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ts))) = #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>(tsMed\<cdot>ts\<cdot>p))))
+  \<Longrightarrow> tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ts)) = tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>(tsMed\<cdot>ts\<cdot>p)))"
+  sorry
+
+(* ----------------------------------------------------------------------- *)
+subsection {* complete composition *}
+(* ----------------------------------------------------------------------- *)
+
+text {* 
+   i = input stream
+   as = acks stream (in sender)
+   ar = acks stream (from receiver)
+   ds = output stream (from sender)
+   dr = output stream (in receiver)
+   p1/p2 = oracle stream
+*}
+
+lemma tsaltbitpro_inp2out:
+  assumes send_def: "send \<in> tsSender"
+    and p1_def: "#({True} \<ominus> p1) = \<infinity>"
+    and p2_def: "#({True} \<ominus> p2) = \<infinity>"
+    and ds_def: "ds = send\<cdot>i\<cdot>as"
+    and dr_def: "dr = tsMed\<cdot>ds\<cdot>p1"
+    and ar_def: "ar = tsProjSnd\<cdot>dr"
+    (* definition 5 *)
+    and as_def: "as = tsMed\<cdot>ar\<cdot>p2"
+  shows "tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>dr)) = tsAbs\<cdot>i"
+  proof -
+    (* #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<le> #(tsAbs\<cdot>i) *)
+    have h1: "#(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ds)))"
+      by (simp add: ar_def as_def dr_def p1_def p2_def prop1)
+    hence "#(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<le> #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ds)))"
+      using prop2 trans_lnle by blast
+    hence leq: "#(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<le> #(tsAbs\<cdot>i)"
+      using ds_def send_def set2tssnd_ack2trans by fastforce
+    (* #(tsAbs\<cdot>i) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as)) *)
+    have "#(tsAbs\<cdot>i) < lnsuc\<cdot>(#(tsAbs\<cdot>(tsRemDups\<cdot>as))) 
+          \<or> #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ds))) = lnsuc\<cdot>(#(tsAbs\<cdot>(tsRemDups\<cdot>as)))"
+      by (metis ds_def le_less_linear min_absorb2 send_def set2tssnd_ack2trans)
+    hence "#(tsAbs\<cdot>i) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<or> (#(tsAbs\<cdot>(tsRemDups\<cdot>as)) = \<infinity> \<or> #(tsAbs\<cdot>as) < \<infinity>)"
+      sorry
+    hence "#(tsAbs\<cdot>i) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as)) \<or> #(tsAbs\<cdot>(tsRemDups\<cdot>as)) = \<infinity>"
+      by (metis ar_def as_def dr_def ds_def leI neq_iff prop3 send_def set2tssnd_nack2inftrans)
+    hence geq: "#(tsAbs\<cdot>i) \<le> #(tsAbs\<cdot>(tsRemDups\<cdot>as))"
+      by auto
+    (* equalities *)
+    have eq: "#(tsAbs\<cdot>i) = #(tsAbs\<cdot>(tsRemDups\<cdot>as))"
+      by (simp add: dual_order.antisym geq leq)
+    (* property 6 *)
+    have prop6: "#(tsAbs\<cdot>i) = #(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ds)))"
+      by (metis ds_def eq less_lnsuc min_absorb1 send_def set2tssnd_ack2trans)
+    (* property 7 *)
+    have prop7: "#(tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ds))) = #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ds)))"
+      by (metis eq h1 order_class.order.antisym prop2 prop6)
+    (* property 8 *)
+    have prop8: "#(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>ds))) = #(tsAbs\<cdot>(tsRemDups\<cdot>(tsProjSnd\<cdot>dr)))"
+      by (metis ar_def as_def dr_def dual_order.antisym eq p1_def p2_def prop0 prop6 prop7
+          sfilterl4 tsmed_map tsprojsnd_insert)
+    (* tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>dr)) = tsAbs\<cdot>i *)
+    have h2: "tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>dr)) = tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>ds))"
+      by (metis Inf'_neq_0 dr_def inf_ub less_le p1_def prop3 prop4 prop7 prop8 strict_slen 
+          tsabs_bot tsmed_strict(2) tsprojfst_tsabs_slen)
+    show "tsAbs\<cdot>(tsProjFst\<cdot>(tsRemDups\<cdot>dr)) = tsAbs\<cdot>i"
+      by (simp add: ds_def eq_slen_eq_and_less h2 prop6 send_def set2tssnd_prefix_inp)
+  qed
+    
+end
