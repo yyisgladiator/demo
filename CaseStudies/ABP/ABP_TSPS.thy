e137e0fdc6463d39095d27d32273deb79ab26926
JB <jens.buerger@rwth-aachen.de>
2017-09-06 15:24:42 +0200

    fixed data-type added receiver TSPF

--- CaseStudies/ABP/ABP_TSPS.thy
+++ CaseStudies/ABP/ABP_TSPS.thy
@@ -6,15 +6,17 @@
 *)
 
 theory ABP_TSPS
-  imports "../../TSPS" Receiver Composition Medium
+  imports "../../TSPS" Receiver Composition Medium "../../TSPF"
     
 begin
 
   default_sort message 
   
-(* TODO: datatype instantiation *)
+(* ----------------------------------------------------------------------- *)
+section \<open>Datatype Definition\<close>
+(* ----------------------------------------------------------------------- *)
   
-datatype 'a::type MABP = myPair "('a * bool)" | Bool bool | Data 'a
+datatype 'a::type MABP = BoolPair "('a * bool)" | Bool bool | Data 'a
 
 instantiation MABP ::  (countable) countable
 begin
@@ -24,19 +26,69 @@ end
 
 instantiation MABP ::  (countable) message
 begin
-  fun ctype_MABP :: "channel \<Rightarrow> 'a MABP set" where
-  "ctype_MABP as = range myPair" | 
-  "ctype_MABP ds = range Bool" | 
-  "ctype_MABP ar = range Data" |
+fun ctype_MABP :: "channel \<Rightarrow> 'a MABP set" where
+  "ctype_MABP abpIn = range Data" |
+  "ctype_MABP abpOut = range Data" |
+  "ctype_MABP ds = range BoolPair" |
+  "ctype_MABP dr = range BoolPair" |
+  "ctype_MABP as = range Bool" |
+  "ctype_MABP ar = range Bool" |
   "ctype_MABP other = {}"
 
   instance ..
 end
+
+declare [[show_types]]  
   
-(* general lemmata *)
+(* ----------------------------------------------------------------------- *)
+section \<open>Definitions\<close>
+(* ----------------------------------------------------------------------- *)  
+ 
+subsection \<open>datatype destructors\<close>
+abbreviation invBoolPair :: "'a MABP \<Rightarrow> ('a \<times> bool)" where
+"invBoolPair \<equiv> (inv BoolPair)"
+
+abbreviation invBool :: "'a MABP \<Rightarrow> bool" where
+"invBool \<equiv> (inv Bool)"
+
+abbreviation invData :: "'a MABP \<Rightarrow> 'a" where
+"invData \<equiv> (inv Data)"
+  
+
+subsection \<open>receiver\<close> 
+definition recvTSPF :: "'a MABP TSPF" where
+"recvTSPF \<equiv> 
+let recRes = (\<lambda> x. tsRec\<cdot>((tsMap invBoolPair)\<cdot>(x . dr)))
+in Abs_CTSPF (\<lambda> x. (tsbDom\<cdot>x = {dr}) \<leadsto> [ar    \<mapsto> tsMap Bool\<cdot>(fst (recRes x)),
+                                        abpOut \<mapsto> tsMap Data\<cdot>(snd (recRes x))]\<Omega>)"
+  
+
+
+(* ----------------------------------------------------------------------- *)
+section \<open>lemmata\<close>
+(* ----------------------------------------------------------------------- *) 
+  
+subsection \<open>general\<close>
+  
 lemma tsmap_id[simp]: assumes "inj f" shows "tsMap (inv f)\<cdot>(tsMap f\<cdot>ts) = ts"
 apply(induction ts)
 by(simp_all add: assms tsmap_delayfun  tsmap_mlscons)
+ 
+subsection \<open>datatype\<close>  
+  
+  (* inverse BoolPair applied to BoolPair is identity *)
+lemma data_bool_pair_inv [simp]: "(invBoolPair) (BoolPair x) = x"
+  by (meson MABP.inject(1) f_inv_into_f rangeI)
+  
+  (* inverse Bool applied to Bool is identity *)
+lemma data_bool_inv [simp]: "(inv Bool) (Bool x) = x"
+  by (meson MABP.inject(2) f_inv_into_f rangeI)
+    
+  (* inverse Data applied to Data is identity *)
+lemma data_data_inv [simp]: "(inv Data) (Data x) = x"
+  by (meson MABP.inject(3) f_inv_into_f rangeI)
+  
+
 
 lemma "(\<lambda> x. if x= Bool True then True else False) (Bool False) = False"
   by simp
@@ -49,6 +101,7 @@ lemma "(inv Bool) (Bool x) = x"
     
 lemma "(inv Data) (Data x) = x"
   by (meson MABP.inject(3) f_inv_into_f rangeI)
+    
   
   
     
