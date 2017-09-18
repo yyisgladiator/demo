e6841f43a9f4e37002d6f0a139da0c0d493e1dfc
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2017-09-18 21:31:00 +0200

    Fetched back prop4s_h3.

--- CaseStudies/ABP/Medium_DS.thy
+++ CaseStudies/ABP/Medium_DS.thy
@@ -53,5 +53,110 @@ lemma sprojsnd_srcdups:
       apply simp_all
   by (metis antisym_conv2 inf_ub le2lnle order_refl slen_sprojsnd sprojsnd_scons sprojsnd_srcdups_slen)
   qed
+
+lemma prop4s_h3_h1: "a\<noteq>shd s \<Longrightarrow> srcdups\<cdot>(\<up>a \<bullet> s) = \<up>a\<bullet>srcdups\<cdot>s" 
+  by (metis srcdups_neq srcdups_shd srcdups_srt strict_sdropwhile surj_scons)
+    
+lemma prop4s_h3_h2: "a = shd s \<Longrightarrow> s\<noteq>\<epsilon>\<Longrightarrow> srcdups\<cdot>(\<up>a \<bullet> s) = srcdups\<cdot>s"
+  using srcdups_eq[of "shd s" "srt\<cdot>s"] surj_scons[of s] by auto    
+    
+lemma prop4s_h3_h3: "#(srcdups\<cdot>(sprojsnd\<cdot>(\<up>a))) = Fin 1"
+  by (simp add: sprojsnd_def)
+    
+lemma prop4s_h3_h4: "s \<noteq> \<epsilon> \<Longrightarrow> shd s = a \<Longrightarrow>
+    srcdups\<cdot>(\<up>(snd a) \<bullet> smap snd\<cdot>s) = srcdups\<cdot>(smap snd\<cdot>s)"
+  by (simp add: smap_hd_rst)    
+    
+lemma prop4s_h3_h5: "sMed\<cdot>s\<cdot>as \<noteq> \<epsilon> \<Longrightarrow> shd (smap snd\<cdot>(sMed\<cdot>s\<cdot>as)) \<noteq> snd a \<Longrightarrow> 
+    srcdups\<cdot>(\<up>(snd a) \<bullet> smap snd\<cdot>(sMed\<cdot>s\<cdot>as)) = \<up>(snd a) \<bullet> srcdups\<cdot>(smap snd\<cdot>(sMed\<cdot>s\<cdot>as))"
+  by (simp add: prop4s_h3_h1)    
+    
+lemma prop4s_h3_h6: " #(srcdups\<cdot>(smap snd\<cdot>s)) \<noteq> \<infinity> \<Longrightarrow>  lnsuc\<cdot>(#(srcdups\<cdot>(smap snd\<cdot>(sMed\<cdot>s\<cdot>as)))) = #(srcdups\<cdot>(smap snd\<cdot>s)) \<Longrightarrow> false"
+ sorry
+
+lemma prop4s_h3_h7: "s \<noteq> \<epsilon> \<Longrightarrow> shd (smap snd\<cdot>s) \<noteq> snd a \<Longrightarrow> 
+    srcdups\<cdot>(\<up>(snd a) \<bullet> smap snd\<cdot>s) = \<up>(snd a) \<bullet> srcdups\<cdot>(smap snd\<cdot>s)"
+  by (simp add: prop4s_h3_h1)   
+    
+lemma prop4s_h3_h8: "s \<noteq> \<epsilon> \<Longrightarrow> shd (smap snd\<cdot>s) =  snd a \<Longrightarrow>
+    srcdups\<cdot>(\<up>(snd a) \<bullet> smap snd\<cdot>s) = srcdups\<cdot>(smap snd\<cdot>s)"
+  by (simp add: smap_hd_rst)      
+    
+lemma prop4s_h3_h9: "#(srcdups\<cdot>s) \<noteq> \<infinity> \<Longrightarrow>
+         #(srcdups\<cdot>(smap snd\<cdot>s)) = lnsuc\<cdot>(#(srcdups\<cdot>s)) \<Longrightarrow> false" 
+  sorry
+
+lemma prop4s_h3_h10: " #(srcdups\<cdot>(smap snd\<cdot>s)) \<noteq> \<infinity> \<Longrightarrow>
+          #(srcdups\<cdot>(smap snd\<cdot>(sMed\<cdot>s\<cdot>as))) = lnsuc\<cdot>(#(srcdups\<cdot>(smap snd\<cdot>s))) \<Longrightarrow> false"     
+  sorry
+    
+lemma prop4s_h3: assumes  "#(srcdups\<cdot>s) \<noteq> \<infinity>" "#({True} \<ominus> p) = \<infinity>" "#(srcdups\<cdot>(sprojsnd\<cdot>(sMed\<cdot>s\<cdot>p))) = #(srcdups\<cdot>s)"  
+       "#(srcdups\<cdot>(sprojsnd\<cdot>s))= #(srcdups\<cdot>s)" shows 
+       "(srcdups\<cdot>s) = (srcdups\<cdot>(sMed\<cdot>s\<cdot>p))" 
+     using assms proof(induction s arbitrary: p rule:ind)
+       case 1
+       then show ?case sorry
+     next
+       case 2
+       then show ?case by simp
+     next
+       case (3 a s)
+       then show ?case 
+         apply (case_tac "s = \<epsilon>")
+         apply (cases rule: oracases,simp) 
+         apply (metis (no_types, lifting) smed_bot1 smed_t)
+         apply (metis (no_types, lifting) slen_empty_eq smed_bot1 smed_f  strict_sprojsnd strict_srcdups)         
+         apply (case_tac "shd s= a")
+         apply (simp add: prop4s_h3_h2)
+         apply (cases rule: oracases)
+         apply (simp add: prop4s_h3_h2)  
+         apply simp  
+         apply (case_tac "sMed\<cdot>s\<cdot>as= \<epsilon>") 
+         apply (simp add: prop4s_h3_h3)
+         apply (metis Fin_02bot Fin_Suc bot_is_0 inject_lnsuc sconc_snd_empty slen_empty_eq 
+                  srcdups_nbot srcdups_shd srt_decrements_length surj_scons)
+         apply (case_tac "shd (sMed\<cdot>s\<cdot>as)= a")
+         apply (simp add: prop4s_h3_h2)  
+         apply (simp add: sprojsnd_def)
+         apply (metis (no_types, lifting) smap_scons srcdups_eq surj_scons) 
+         apply (simp add: prop4s_h3_h1)  
+           apply (simp add: sprojsnd_def) 
+           apply (case_tac "shd (smap snd\<cdot>(sMed\<cdot>s\<cdot>as)) = snd a")
+         apply (smt prop4s_h3_h4 slen_empty_eq slen_smap srcdups_eq srcdups_shd2 surj_scons)
+           apply (simp add: prop4s_h3_h4 prop4s_h3_h5)
+         apply (metis prop4s_h3_h6)
+         apply (simp add: sprojsnd_def) 
+        apply (case_tac "shd (smap snd\<cdot>s) = snd a")
+           apply (simp add: prop4s_h3_h4)
+         apply (simp add: prop4s_h3_h4)
+         apply (simp add: prop4s_h3_h1)
+         apply (cases rule: oracases,simp)
+          apply simp  
+          apply (case_tac "sMed\<cdot>s\<cdot>as= \<epsilon>") 
+           apply (simp add: prop4s_h3_h3)
+         apply (metis Fin_02bot Fin_Suc bot_is_0 lnat.injects slen_empty_eq)
+           apply (case_tac "shd (sMed\<cdot>s\<cdot>as)= a")
+           apply (simp add: prop4s_h3_h2)
+           apply (simp add: sprojsnd_def)
+            apply (simp add: prop4s_h3_h4)
+         apply (case_tac "shd (smap snd\<cdot>s) = snd a")
+         apply (simp add: prop4s_h3_h8)
+         using prop4s_h3_h9 apply blast
+         apply (simp add: prop4s_h3_h1)
+         apply (metis prop4s_h3_h10)
+          apply (simp add: prop4s_h3_h1)
+           apply (simp add: sprojsnd_def) 
+         apply (case_tac "shd (smap snd\<cdot>s) = snd a")  
+           apply (simp add: prop4s_h3_h8)
+           apply (case_tac "shd (smap snd\<cdot>(sMed\<cdot>s\<cdot>as)) = snd a")
+           apply (simp add: prop4s_h3_h8)
+         using prop4s_h3_h9 apply blast
+         using prop4s_h3_h9 apply blast
+         apply (metis (no_types, lifting) lnat.sel_rews(2) prop4s_h3_h1 prop4s_h3_h10 prop4s_h3_h8 slen_scons)
+          apply (simp add: sprojsnd_def) 
+          apply (case_tac "shd (smap snd\<cdot>s) = snd a")
+         using prop4s_h3_h8 prop4s_h3_h9 apply fastforce
+         by (metis "3.prems"(1) fold_inf prop4s_h3_h1 prop4s_h3_h10 slen_scons)   
+  qed  
  
 end
