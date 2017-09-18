c5236b2b5bc8d75d95682e1f40a124fee8fe9781
Jan Müllers <jan.muellers@rwth-aachen.de>
2017-09-18 23:43:39 +0200

    Proved some helpers for Prop4

--- CaseStudies/ABP/Medium_DS.thy
+++ CaseStudies/ABP/Medium_DS.thy
@@ -12,7 +12,7 @@ imports "../../TStream" Medium
 
 begin
 default_sort countable
-
+(*
 lemma sprojsnd_srcdups:
   "#(srcdups\<cdot>s) \<noteq> \<infinity> \<Longrightarrow> #(srcdups\<cdot>(sprojsnd\<cdot>s)) = #(srcdups\<cdot>s)
      \<Longrightarrow> srcdups\<cdot>(sprojsnd\<cdot>s) = sprojsnd\<cdot>(srcdups\<cdot>s)"
@@ -53,7 +53,35 @@ lemma sprojsnd_srcdups:
       apply simp_all
   by (metis antisym_conv2 inf_ub le2lnle order_refl slen_sprojsnd sprojsnd_scons sprojsnd_srcdups_slen)
   qed
-
+*)
+lemma srcdups_smed_h: " #(srcdups\<cdot>(sMed\<cdot>s\<cdot>p)) \<le> #(srcdups\<cdot>s)"
+   proof (induction s arbitrary: p rule: ind)
+    case 1
+    then show ?case sorry
+  next
+    case 2
+    then show ?case by simp
+  next
+    case (3 a s)
+    then show ?case 
+      apply (case_tac "s= \<epsilon>")
+      apply (cases rule: oracases,simp)
+      apply (metis eq_refl smed_bot1 smed_t)
+      apply (metis bot_is_0 eq_bottom_iff le_cases lnle_def smed_bot1 smed_f strict_slen strict_srcdups) 
+      apply (cases rule: oracases,simp_all)
+      apply (case_tac "sMed\<cdot>s\<cdot>as = \<epsilon>")
+      apply (simp add: neq02Suclnle srcdups_nbot)
+      apply (case_tac "shd s = a")
+      apply (case_tac "shd (sMed\<cdot>s\<cdot>as) = a")
+      apply (simp add: srcdups_fst2snd)
+      apply (smt inject_scons smed_bot2 smed_f smed_t srcdups_eq surj_scons surj_scons)
+      apply (smt less_lnsuc lnsuc_lnle_emb srcdups_nfst2snd srcdups_fst2snd slen_scons trans_lnle)
+      by (metis (no_types, lifting) less_lnsuc srcdups_nfst2snd srcdups_fst2snd slen_scons trans_lnle) 
+  qed              
+    
+lemma smed_sprojsnd: "sprojsnd\<cdot>(sMed\<cdot>s\<cdot>p) = sMed\<cdot>(sprojsnd\<cdot>s)\<cdot>p" 
+  by (simp add: sprojsnd_def smed_smap)
+    
 lemma prop4s_h3_h1: "a\<noteq>shd s \<Longrightarrow> srcdups\<cdot>(\<up>a \<bullet> s) = \<up>a\<bullet>srcdups\<cdot>s" 
   by (metis srcdups_neq srcdups_shd srcdups_srt strict_sdropwhile surj_scons)
     
@@ -83,12 +111,31 @@ lemma prop4s_h3_h8: "s \<noteq> \<epsilon> \<Longrightarrow> shd (smap snd\<cdot
   by (simp add: smap_hd_rst)      
     
 lemma prop4s_h3_h9: "#(srcdups\<cdot>s) \<noteq> \<infinity> \<Longrightarrow>
-         #(srcdups\<cdot>(smap snd\<cdot>s)) = lnsuc\<cdot>(#(srcdups\<cdot>s)) \<Longrightarrow> false" 
-  sorry
+         #(srcdups\<cdot>(smap snd\<cdot>s)) = lnsuc\<cdot>(#(srcdups\<cdot>s)) \<Longrightarrow> False" 
+proof -
+  have "(srcdups\<cdot>(smap snd\<cdot>s)) = (srcdups\<cdot>(sprojsnd\<cdot>s))"
+    by (simp add: sprojsnd_def)
+  from this have "#(srcdups\<cdot>(smap snd\<cdot>s)) \<le> (#(srcdups\<cdot>s))"
+    using sprojsnd_srcdups_slen
+    by (metis slen_sprojsnd)
+  from this show "#(srcdups\<cdot>s) \<noteq> \<infinity> \<Longrightarrow>
+         #(srcdups\<cdot>(smap snd\<cdot>s)) = lnsuc\<cdot>(#(srcdups\<cdot>s)) \<Longrightarrow> False"
+    using le2lnle less_le by fastforce  
+  qed
 
 lemma prop4s_h3_h10: " #(srcdups\<cdot>(smap snd\<cdot>s)) \<noteq> \<infinity> \<Longrightarrow>
-          #(srcdups\<cdot>(smap snd\<cdot>(sMed\<cdot>s\<cdot>as))) = lnsuc\<cdot>(#(srcdups\<cdot>(smap snd\<cdot>s))) \<Longrightarrow> false"     
-  sorry
+          #(srcdups\<cdot>(smap snd\<cdot>(sMed\<cdot>s\<cdot>as))) = lnsuc\<cdot>(#(srcdups\<cdot>(smap snd\<cdot>s))) \<Longrightarrow> False"     
+  proof -
+  have h1: "(srcdups\<cdot>(smap snd\<cdot>(sMed\<cdot>s\<cdot>p))) = (srcdups\<cdot>(sprojsnd\<cdot>(sMed\<cdot>s\<cdot>p)))"
+    by (simp add: sprojsnd_def)
+  have h2: "(srcdups\<cdot>(smap snd\<cdot>s)) = (srcdups\<cdot>(sprojsnd\<cdot>s))"   
+    by (simp add: sprojsnd_def)
+  have "#(srcdups\<cdot>(sprojsnd\<cdot>(sMed\<cdot>s\<cdot>p))) \<le> (#(srcdups\<cdot>(sprojsnd\<cdot>s)))"
+    by(simp add: smed_sprojsnd srcdups_smed_h)
+  from this h1 h2 show "#(srcdups\<cdot>(smap snd\<cdot>s)) \<noteq> \<infinity> \<Longrightarrow>
+          #(srcdups\<cdot>(smap snd\<cdot>(sMed\<cdot>s\<cdot>as))) = lnsuc\<cdot>(#(srcdups\<cdot>(smap snd\<cdot>s))) \<Longrightarrow> False"
+    by (metis inf_ub leD less_le ln_less smed_smap srcdups_smed_h)
+  qed
     
 lemma prop4s_h3: assumes  "#(srcdups\<cdot>s) \<noteq> \<infinity>" "#({True} \<ominus> p) = \<infinity>" "#(srcdups\<cdot>(sprojsnd\<cdot>(sMed\<cdot>s\<cdot>p))) = #(srcdups\<cdot>s)"  
        "#(srcdups\<cdot>(sprojsnd\<cdot>s))= #(srcdups\<cdot>s)" shows 
