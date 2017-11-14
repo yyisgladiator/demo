8522503d9621df603d4a6e4f411a1e0b6b655ced
Marc <Marc.Wiartalla@rwth-aachen.de>
2017-11-14 22:06:31 +0100

    Renamed classes and definitions Started definition of ufun data type

--- UBundle.thy
+++ UBundle.thy
@@ -4,22 +4,23 @@
     Description:  All "Universal Classes". Later used to define bundles/pfun 
 *)
 
-theory CBundle
+theory UBundle
   imports UnivClasses
 begin
 
+  
 (****************************************************)
 section\<open>Data type\<close>
 (****************************************************)  
   
   
-definition cbWell :: "(channel \<rightharpoonup> ('s::us)) \<Rightarrow> bool" where
-"cbWell f \<equiv> \<forall>c \<in> (dom f). (usOkay c (f\<rightharpoonup>c))" 
+definition ubWell :: "(channel \<rightharpoonup> ('s::uscl)) \<Rightarrow> bool" where
+"ubWell f \<equiv> \<forall>c \<in> (dom f). (usOkay c (f\<rightharpoonup>c))" 
 
-lemma cbWell_empty: "cbWell empty"
-  by(simp add: cbWell_def)
+lemma ubWell_empty: "ubWell empty"
+  by(simp add: ubWell_def)
 
-lemma cbWell_adm: "adm cbWell"
+lemma ubWell_adm: "adm ubWell"
 proof - 
   have "\<And>(Y :: nat \<Rightarrow> (channel \<Rightarrow> 'a option)). chain Y \<Longrightarrow> (\<forall>i. \<forall>c\<in>dom (Y i). usOkay c Y i\<rightharpoonup>c) \<longrightarrow> (\<forall>c\<in>dom (Lub Y). usOkay c Lub Y\<rightharpoonup>c)"  
   proof -
@@ -32,21 +33,21 @@ proof -
       fix c
       assume "c\<in>dom (Lub Y)"
       show "(\<forall>i. usOkay c Y i\<rightharpoonup>c) \<longrightarrow> (usOkay c Lub Y\<rightharpoonup>c)"
-        using usokay_adm adm_def by (metis (mono_tags, lifting) f0 part_the_chain part_the_lub)
+        using usOkay_adm adm_def by (metis (mono_tags, lifting) f0 part_the_chain part_the_lub)
     qed
     show "(\<forall>i. \<forall>c\<in>dom (Y i). usOkay c Y i\<rightharpoonup>c) \<longrightarrow> (\<forall>c\<in>dom (Lub Y). usOkay c Lub Y\<rightharpoonup>c)"
       by (simp add: f1 f2)
   qed
   then show ?thesis
-    by(simp add: adm_def cbWell_def)
+    by(simp add: adm_def ubWell_def)
 qed
 
-cpodef 's::us cbundle ("(_\<^sup>\<omega>)" [1000] 999) = "{b :: channel \<rightharpoonup> 's . cbWell b}"
-  using cbWell_empty apply auto[1]
-  using cbWell_adm by auto
+cpodef 's::uscl ubundle ("(_\<^sup>\<omega>)" [1000] 999) = "{b :: channel \<rightharpoonup> 's . ubWell b}"
+  using ubWell_empty apply auto[1]
+  using ubWell_adm by auto
 
 
-setup_lifting type_definition_cbundle
+setup_lifting type_definition_ubundle
 
 
 (****************************************************)
@@ -54,30 +55,30 @@ section\<open>Definitions\<close>
 (****************************************************)
 
 
-default_sort us
+default_sort uscl
 
 (* This function can be used in "'m stream USB" and "'m tstream USB" *)
 (* and by the way, look at the "'m\<^sup>\<omega>" shorcode for 'm USB *)
-definition cbDom :: "'m\<^sup>\<omega> \<rightarrow> channel set" where
-"cbDom \<equiv> \<Lambda> b. dom (Rep_cbundle b)"
+definition ubDom :: "'m\<^sup>\<omega> \<rightarrow> channel set" where
+"ubDom \<equiv> \<Lambda> b. dom (Rep_ubundle b)"
 
-definition cbRestrict:: "channel set \<Rightarrow> 'm cbundle \<rightarrow> 'm cbundle" where
-"cbRestrict cs  \<equiv> \<Lambda> b. Abs_cbundle (Rep_cbundle b |` cs)"
+definition ubRestrict:: "channel set \<Rightarrow> 'm ubundle \<rightarrow> 'm ubundle" where
+"ubRestrict cs  \<equiv> \<Lambda> b. Abs_ubundle (Rep_ubundle b |` cs)"
 
-definition cbGetCh :: "channel \<Rightarrow> 'm cbundle \<rightarrow> 'm"  where
-"cbGetCh c = (\<Lambda> b. ((Rep_cbundle b) \<rightharpoonup> c))"
+definition ubGetCh :: "channel \<Rightarrow> 'm ubundle \<rightarrow> 'm"  where
+"ubGetCh c = (\<Lambda> b. ((Rep_ubundle b) \<rightharpoonup> c))"
 
-definition cbLeast :: "channel set \<Rightarrow> 'm cbundle"  where
-"cbLeast cs \<equiv> Abs_cbundle (\<lambda>c. (c \<in> cs) \<leadsto> \<bottom> )"
+definition ubLeast :: "channel set \<Rightarrow> 'm ubundle"  where
+"ubLeast cs \<equiv> Abs_ubundle (\<lambda>c. (c \<in> cs) \<leadsto> \<bottom> )"
 
 (* Interesting function, uses the "len" operator over 'm *)
-definition cbLen:: " 'm cbundle \<Rightarrow> lnat " where
-"cbLen b \<equiv> if cbDom\<cdot>b \<noteq> {} then (LEAST ln. ln\<in>{(usLen\<cdot>(cbGetCh c\<cdot>b)) | c. c \<in> cbDom\<cdot>b}) else \<infinity>"  
+definition ubLen:: " 'm ubundle \<Rightarrow> lnat " where
+"ubLen b \<equiv> if ubDom\<cdot>b \<noteq> {} then (LEAST ln. ln\<in>{(usLen\<cdot>(ubGetCh c\<cdot>b)) | c. c \<in> ubDom\<cdot>b}) else \<infinity>"  
 
 (* Thats an easy converter. For example from "tstream USB" to "stream USB" *)
 (* Can also be cont *)
-definition cbShift :: "('a \<Rightarrow> 'b) \<Rightarrow> 'a cbundle \<Rightarrow> 'b cbundle" where
-"cbShift f sb = Abs_cbundle (\<lambda>c. ((c\<in>cbDom\<cdot>sb) \<leadsto> f (cbGetCh c\<cdot>sb)))"
+definition ubShift :: "('a \<Rightarrow> 'b) \<Rightarrow> 'a ubundle \<Rightarrow> 'b ubundle" where
+"ubShift f sb = Abs_ubundle (\<lambda>c. ((c\<in>ubDom\<cdot>sb) \<leadsto> f (ubGetCh c\<cdot>sb)))"
 
 
 (****************************************************)
--- UnivClasses.thy
+++ UnivClasses.thy
@@ -7,71 +7,76 @@
 theory UnivClasses
   imports "inc/LNat" "inc/OptionCpo"
 begin
-(* SWS: I prefer PCPO, usbLeast/usbFix are IMPORTANT! *)
+  
+
 default_sort pcpo
 
-
-(* Mockup for the channel datatype *)
-(* This channel is not a CPO... and thats good so *)
+  
+(****************************************************)
+section\<open>Message\<close>
+(****************************************************) 
+  
+  
 datatype channel = c1 | c2
 
-
-(* The Old way *)
 class message = countable +
   fixes ctype :: "channel \<Rightarrow> ('a::type) set" 
 begin
 end
 
 
+(****************************************************)
+section\<open>Universal Stream\<close>
+(****************************************************) 
 
-(* The new way. 
-  us = universal stream *)
+
 (* This class is just the very basic functions required for an Bundle *)
-class us = pcpo +
+class uscl = pcpo +
   fixes usOkay :: "channel \<Rightarrow> 'a \<Rightarrow> bool"
-  fixes usLen :: "'a \<rightarrow> lnat"  (* Debatable *)
-(*  fixes usConc :: "'a \<Rightarrow> 'a \<rightarrow> 'a"  (* Is not really required for a SB... just here for a demo *) *)
+  fixes usLen :: "'a \<rightarrow> lnat"
 
-  assumes usokay_bot: "\<And>c. usOkay c \<bottom>" (* Just an example *)
-(*  assumes "\<And>c. (usOkay c s1 \<Longrightarrow> usOkay c s2 \<Longrightarrow> usOkay c (conc s1\<cdot>s2))" (* Demo that we can set conditions *) *)
-  assumes usokay_adm: "\<And>c. adm (usOkay c)"  (* Should be required by SB-adm proof *)
+  assumes usOkay_bot: "\<And>c. usOkay c \<bottom>"
+  assumes usOkay_adm: "\<And>c. adm (usOkay c)"
 begin
 end
 
 
+(****************************************************)
+section\<open>Universal Stream Bundle\<close>
+(****************************************************) 
+  
 
-
-(* The new way. 
-  usb = universal stream bundle *)
 (* This class is just the very basic functions required for an SPF *)
 default_sort cpo
 
-class usb = cpo +
-  fixes usbDom :: "'a \<rightarrow> channel set"
-  fixes usbLen :: "'a \<Rightarrow> lnat"  (* Debatable *)
-  fixes usbLeast :: "channel set \<Rightarrow> 'a"
+class ubcl = cpo +
+  fixes ubDom :: "'a \<rightarrow> channel set"
+  fixes ubLen :: "'a \<Rightarrow> lnat"  (* Debatable *)
+  fixes ubLeast :: "channel set \<Rightarrow> 'a"
 
-  assumes usbdom_fix: "\<And> x y. x\<sqsubseteq>y \<Longrightarrow> usbDom\<cdot>x = usbDom\<cdot>y"
-  assumes usbdom_least: "\<And> x. usbLeast (usbDom\<cdot>x)\<sqsubseteq>x"
+  assumes ubdom_fix: "\<And> x y. x\<sqsubseteq>y \<Longrightarrow> ubDom\<cdot>x = ubDom\<cdot>y"
+  assumes ubdom_least: "\<And> x. ubLeast (ubDom\<cdot>x)\<sqsubseteq>x"
 begin
 end
 
 
-
+(****************************************************)
+section\<open>Universal Stream Processing Function\<close>
+(****************************************************) 
 
 
 (* First a general class for USPF/USPFw/USPFs *)
-class uspf = cpo +
-  fixes upfDom :: "'a \<rightarrow> channel set"
-  fixes upfRan :: "'a \<rightarrow> channel set"
+class ufuncl = cpo +
+  fixes ufDom :: "'a \<rightarrow> channel set"
+  fixes ufRan :: "'a \<rightarrow> channel set"
 
-  assumes uspfdom_fix: "\<And>x y. x\<sqsubseteq>y \<Longrightarrow> uspfDom\<cdot>x = uspfDom\<cdot>y" 
-  assumes uspfran_fix: "\<And>x y. x\<sqsubseteq>y \<Longrightarrow> uspfRan\<cdot>x = uspfRan\<cdot>y" 
+  assumes ufDom_fix: "\<And>x y. x\<sqsubseteq>y \<Longrightarrow> ufDom\<cdot>x = ufDom\<cdot>y" 
+  assumes ufRan_fix: "\<And>x y. x\<sqsubseteq>y \<Longrightarrow> ufRan\<cdot>x = ufRan\<cdot>y" 
 begin
 end
 
-class uspf_comp = uspf +
-  fixes uspfComp :: "'a \<rightarrow> 'a \<rightarrow> 'a"  (* Here we can put the abbreviation \<otimes> *)
+class ufuncl_comp = ufuncl +
+  fixes ufunclComp :: "'a \<rightarrow> 'a \<rightarrow> 'a"  (* Here we can put the abbreviation \<otimes> *)
 begin
 end
 
