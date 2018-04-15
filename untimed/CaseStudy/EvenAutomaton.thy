28da8339094e74136e679ee1fc2c8bdb9cd9162a
Hendrik <hendrik.kausch@rwth-aachen.de>
2018-04-15 10:16:55 +0200

    changed structure so that EvenStream and EvenAutomatonSPF use the same datatypes, also changed rek2evenstream to work with new definition of EvenStream and removed type assumption from rek2evenstream

--- untimed/CaseStudy/EvenAutomaton.thy
+++ untimed/CaseStudy/EvenAutomaton.thy
@@ -1,22 +1,9 @@
 theory EvenAutomaton
 
-imports Automaton
+imports Automaton "../../timesyn/tsynStream" "../../timesyn/tsynBundle"
 
 begin
 
-
-(* BEGIN: tsynBundle *)
-  (* Copied here, because importing leads to an ML-Error *)
-  (* TODO: import it ! ! ! *)
-lift_definition tsynbOneTick:: "channel \<Rightarrow> 'm::message event SB" is
-"\<lambda>c. [c \<mapsto> \<up>Tick]"
-  unfolding ubWell_def
-  unfolding usclOkay_stream_def
-  unfolding ctype_event_def
-  by simp
-
-lemma tsynbonetick_dom [simp]: "ubDom\<cdot>(tsynbOneTick c) = {c}"
-  by (simp add: tsynbOneTick.rep_eq ubdom_insert)
 
 (* END: tsynBundle *)
 
--- untimed/CaseStudy/EvenAutomatonProof.thy
+++ untimed/CaseStudy/EvenAutomatonProof.thy
@@ -3,41 +3,39 @@ theory EvenAutomatonProof
 imports EvenAutomaton EvenStream
 
 begin
+  
 lemma evenStreamBundle_well[simp]:assumes "sdom\<cdot>s \<subseteq> ctype c" shows "ubWell ([c \<mapsto> s::EvenAutomaton event stream])"
 by(simp add: ubWell_def usclOkay_stream_def assms)
 
 
 definition EvenStream::"EvenAutomatonState \<Rightarrow> nat event stream \<rightarrow> EvenAutomaton event stream" where
-"EvenStream state \<equiv> (\<Lambda> s. ((h EvenAutomatonAutomaton state) \<rightleftharpoons> (Abs_ubundle [c1 \<mapsto> nat2even s])) . c2)"
+"EvenStream state \<equiv> (\<Lambda> s. ((h EvenAutomatonAutomaton state) \<rightleftharpoons> (Abs_ubundle [c1 \<mapsto> (nat2even\<cdot>s)])) . c2)"
 
-fun evenMakeSubstate :: "bool \<Rightarrow> EvenAutomatonSubstate" where
-"evenMakeSubstate True = Even" | 
-"evenMakeSubstate False = Odd"
 
-lemma evenStream_insert:"EvenStream state\<cdot>s = ((h EvenAutomatonAutomaton state) \<rightleftharpoons> (Abs_ubundle [c1 \<mapsto> s])) . c2"
+lemma evenStream_insert:"EvenStream state\<cdot>s = ((h EvenAutomatonAutomaton state) \<rightleftharpoons> (Abs_ubundle [c1 \<mapsto> (nat2even\<cdot>s)])) . c2"
   apply(simp add: EvenStream_def,rule beta_cfun)
 proof(rule Cont.contI2)
-  show"monofun (\<lambda>x::EvenAutomaton event stream. (h EvenAutomatonAutomaton state \<rightleftharpoons> Abs_ubundle [c1 \<mapsto> x])  .  c2)"
+  show"monofun (\<lambda>x::nat event stream. (h EvenAutomatonAutomaton state \<rightleftharpoons> Abs_ubundle [c1 \<mapsto> nat2even\<cdot>x])  .  c2)"
    apply(rule monofunI)
     sorry
 next
-  fix Y:: "nat \<Rightarrow>EvenAutomaton event stream"
+  fix Y:: "nat \<Rightarrow>nat event stream"
   assume a1: "chain Y"
-  assume a2:"chain (\<lambda>i::nat. (h EvenAutomatonAutomaton state \<rightleftharpoons> Abs_ubundle [c1 \<mapsto> Y i])  .  c2)"
-  have "\<And>i. sdom\<cdot>(Y i) \<subseteq> ctype c1"
+  assume a2:"chain (\<lambda>i::nat. (h EvenAutomatonAutomaton state \<rightleftharpoons> Abs_ubundle [c1 \<mapsto> nat2even\<cdot>(Y i)])  .  c2)"
+  have "\<And>i. sdom\<cdot>(nat2even\<cdot>(Y i)) \<subseteq> ctype c1"
     sorry
-  have h1:"(Abs_ubundle [c1 \<mapsto> \<Squnion>i. Y i]) = (\<Squnion>i. Abs_ubundle [c1 \<mapsto>  Y i])"
+  have h1:"(Abs_ubundle [c1 \<mapsto> nat2even\<cdot>(\<Squnion>i. Y i)]) = (\<Squnion>i. Abs_ubundle [c1 \<mapsto>  nat2even\<cdot>(Y i)])"
     sorry
-  have h2:"chain (\<lambda>i::nat. Abs_ubundle [c1 \<mapsto> Y i])"
+  have h2:"chain (\<lambda>i::nat. Abs_ubundle [c1 \<mapsto> nat2even\<cdot>(Y i)])"
     sorry
-  show "(h EvenAutomatonAutomaton state \<rightleftharpoons> Abs_ubundle [c1 \<mapsto> \<Squnion>i::nat. Y i])  .  c2 \<sqsubseteq> 
-          (\<Squnion>i::nat. (h EvenAutomatonAutomaton state \<rightleftharpoons> Abs_ubundle [c1 \<mapsto> Y i])  .  c2)"
+  show "(h EvenAutomatonAutomaton state \<rightleftharpoons> Abs_ubundle [c1 \<mapsto> nat2even\<cdot>(\<Squnion>i::nat. Y i)])  .  c2 \<sqsubseteq>
+       (\<Squnion>i::nat. (h EvenAutomatonAutomaton state \<rightleftharpoons> Abs_ubundle [c1 \<mapsto> nat2even\<cdot>(Y i)])  .  c2)"
     apply(simp add: h1, subst contlub_cfun_arg, simp add: h2)
     sorry
 qed
   
 
-lemma  msg_assms: "EvenStream (State ooo summe)\<cdot>(\<up>(Msg (A m)) \<bullet> xs)
+lemma  msg_assms: "EvenStream (State ooo summe)\<cdot>(\<up>(Msg m) \<bullet> xs)
                  = \<up>(Msg (B (Parity.even (summe + m)))) \<bullet> (EvenStream (State (evenMakeSubstate (Parity.even (summe + m)))  (summe + m))\<cdot>xs)"
   sorry
 (*  proof(induction arbitrary: ooo summe rule: tsyn_ind [of _xs])
@@ -55,8 +53,7 @@ lemma  msg_assms: "EvenStream (State ooo summe)\<cdot>(\<up>(Msg (A m)) \<bullet
   qed*)
 
 lemma tick_assms: "EvenStream state\<cdot>(\<up>Tick \<bullet> xs) = \<up>Tick \<bullet> (f state\<cdot>xs)"
-  sorry
-(*proof(induction rule: tsyn_ind [of _xs])
+proof(induction rule: tsyn_ind [of _xs])
   case 1
   then show ?case by simp
 next
@@ -68,7 +65,7 @@ next
 next
   case (4 s)
   then show ?case sorry
-qed*)
+qed
       
 lemma bot_assms: "EvenStream state\<cdot>\<bottom> = \<bottom>"
   apply(simp add: evenStream_insert)
@@ -76,6 +73,7 @@ lemma bot_assms: "EvenStream state\<cdot>\<bottom> = \<bottom>"
   apply(simp add: getDom_def EvenAutomatonAutomaton.rep_eq)
   apply(subst ubgetch_ubrep_eq, auto)
   by(simp add: getRan_def EvenAutomatonAutomaton.rep_eq ubclLeast_ubundle_def)
+   
     
 end
   
