54805bad22da3d24cb49a3b463414923ddd8da23
sokrutu <loler47@hotmail.com>
2018-04-30 16:22:12 +0200

    Definition und dom-Lemma. Aber noch ein Problem "Pending sort hypotheses: ubcl_comp"

--- USpec.thy
+++ USpec.thy
@@ -2,11 +2,11 @@
     Author:       Stüber, Jens, Marc
     e-mail:       sebastian.stueber@rwth-aachen.de
 
-    Description:  Defines "universal spezification" 
+    Description:  Defines "universal specification" 
 *)
 
 theory USpec
-  imports UnivClasses
+  imports UnivClasses UFun_Comp (* TODO ufclLeast bräuchte ich, dann kann das UFun weg *)
 begin
 
 default_sort ufuncl
@@ -232,5 +232,39 @@ lemma uspec_ran_eq: assumes "f \<in> (inv Rev) (Rep_uspec S)" shows "ufclRan\<cd
 
 lemma uspec_ran_eq2: assumes "uspecIsConsistent S" shows "\<forall> f \<in> Rep_rev_uspec S. ufclRan\<cdot>f = uspecRan S"
   by (simp add: uspec_ran_eq)
+
+
+
+(* TODO Welche Theory und welche Section? *)
+
+definition uspecLeast :: "channel set \<Rightarrow> channel set \<Rightarrow> 'm uspec" where
+"uspecLeast cin cout = Abs_uspec (Rev {f. ufclDom\<cdot>f = cin \<and> ufclRan\<cdot>f = cout})"
+
+lemma uspecLeast_well: "uspecWell {f. ufclDom\<cdot>f = cin \<and> ufclRan\<cdot>f = cout}"
+  sorry
+
+lemma uspecLeast_elem: "ufLeast cin cout \<in> Rep_rev_uspec(uspecLeast cin cout)"
+  sorry
+
+lemma uspecLeast_consistent: "uspecIsConsistent (uspecLeast cin cout)"
+  sorry
+
+lemma ver: "\<forall>f \<in> {f. ufclDom\<cdot>f = cin \<and> ufclRan\<cdot>f = cout}. ufclDom\<cdot>f = cin"
+  by auto
+
+lemma uspecLeast_dom: "uspecDom (uspecLeast cin cout) = cin" (is "?L = ?R")
+  proof -
+    have a1: "?L = ufclDom\<cdot>(SOME f::'a. ufclDom\<cdot>f = cin \<and> ufclRan\<cdot>f = cout)"
+      apply(simp add: uspecDom_def)
+      apply(simp add: uspecLeast_def)
+      apply(simp only: uspecLeast_well rep_abs_uspec)
+      by(simp add: inv_rev_rev)
+    moreover have "ufclDom\<cdot>(SOME f::'a. ufclDom\<cdot>f = cin \<and> ufclRan\<cdot>f = cout) = ufclDom\<cdot>(ufLeast cin cout)"
+      by (metis (mono_tags, lifting) a1 rep_abs_rev_simp ufclDom_ufun_def ufleast_ufdom uspecLeast_consistent uspecLeast_def uspecLeast_well uspec_consist_f_ex uspec_dom_eq2 ver)
+    moreover have "ufclDom\<cdot>(ufLeast cin cout) = cin"
+      by (simp add: ufclDom_ufun_def)
+    ultimately show ?thesis
+      by blast
+  qed
 
 end
