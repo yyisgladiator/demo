3e6b7b81f44d5aa0aaaef0cdd41daf0de05141bb
Dmitry Manuilov <dmitry.manuilov@rwth-aachen.de>
2018-05-18 15:39:05 +0200

    Replace USpec.thy

--- USpec.thy
+++ USpec.thy
@@ -63,6 +63,7 @@ abbreviation Rep_rev_uspec:: "'m uspec \<Rightarrow> 'm set" where
 abbreviation Abs_rev_uspec:: "'m set \<Rightarrow> 'm uspec" where
 "Abs_rev_uspec spec \<equiv> Abs_uspec (Rev spec)"
 
+
 definition uspecDom :: "'m uspec \<Rightarrow> channel set" where
 "uspecDom S = ufclDom\<cdot>(SOME f. f\<in>  ((inv Rev) (Rep_uspec S)))"
 
@@ -231,56 +232,5 @@ lemma uspec_ran_eq: assumes "f \<in> (inv Rev) (Rep_uspec S)" shows "ufclRan\<cd
 
 lemma uspec_ran_eq2: assumes "uspecIsConsistent S" shows "\<forall> f \<in> Rep_rev_uspec S. ufclRan\<cdot>f = uspecRan S"
   by (simp add: uspec_ran_eq)
-
-lemma revBelowNeqSubset: "\<And>A:: 'a set rev. \<forall>B:: 'a set rev. A \<sqsubseteq> B \<longleftrightarrow> (inv Rev B \<subseteq> inv Rev A)"
-  by (smt SetPcpo.less_set_def below_rev.elims(2) below_rev.elims(3) inv_rev_rev)
-
-lemma SLEI_help1:  "\<And>Y::nat \<Rightarrow> 'a set rev. 
-  chain Y \<Longrightarrow> Rev (\<Inter>{x. \<exists>i. x = inv Rev (Y i)}) \<sqsubseteq> (\<Squnion>i. Y i)" 
-proof -
-fix Y :: "nat \<Rightarrow> 'a set rev"
-  assume a1: "chain Y"
-  obtain AA :: "'a set set \<Rightarrow> 'a set \<Rightarrow> 'a set" where
-    "\<forall>x0 x1. (\<exists>v2. v2 \<in> x0 \<and> \<not> x1 \<subseteq> v2) = (AA x0 x1 \<in> x0 \<and> \<not> x1 \<subseteq> AA x0 x1)"
-  by moura
-  then have f2: "(\<not> inv Rev (Lub Y) \<subseteq> \<Inter>{A. \<exists>n. A = inv Rev (Y n)} \<or> 
-    (\<forall>A. A \<notin> {A. \<exists>n. A = inv Rev (Y n)} \<or> inv Rev (Lub Y) \<subseteq> A)) \<and> (inv Rev (Lub Y) \<subseteq> 
-    \<Inter>{A. \<exists>n. A = inv Rev (Y n)} \<or> AA {A. \<exists>n. A = inv Rev (Y n)} (inv Rev (Lub Y)) \<in> 
-    {A. \<exists>n. A = inv Rev (Y n)} \<and> \<not> inv Rev (Lub Y) \<subseteq> AA {A. \<exists>n. A = inv Rev (Y n)} 
-    (inv Rev (Lub Y)))"
-  by (meson le_Inf_iff)
-  obtain nn :: "'a set \<Rightarrow> nat" where
-    f3: "((\<nexists>n. AA {A. \<exists>n. A = inv Rev (Y n)} (inv Rev (Lub Y)) = inv Rev (Y n)) \<or> 
-    AA {A. \<exists>n. A = inv Rev (Y n)} (inv Rev (Lub Y)) = inv Rev (Y (nn (AA {A. \<exists>n. A = inv Rev (Y n)} 
-    (inv Rev (Lub Y)))))) \<and> ((\<exists>n. AA {A. \<exists>n. A = inv Rev (Y n)} (inv Rev (Lub Y)) = inv Rev (Y n)) \<or> 
-    (\<forall>n. AA {A. \<exists>n. A = inv Rev (Y n)} (inv Rev (Lub Y)) \<noteq> inv Rev (Y n)))"
-    by meson
-  { assume "AA {A. \<exists>n. A = inv Rev (Y n)} (inv Rev (Lub Y)) \<noteq> 
-    inv Rev (Y (nn (AA {A. \<exists>n. A = inv Rev (Y n)} (inv Rev (Lub Y)))))"
-    then have "inv Rev (Lub Y) \<subseteq> \<Inter>{A. \<exists>n. A = inv Rev (Y n)}"
-  using f3 f2 by auto }
-  then have "inv Rev (Lub Y) \<subseteq> \<Inter>{A. \<exists>n. A = inv Rev (Y n)}"
-using a1 is_ub_thelub revBelowNeqSubset by blast
-  then show "Rev (\<Inter>{A. \<exists>n. A = inv Rev (Y n)}) \<sqsubseteq> (\<Squnion>n. Y n)"
-    by (simp add: inv_rev_rev revBelowNeqSubset)
-qed
-
-lemma SLEI_help2:  "\<And>Y::nat \<Rightarrow> 'a set rev. 
-  chain Y \<Longrightarrow> (\<Squnion>i. Y i) \<sqsubseteq> Rev (\<Inter>{x. \<exists>i. x = inv Rev (Y i)})"
-proof -
-  have a0: "\<And>Y::nat \<Rightarrow> 'a set rev. 
-  chain Y \<Longrightarrow>{y} \<subseteq> (\<Inter>{x. \<exists>i. x = inv Rev (Y i)}) \<longrightarrow> (\<forall>z. (Y z) \<sqsubseteq> Rev {y})"
-    by (metis (mono_tags, lifting) CollectI revBelowNeqSubset insert_subset inv_rev_rev mem_simps(11)
-      rev_bot_top set_cpo_simps(3))
-  have a1:  "\<And>Y::nat \<Rightarrow> 'a set rev. chain Y \<Longrightarrow> (\<forall>z. (Y z) \<sqsubseteq> Rev {y}) \<longrightarrow> (\<Squnion>i. Y i) \<sqsubseteq> Rev {y}"
-    by (simp add: lub_below)
-  show  "\<And>Y::nat \<Rightarrow> 'a set rev. 
-  chain Y \<Longrightarrow> (\<Squnion>i. Y i) \<sqsubseteq> Rev (\<Inter>{x. \<exists>i. x = inv Rev (Y i)})"
-    by (smt CollectI below_rev.elims(3) inv_rev_rev le_Inf_iff lub_below order_refl set_cpo_simps(1))
-qed
-
-lemma setrevLubEqInter:  "\<And>Y::nat \<Rightarrow> 'a set rev. 
-  chain Y \<Longrightarrow> (\<Squnion>i. Y i) = Rev (\<Inter>{x. \<exists>i. x = inv Rev (Y i)})"
-  using SLEI_help1 SLEI_help2 po_eq_conv by blast                               
 
 end
