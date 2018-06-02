b5d98c27691afee98ba081a4a3c14ebeb2ac9c90
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2018-06-02 19:34:09 +0200

    Some minor adjustments.

--- timesyn/tsynBundle.thy
+++ timesyn/tsynBundle.thy
@@ -18,22 +18,24 @@ default_sort message
   section {* tsynbNull - Automaton *}
 (* ----------------------------------------------------------------------- *)
 
+(* ToDo: add descriptions. *)
+
 lift_definition tsynbNull :: "channel \<Rightarrow> 'm tsyn SB" is
   "\<lambda>c. [c \<mapsto> \<up>null]"
   by (simp add: ubWell_def usclOkay_stream_def ctype_tsyn_def)
 
-lemma tsynnullsb_ubdom [simp]: "ubDom\<cdot>(tsynbNull c) = {c}"
+lemma tsynbnull_ubdom [simp]: "ubDom\<cdot>(tsynbNull c) = {c}"
   by (simp add:tsynbNull.rep_eq ubdom_insert)
 
-lemma tsynnullsb_ubgetch [simp]: "tsynbNull c  .  c = \<up>null"
+lemma tsynbnull_ubgetch [simp]: "tsynbNull c  .  c = \<up>null"
   by (simp add: tsynbNull.rep_eq ubgetch_insert)
 
-lemma tsynnullsb_ubconc [simp]:
-  assumes "c \<in> ubDom\<cdot>sb" 
+lemma tsynbnull_ubconc [simp]:
+  assumes "c \<in> ubDom\<cdot>sb"
   shows "ubConc (tsynbNull c)\<cdot>sb  .  c = \<up>null \<bullet> (sb  .  c)"
   by (simp add: assms ubConc_usclConc_eq usclConc_stream_def)
     
-lemma tsynnullsb_ubconc_sbrt [simp]:
+lemma tsynbnull_ubconc_sbrt [simp]:
   assumes "ubDom\<cdot>sb = {c}"
   shows "sbRt\<cdot>(ubConc (tsynbNull c)\<cdot>sb) = sb"
   apply (rule ub_eq)
