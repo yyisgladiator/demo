93a04e9896e360aca451451d907f37ed09fc2fa5
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2018-06-03 18:59:16 +0200

    Tried a few things. #120

--- timesyn/CaseStudies/ABP/Components.thy
+++ timesyn/CaseStudies/ABP/Components.thy
@@ -12,9 +12,9 @@ imports "../../tsynStream"
 
 begin
 
-(*
-fun tsynRecProj :: "('a \<times> bool) tsyn discr \<Rightarrow> 'a tsyn discr" where
-  "tsynRecProj (Discr (Msg (m, b))) = null" |
+(* ToDo: not cont.
+fun tsynRecProj :: "('a \<times> bool) tsyn discr \<rightarrow> 'a tsyn discr" where
+  "tsynRecProj (Discr (Msg (m, b))) = Discr (Msg m)" |
   "tsynRecProj (Discr null) = Discr null"
 
 fixrec tsynRec_h :: "('a \<times> bool) tsyn stream \<rightarrow> bool \<rightarrow> 'a tsyn stream" where
@@ -22,7 +22,7 @@ fixrec tsynRec_h :: "('a \<times> bool) tsyn stream \<rightarrow> bool \<rightar
   "tsynRec_h\<cdot>(up\<cdot>a && as)\<cdot>bit = (
      if (undiscr a) = null then (up\<cdot>(Discr null)) && tsynRec_h\<cdot>as\<cdot>bit
      else (
-       if bit = snd (invMsg (undiscr a)) then up\<cdot>a &&  tsynRec_h\<cdot>as\<cdot>True
+       if bit = snd (invMsg (undiscr a)) then up\<cdot>(tsynRecProj a) &&  tsynRec_h\<cdot>as\<cdot>True
        else tsynRec_h\<cdot>as\<cdot>True
      )
   )"
@@ -34,23 +34,6 @@ rec b \<epsilon> = (\<epsilon>, \<epsilon>)
 rec b (- \<bullet> dats) = (- \<bullet> bits, - \<bullet> msgs) where (bits, msgs) = rec(b, dats)
 rec b ((m, b) \<bullet> dats) = (b \<bullet> bits, m \<bullet> msgs) where (bits, msgs) = rec(\<not>b, dats)
 rec b ((m, \<not>b) \<bullet> dats) = (\<not>b \<bullet> bits, - \<bullet> msgs) where (bits, msgs) = rec(b, dats)
-
-|
-  "tsynRemDups_h\<cdot>(up\<cdot>a && as)\<cdot>None = (
-     if (undiscr a) = null then up\<cdot>a && tsynRemDups_h\<cdot>as\<cdot>None
-     else up\<cdot>a && tsynRemDups_h\<cdot>as\<cdot>(Some a)
-  )" |
-  "tsynRemDups_h\<cdot>(up\<cdot>a && as)\<cdot>(Some b) = (
-     if a = b then up\<cdot>(Discr null) && tsynRemDups_h\<cdot>as\<cdot>(Some b)
-     else up\<cdot>a && tsynRemDups_h\<cdot>as\<cdot>(Some a)
-  )"
-
-lemma tsynRemDups_h_sconc_msg: assumes "a \<noteq> null"
-  shows " tsynRemDups_h\<cdot>(\<up>(Msg a) \<bullet> as)\<cdot>None = \<up>(Msg a) \<bullet> tsynRemDups_h\<cdot>as\<cdot>(Some (Discr (Msg a)))"
-  by (metis lscons_conv tsyn.distinct(1) tsynRemDups_h.simps(2) undiscr_Discr)
-
-lemma tsynRemDups_h_sconc_null: "tsynRemDups_h\<cdot>(\<up>null \<bullet> as)\<cdot>None = \<up>null \<bullet> tsynRemDups_h\<cdot>as\<cdot>None"
-  by (fold lscons_conv, simp)
 *)
 
 end
