2ac7667bec2bfeb49160357590c49bcfd6092b2c
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-06-06 17:23:22 +0200

    SPS/Medium_SPS file

---
+++ untimed/SPS.thy
@@ -0,0 +1,20 @@
+theory SPS
+
+imports SPF "../USpec"
+
+begin
+
+default_sort message
+
+type_synonym 'm SPS = "'m SPF uspec"
+
+
+
+section \<open>Definition\<close>
+  (* TODO *)
+
+section \<open>Lemma\<close>
+  (* TODO *)
+
+
+end
