7d6f4acaf2b8d2effd0b0e8dcbf21a21046cc0b5
cody <sgreber@bitsolve.de>
2018-06-08 11:39:39 +0200

    new sender

--- timesyn/CaseStudies/ABP/SenderAutomaton.thy
+++ timesyn/CaseStudies/ABP/SenderAutomaton.thy
@@ -8,31 +8,28 @@ fun prepend:: "nat list \<Rightarrow> nat \<Rightarrow> nat list" where
 "prepend xs x= x#xs"
 
 (* This are the actual states from MAA *)
-datatype SenderSubstate = St | Sf
+datatype SenderSubstate = Sf | St
 
 (* And these have also the variables *)
-datatype SenderState = State SenderSubstate "nat" "nat list"
+datatype SenderState = State SenderSubstate "nat list"
 
 fun getSubState :: "SenderState \<Rightarrow> SenderSubstate" where
-    "getSubState (State state_s automaton_c automaton_buffer) = state_s"
+    "getSubState (State state_s automaton_buffer) = state_s"
 
-fun getC :: "SenderState \<Rightarrow> nat" where
-"getC (State _ automaton_c automaton_buffer) = automaton_c"
-     
 fun getBuffer :: "SenderState \<Rightarrow> nat list" where
-"getBuffer (State _ automaton_c automaton_buffer) = automaton_buffer"
+"getBuffer (State _ automaton_buffer) = automaton_buffer"
     
 
-datatype Sender = B "nat" | A "bool" | C "(nat\<times>bool)"
+datatype Sender = A "nat" | B "bool" | C "(nat\<times>bool)"
 instance Sender :: countable
 apply(intro_classes)
 by(countable_datatype)
 
-abbreviation input_as_c1 :: "channel" ("\<guillemotright>as") where
-"\<guillemotright>as \<equiv> c1"
+abbreviation input_i_c1 :: "channel" ("\<guillemotright>i") where
+"\<guillemotright>i \<equiv> c1"
 
-abbreviation input_i_c2 :: "channel" ("\<guillemotright>i") where
-"\<guillemotright>i \<equiv> c2"
+abbreviation input_as_c2 :: "channel" ("\<guillemotright>as") where
+"\<guillemotright>as \<equiv> c2"
 
 abbreviation output_ds_c3 :: "channel" ("ds\<guillemotright>") where
 "ds\<guillemotright> \<equiv> c3"
@@ -40,8 +37,8 @@ abbreviation output_ds_c3 :: "channel" ("ds\<guillemotright>") where
 instantiation Sender :: message
 begin
 fun ctype_Sender :: "channel  \<Rightarrow> Sender set" where
-    "ctype_Sender \<guillemotright>as = range A" | 
-    "ctype_Sender \<guillemotright>i = range B" | 
+    "ctype_Sender \<guillemotright>i = range A" | 
+    "ctype_Sender \<guillemotright>as = range B" | 
     "ctype_Sender ds\<guillemotright> = range C" 
 instance
 by(intro_classes)
@@ -56,66 +53,58 @@ by simp
 
 
 fun senderTransitionH :: "(SenderState \<times> (Sender tsyn \<times> Sender tsyn)) \<Rightarrow> (SenderState \<times> Sender tsyn SB)" where
-    "senderTransitionH (State Sf automaton_c automaton_buffer, (Msg (A a), Msg (B b))) = 
-       (if((size automaton_buffer)=1 \<and> a=False) then ((State St (2) ((prepend (butlast automaton_buffer))b),(createDsOutput (Pair b True ))))
-       else if((size automaton_buffer)>1 \<and> a=False) then ((State St (2) ((prepend (butlast automaton_buffer))b),(createDsOutput (Pair (last (butlast automaton_buffer)) True ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c>0 \<and> a=True) then ((State Sf (automaton_c-1) ((prepend automaton_buffer)b),(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)>0 \<and> automaton_c=0 \<and> a=True) then ((State Sf (2) ((prepend automaton_buffer)b),(createDsOutput (Pair (last automaton_buffer) False ))))
-       else if((size automaton_buffer)=0) then ((State Sf (2) ((prepend automaton_buffer)b),(createDsOutput (Pair b False ))))
-       else  (State Sf automaton_c automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
+    "senderTransitionH (State Sf automaton_buffer, (Msg (A a), Msg (B b))) = 
+       (if((size automaton_buffer)>0 \<and> b=True) then ((State Sf ((prepend automaton_buffer)a),(createDsOutput (Pair (last automaton_buffer) False ))))
+       else if((size automaton_buffer)>1 \<and> b=False) then ((State St ((prepend (butlast automaton_buffer))a),(createDsOutput (Pair (last (butlast automaton_buffer)) True ))))
+       else if((size automaton_buffer)=1 \<and> b=False) then ((State St ((prepend (butlast automaton_buffer))a),(createDsOutput (Pair a True ))))
+       else if((size automaton_buffer)=0) then ((State Sf ((prepend automaton_buffer)a),(createDsOutput (Pair a False ))))
+       else  (State Sf automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
 
-    "senderTransitionH (State Sf automaton_c automaton_buffer, (null, Msg (B b))) = 
-       (if((size automaton_buffer)>0 \<and> automaton_c=0) then ((State Sf (2) ((prepend automaton_buffer)b),(createDsOutput (Pair (last automaton_buffer) False ))))
-       else if((size automaton_buffer)=0) then ((State Sf (2) ((prepend automaton_buffer)b),(createDsOutput (Pair b False ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c>0) then ((State Sf (automaton_c-1) ((prepend automaton_buffer)b),(tsynbNull ds\<guillemotright>)))
-       else  (State Sf automaton_c automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
+    "senderTransitionH (State Sf automaton_buffer, (null, Msg (B b))) = 
+       (if((size automaton_buffer)>0 \<and> b=True) then ((State Sf automaton_buffer,(createDsOutput (Pair (last automaton_buffer) False ))))
+       else if((size automaton_buffer)>1 \<and> b=False) then ((State St ((butlast automaton_buffer)),(createDsOutput (Pair (last (butlast automaton_buffer)) True ))))
+       else if((size automaton_buffer)=0) then ((State Sf automaton_buffer,(tsynbNull ds\<guillemotright>)))
+       else if((size automaton_buffer)=1 \<and> b=False) then ((State St ((butlast automaton_buffer)),(tsynbNull ds\<guillemotright>)))
+       else  (State Sf automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
 
-    "senderTransitionH (State Sf automaton_c automaton_buffer, (Msg (A a), null)) = 
-       (if((size automaton_buffer)=0) then ((State Sf automaton_c automaton_buffer,(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)=1 \<and> a=False) then ((State St automaton_c ((butlast automaton_buffer)),(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)>1 \<and> a=False) then ((State St (2) ((butlast automaton_buffer)),(createDsOutput (Pair (last (butlast automaton_buffer)) True ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c=0 \<and> a=True) then ((State Sf (2) automaton_buffer,(createDsOutput (Pair (last automaton_buffer) False ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c>0 \<and> a=True) then ((State Sf (automaton_c-1) automaton_buffer,(tsynbNull ds\<guillemotright>)))
-       else  (State Sf automaton_c automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
+    "senderTransitionH (State Sf automaton_buffer, (Msg (A a), null)) = 
+       (if((size automaton_buffer)=0) then ((State Sf ((prepend automaton_buffer)a),(createDsOutput (Pair a False ))))
+       else if((size automaton_buffer)>0) then ((State Sf ((prepend automaton_buffer)a),(createDsOutput (Pair (last automaton_buffer) False ))))
+       else  (State Sf automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
 
-    "senderTransitionH (State Sf automaton_c automaton_buffer, (null, null)) = 
-       (if((size automaton_buffer)>0 \<and> automaton_c=0) then ((State Sf (2) automaton_buffer,(createDsOutput (Pair (last automaton_buffer) False ))))
-       else if((size automaton_buffer)=0) then ((State Sf automaton_c automaton_buffer,(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)>0 \<and> automaton_c>0) then ((State Sf (automaton_c-1) automaton_buffer,(tsynbNull ds\<guillemotright>)))
-       else  (State Sf automaton_c automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
+    "senderTransitionH (State Sf automaton_buffer, (null, null)) = 
+       (if((size automaton_buffer)>0) then ((State Sf automaton_buffer,(createDsOutput (Pair (last automaton_buffer) False ))))
+       else if((size automaton_buffer)=0) then ((State Sf automaton_buffer,(tsynbNull ds\<guillemotright>)))
+       else  (State Sf automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
 
-    "senderTransitionH (State St automaton_c automaton_buffer, (Msg (A a), Msg (B b))) = 
-       (if((size automaton_buffer)=1 \<and> a=True) then ((State Sf (2) ((prepend (butlast automaton_buffer))b),(createDsOutput (Pair b False ))))
-       else if((size automaton_buffer)=0) then ((State St (2) ((prepend automaton_buffer)b),(createDsOutput (Pair b True ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c=0 \<and> a=False) then ((State St (2) ((prepend automaton_buffer)b),(createDsOutput (Pair (last automaton_buffer) True ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c>0 \<and> a=False) then ((State St (automaton_c-1) ((prepend automaton_buffer)b),(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)>1 \<and> a=True) then ((State Sf (2) ((prepend (butlast automaton_buffer))b),(createDsOutput (Pair (last (butlast automaton_buffer)) False ))))
-       else  (State St automaton_c automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
+    "senderTransitionH (State St automaton_buffer, (Msg (A a), Msg (B b))) = 
+       (if((size automaton_buffer)>0 \<and> b=False) then ((State St ((prepend automaton_buffer)a),(createDsOutput (Pair (last automaton_buffer) True ))))
+       else if((size automaton_buffer)=1 \<and> b=True) then ((State Sf ((prepend (butlast automaton_buffer))a),(createDsOutput (Pair a False ))))
+       else if((size automaton_buffer)=0) then ((State St ((prepend automaton_buffer)a),(createDsOutput (Pair a True ))))
+       else if((size automaton_buffer)>1 \<and> b=True) then ((State Sf ((prepend (butlast automaton_buffer))a),(createDsOutput (Pair (last (butlast automaton_buffer)) False ))))
+       else  (State St automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
 
-    "senderTransitionH (State St automaton_c automaton_buffer, (null, Msg (B b))) = 
-       (if((size automaton_buffer)=0) then ((State St (2) ((prepend automaton_buffer)b),(createDsOutput (Pair b True ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c=0) then ((State St (2) ((prepend automaton_buffer)b),(createDsOutput (Pair (last automaton_buffer) True ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c>0) then ((State St (automaton_c-1) ((prepend automaton_buffer)b),(tsynbNull ds\<guillemotright>)))
-       else  (State St automaton_c automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
+    "senderTransitionH (State St automaton_buffer, (null, Msg (B b))) = 
+       (if((size automaton_buffer)=0) then ((State St automaton_buffer,(tsynbNull ds\<guillemotright>)))
+       else if((size automaton_buffer)>1 \<and> b=True) then ((State Sf ((butlast automaton_buffer)),(createDsOutput (Pair (last (butlast automaton_buffer)) False ))))
+       else if((size automaton_buffer)=1 \<and> b=True) then ((State Sf ((butlast automaton_buffer)),(tsynbNull ds\<guillemotright>)))
+       else if((size automaton_buffer)>0 \<and> b=False) then ((State St automaton_buffer,(createDsOutput (Pair (last automaton_buffer) True ))))
+       else  (State St automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
 
-    "senderTransitionH (State St automaton_c automaton_buffer, (Msg (A a), null)) = 
-       (if((size automaton_buffer)>0 \<and> automaton_c>0 \<and> a=False) then ((State St (automaton_c-1) automaton_buffer,(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)=0) then ((State St automaton_c automaton_buffer,(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)=1 \<and> a=True) then ((State Sf automaton_c ((butlast automaton_buffer)),(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)>1 \<and> a=True) then ((State Sf (2) ((butlast automaton_buffer)),(createDsOutput (Pair (last (butlast automaton_buffer)) False ))))
-       else if((size automaton_buffer)>0 \<and> automaton_c=0 \<and> a=False) then ((State St (2) automaton_buffer,(createDsOutput (Pair (last automaton_buffer) True ))))
-       else  (State St automaton_c automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
+    "senderTransitionH (State St automaton_buffer, (Msg (A a), null)) = 
+       (if((size automaton_buffer)>0) then ((State St ((prepend automaton_buffer)a),(createDsOutput (Pair (last automaton_buffer) True ))))
+       else if((size automaton_buffer)=0) then ((State St ((prepend automaton_buffer)a),(createDsOutput (Pair a True ))))
+       else  (State St automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  |
 
-    "senderTransitionH (State St automaton_c automaton_buffer, (null, null)) = 
-       (if((size automaton_buffer)>0 \<and> automaton_c>0) then ((State St (automaton_c-1) automaton_buffer,(tsynbNull ds\<guillemotright>)))
-       else if((size automaton_buffer)>0 \<and> automaton_c=0) then ((State St (2) automaton_buffer,(createDsOutput (Pair (last automaton_buffer) True ))))
-       else if((size automaton_buffer)=0) then ((State St automaton_c automaton_buffer,(tsynbNull ds\<guillemotright>)))
-       else  (State St automaton_c automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  
+    "senderTransitionH (State St automaton_buffer, (null, null)) = 
+       (if((size automaton_buffer)>0) then ((State St automaton_buffer,(createDsOutput (Pair (last automaton_buffer) True ))))
+       else if((size automaton_buffer)=0) then ((State St automaton_buffer,(tsynbNull ds\<guillemotright>)))
+       else  (State St automaton_buffer, ((tsynbNull ds\<guillemotright>))))"  
 
 fun senderTransition :: "(SenderState \<times> (channel \<rightharpoonup> Sender tsyn)) \<Rightarrow> (SenderState \<times> Sender tsyn SB)" where
-"senderTransition (s,f) = (if dom(f) = {\<guillemotright>as,\<guillemotright>i} then senderTransitionH (s,(f\<rightharpoonup>\<guillemotright>as,f\<rightharpoonup>\<guillemotright>i)) else undefined)"
+"senderTransition (s,f) = (if dom(f) = {\<guillemotright>i,\<guillemotright>as} then senderTransitionH (s,(f\<rightharpoonup>\<guillemotright>i,f\<rightharpoonup>\<guillemotright>as)) else undefined)"
 
-lift_definition SenderAutomaton :: "(SenderState, Sender tsyn) automaton" is "(senderTransition, State Sf 0 [], (tsynbNull ds\<guillemotright>), {c1,c2}, {c3})"
+lift_definition SenderAutomaton :: "(SenderState, Sender tsyn) automaton" is "(senderTransition, State Sf [], (tsynbNull ds\<guillemotright>), {c1,c2}, {c3})"
 sorry
 
 definition SenderSPF :: "Sender tsyn SPF" where
