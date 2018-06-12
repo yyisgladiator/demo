18ec2ad1f00ff32a8021d17696903d66f57533c5
Dennis Slotboom <dennis.slotboom@rwth-aachen.de>
2018-06-12 21:03:12 +0200

    Tidied up tsynbAbs lemmata. #123

--- timesyn/tsynBundle.thy
+++ timesyn/tsynBundle.thy
@@ -24,9 +24,6 @@ lift_definition tsynbNull :: "channel \<Rightarrow> 'm tsyn SB" is
   "\<lambda>c. [c \<mapsto> \<up>null]"
   by (simp add: ubWell_def usclOkay_stream_def ctype_tsyn_def)
     
-definition tsynbAbs :: "'a tsyn stream ubundle \<rightarrow> 'a stream ubundle option" where 
-  "tsynbAbs  \<equiv> \<Lambda> sb. (ubclDom\<cdot>sb = {c1}) \<leadsto> Abs_ubundle[c2 \<mapsto> tsynAbs\<cdot>(sb  .  c1)]"
-
 lemma tsynbnull_ubdom [simp]: "ubDom\<cdot>(tsynbNull c) = {c}"
   by (simp add:tsynbNull.rep_eq ubdom_insert)
 
@@ -43,46 +40,65 @@ lemma tsynbnull_ubconc_sbrt [simp]:
   shows "sbRt\<cdot>(ubConc (tsynbNull c)\<cdot>sb) = sb"
   apply (rule ub_eq)
   by (simp add: assms sbRt_def)+
-   
-instantiation nat :: message 
+
+(* ----------------------------------------------------------------------- *)
+  section {* Definitions on Time-Synchronous Stream Bundles *}
+(* ----------------------------------------------------------------------- *)
+
+definition tsynbAbs :: "'a tsyn stream ubundle \<rightarrow> 'a stream ubundle option" where 
+  "tsynbAbs  \<equiv> \<Lambda> sb. (ubclDom\<cdot>sb = {c1}) \<leadsto> Abs_ubundle [c2 \<mapsto> tsynAbs\<cdot>(sb  .  c1)]"
+
+(* ----------------------------------------------------------------------- *)
+  section {* Definitions of Time-Synchronous Test Bundles *}
+(* ----------------------------------------------------------------------- *)
+
+instantiation nat :: message
 begin
   fun ctype_nat :: "channel \<Rightarrow> nat set" where 
   "ctype_nat c = range nat" 
-
   instance
-  by(intro_classes)
+    by (intro_classes)
 end
  
-lift_definition sbun :: "nat stream ubundle " is "[c2 \<mapsto> <[(1::nat),2,1]>]"
-  apply (simp add: ubWell_def usclOkay_stream_def)
-  by (metis nat_1 nat_2 numeral_2_eq_2 range_eqI)  
-    
-lift_definition tsynbun :: "nat tsyn stream ubundle " is "[c1 \<mapsto> <[Msg (1::nat), null, Msg 2, Msg 1]>]"
+lift_definition tsynbabs_test_input :: "nat tsyn stream ubundle " is 
+  "[c1 \<mapsto> <[Msg (1 :: nat), null, Msg 2, Msg 1]>]"
   apply (simp add: ubWell_def usclOkay_stream_def ctype_tsyn_def)
   by (metis image_eqI nat_1 nat_2 numeral_2_eq_2 range_eqI)
-  
-lemma conttsynbabs:
- "cont(\<lambda> sb. (ubclDom\<cdot>sb = {c1}) \<leadsto> Abs_ubundle[c2 \<mapsto> tsynAbs\<cdot>(sb  .  c1)])"
+
+lift_definition tsynbabs_test_output :: "nat stream ubundle " is 
+  "[c2 \<mapsto> <[(1 :: nat), 2, 1]>]"
+  apply (simp add: ubWell_def usclOkay_stream_def)
+  by (metis nat_1 nat_2 numeral_2_eq_2 range_eqI)
+
+lemma tsynbabs_test_input_ubdom: "ubDom\<cdot>tsynbabs_test_input = {c1}"
+  sorry
+
+(* ----------------------------------------------------------------------- *)
+  section {* Lemmata on Time-Synchronous Stream Bundles *}
+(* ----------------------------------------------------------------------- *)
+
+lemma tsynbabs_mono:
+  "monofun (\<lambda> sb. (ubDom\<cdot>sb = {c1}) \<leadsto> Abs_ubundle [c2 \<mapsto> tsynAbs\<cdot>(sb  .  c1)])"
+  sorry
+
+lemma tsynbabs_cont:
+  "cont (\<lambda> sb. (ubclDom\<cdot>sb = {c1}) \<leadsto> Abs_ubundle [c2 \<mapsto> tsynAbs\<cdot>(sb  .  c1)])"
   apply (simp add: ubclDom_ubundle_def)
-  apply(rule contI2)
-  apply(rule monofunI)
-  apply (simp add: below_option_def)
+  apply (rule contI2)
+  apply (rule monofunI)
+  apply (simp add: tsynbabs_mono)
+  sorry
+
+lemma tsynbabs_insert: 
+  "tsynbAbs\<cdot>sb = (ubclDom\<cdot>sb = {c1}) \<leadsto> Abs_ubundle [c2 \<mapsto> tsynAbs\<cdot>(sb  .  c1)]"
   sorry
     
-lemma nbhj:"ubWell([c1 \<mapsto> \<up>(\<M> Suc (0::nat)) \<bullet> \<up>- \<bullet> \<up>(\<M> 2::nat) \<bullet> \<up>(\<M> Suc (0::nat))])"
-  apply (simp add: ubWell_def usclOkay_stream_def ctype_tsyn_def)
-  by (metis image_eqI nat_1 nat_2 numeral_2_eq_2 range_eqI)
-    
-lemma kk:"ubclDom\<cdot>(Abs_ubundle [c1 \<mapsto> \<up>(\<M> Suc (0::nat)) \<bullet> \<up>- \<bullet> \<up>(\<M> 2::nat) \<bullet> \<up>(\<M> Suc (0::nat))]) = {c1}"
-  apply(simp add: ubclDom_ubundle_def)
-  by(simp add:ubdom_ubrep_eq nbhj)
- 
-lemma tsynrec_test_finstream:
-  "tsynbAbs\<cdot>(tsynbun) = Some(sbun)"
-  apply(simp add: tsynbAbs_def tsynbun_def sbun_def)
-  apply(simp add: conttsynbabs)
-  apply(simp add: kk)
-  by(simp add: ubgetch_ubrep_eq nbhj tsynabs_insert)
-  
+lemma tsynbabs_test_finstream:
+  "tsynbAbs\<cdot>(tsynbabs_test_input) = Some (tsynbabs_test_output)"
+  apply (simp add: tsynbabs_insert ubclDom_ubundle_def tsynbabs_test_input_ubdom)
+  apply (simp only: tsynbabs_test_input_def tsynbabs_test_output_def)
+  apply (subst ubgetch_ubrep_eq)
+  apply (metis tsynbabs_test_input.rep_eq ubrep_well)
+  by (simp add: tsynbabs_insert tsynabs_insert)
     
 end
