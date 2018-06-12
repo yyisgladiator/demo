ca90a612663936e253b483cd3f9cbdfc5d5e35ae
Oliver <oliver.amine.zelmat@rwth-aachen.de>
2018-06-12 17:12:58 +0200

    adedd tsynbAbs for #92

--- timesyn/tsynBundle.thy
+++ timesyn/tsynBundle.thy
@@ -23,6 +23,9 @@ default_sort message
 lift_definition tsynbNull :: "channel \<Rightarrow> 'm tsyn SB" is
   "\<lambda>c. [c \<mapsto> \<up>null]"
   by (simp add: ubWell_def usclOkay_stream_def ctype_tsyn_def)
+    
+definition tsynbAbs :: "'a tsyn stream ubundle \<rightarrow> 'a stream ubundle option" where 
+  "tsynbAbs  \<equiv> \<Lambda> sb. (ubclDom\<cdot>sb = {c1}) \<leadsto> Abs_ubundle[c2 \<mapsto> tsynAbs\<cdot>(sb  .  c1)]"
 
 lemma tsynbnull_ubdom [simp]: "ubDom\<cdot>(tsynbNull c) = {c}"
   by (simp add:tsynbNull.rep_eq ubdom_insert)
@@ -40,5 +43,46 @@ lemma tsynbnull_ubconc_sbrt [simp]:
   shows "sbRt\<cdot>(ubConc (tsynbNull c)\<cdot>sb) = sb"
   apply (rule ub_eq)
   by (simp add: assms sbRt_def)+
+   
+instantiation nat :: message 
+begin
+  fun ctype_nat :: "channel \<Rightarrow> nat set" where 
+  "ctype_nat c = range nat" 
+
+  instance
+  by(intro_classes)
+end
+ 
+lift_definition sbun :: "nat stream ubundle " is "[c2 \<mapsto> <[(1::nat),2,1]>]"
+  apply (simp add: ubWell_def usclOkay_stream_def)
+  by (metis nat_1 nat_2 numeral_2_eq_2 range_eqI)  
+    
+lift_definition tsynbun :: "nat tsyn stream ubundle " is "[c1 \<mapsto> <[Msg (1::nat), null, Msg 2, Msg 1]>]"
+  apply (simp add: ubWell_def usclOkay_stream_def ctype_tsyn_def)
+  by (metis image_eqI nat_1 nat_2 numeral_2_eq_2 range_eqI)
+  
+lemma conttsynbabs:
+ "cont(\<lambda> sb. (ubclDom\<cdot>sb = {c1}) \<leadsto> Abs_ubundle[c2 \<mapsto> tsynAbs\<cdot>(sb  .  c1)])"
+  apply (simp add: ubclDom_ubundle_def)
+  apply(rule contI2)
+  apply(rule monofunI)
+  apply (simp add: below_option_def)
+  sorry
+    
+lemma nbhj:"ubWell([c1 \<mapsto> \<up>(\<M> Suc (0::nat)) \<bullet> \<up>- \<bullet> \<up>(\<M> 2::nat) \<bullet> \<up>(\<M> Suc (0::nat))])"
+  apply (simp add: ubWell_def usclOkay_stream_def ctype_tsyn_def)
+  by (metis image_eqI nat_1 nat_2 numeral_2_eq_2 range_eqI)
+    
+lemma kk:"ubclDom\<cdot>(Abs_ubundle [c1 \<mapsto> \<up>(\<M> Suc (0::nat)) \<bullet> \<up>- \<bullet> \<up>(\<M> 2::nat) \<bullet> \<up>(\<M> Suc (0::nat))]) = {c1}"
+  apply(simp add: ubclDom_ubundle_def)
+  by(simp add:ubdom_ubrep_eq nbhj)
+ 
+lemma tsynrec_test_finstream:
+  "tsynbAbs\<cdot>(tsynbun) = Some(sbun)"
+  apply(simp add: tsynbAbs_def tsynbun_def sbun_def)
+  apply(simp add: conttsynbabs)
+  apply(simp add: kk)
+  by(simp add: ubgetch_ubrep_eq nbhj tsynabs_insert)
+  
     
 end
