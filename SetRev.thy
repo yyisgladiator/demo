7177a42b32736bc1825c75e76f471202ccdebb0e
Daivd Duong <david.duong@rwth-aachen.de>
2018-06-17 17:18:30 +0100

    set flat rev

--- SetRev.thy
+++ SetRev.thy
@@ -422,4 +422,71 @@ lemma "\<And> S. setflat\<cdot>S = \<Union>S"
   apply (subst Union_eq)
   by auto
 
+
+subsection \<open>set flat rev def N lemmas\<close>
+definition setflat_rev :: "'a set set rev \<rightarrow> 'a set rev" where
+"setflat_rev = (\<Lambda> S. Rev {K  | Z K. K\<in>Z \<and> Z \<in> (inv Rev S)} )"
+
+lemma setflat_rev_mono: "monofun (\<lambda>S. Rev {K  | Z K. K\<in>Z \<and> Z \<in> (inv Rev S)} )"
+  apply(rule monofunI)
+  apply auto
+  apply (simp add: less_set_def)
+  apply (subst subset_iff)
+  using revBelowNeqSubset by fastforce
+
+(*
+lemma setflat_rev_cont: "cont (\<lambda>S. Rev {K  | Z K. K\<in>Z \<and> Z \<in> (inv Rev S)} )"
+    apply (rule contI2)
+  using setflat_rev_mono apply blast
+proof -
+  fix Y:: "nat \<Rightarrow> 'a set set rev"
+  assume a1: "chain Y"
+  have "\<And>i. Y i \<sqsubseteq> Lub Y"
+    by (simp add: a1 is_ub_thelub)
+
+  have f0: "\<And>i. inv Rev (Y (Suc i)) \<subseteq> inv Rev (Y i)"
+    by (meson a1 po_class.chainE revBelowNeqSubset)
+  have f1: "inv Rev (Rev {uu::'a. \<exists>(Z::'a set) K::'a. uu = K \<and> K \<in> Z \<and> Z \<in> inv Rev (Lub Y)}) = 
+              {uu::'a. \<exists>(Z::'a set) K::'a. uu = K \<and> K \<in> Z \<and> Z \<in> inv Rev (Lub Y)}"
+    by (simp add: inv_rev_rev)
+  have f2: "chain (\<lambda>i::nat. Rev {uu::'a. \<exists>Z::'a set. uu \<in> Z \<and> Z \<in> inv Rev (Y i)})"
+    apply (rule chainI)
+    apply (subst revBelowNeqSubset)      
+    apply (simp add: inv_rev_rev)
+    apply rule
+    using f0 by fastforce
+  have f3: "inv Rev (\<Squnion>i. Y i) = (\<Inter>{x. \<exists>i. x = inv Rev (Y i)})"
+    using a1 setrevLubEqInterII by fastforce
+
+  have f4: "\<Inter>{x::'a set. \<exists>i::nat. x = {uu::'a. \<exists>Z::'a set. uu \<in> Z \<and> Z \<in> inv Rev (Y i)}} 
+    \<subseteq> {uu::'a. \<exists>Z::'a set. uu \<in> Z \<and> Z \<in> inv Rev (Lub Y)}" (is "?L \<subseteq> ?R")
+  proof (rule subsetI)
+    fix x
+    assume a1: "x \<in> ?L"
+    have "chain (\<lambda> i. Rev {uu::'a. \<exists>Z::'a set. uu \<in> Z \<and> Z \<in> inv Rev (Y i)})"
+    proof  (rule chainI)
+      fix i
+      show "Rev {uu::'a. \<exists>Z::'a set. uu \<in> Z \<and> Z \<in> inv Rev (Y i)} 
+            \<sqsubseteq> Rev {uu::'a. \<exists>Z::'a set. uu \<in> Z \<and> Z \<in> inv Rev (Y (Suc i))}"
+        apply auto
+        apply (simp add: less_set_def)
+        sorry
+    qed
+    have "\<forall> i. x \<in> {uu::'a. \<exists>Z::'a set. uu \<in> Z \<and> Z \<in> inv Rev (Y i)}"
+      by (metis (mono_tags, lifting) CollectI Inter_iff a1)
+    then have "\<forall> i. \<exists> Z. (x \<in> Z \<and> Z \<in> inv Rev (Y i))"
+      by simp
+    then show "x \<in> ?R"
+      sorry
+  qed
+  show "Rev {uu::'a. \<exists>(Z::'a set) K::'a. uu = K \<and> K \<in> Z \<and> Z \<in> inv Rev (\<Squnion>i::nat. Y i)} 
+      \<sqsubseteq> (\<Squnion>i::nat. Rev {uu::'a. \<exists>(Z::'a set) K::'a. uu = K \<and> K \<in> Z \<and> Z \<in> inv Rev (Y i)})"
+    apply (subst revBelowNeqSubset)
+    apply (simp add: inv_rev_rev)
+    apply (simp add: f2 setrevLubEqInterII inv_rev_rev)
+    apply (subst f3)
+    using f3 f4 by auto
+qed
+*)
+
 end
