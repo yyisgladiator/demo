a67d8f35dab559e14799c08ff3092e17bd5f1fff
Annika Savelsberg <annika.savelsberg@rwth-aachen.de>
2018-07-03 10:18:36 +0200

    First attempt on tsynmed_tsyndom. Proof of tsynmed_sconc_msg_t. #161

--- timesyn/CaseStudies/ABP/Medium.thy
+++ timesyn/CaseStudies/ABP/Medium.thy
@@ -31,9 +31,44 @@ lemma oracases [case_names bottom true false]:
   shows "P ts"
   by (metis (full_types) bottom false scases true)
 
+(* ----------------------------------------------------------------------- *)
+subsection {* basic properties of tsMed *}
+(* ----------------------------------------------------------------------- *)
+
 text{* "Lossy" medium that gets messages and an oracle and will transmit the k-th message if
        the k-th element in the oracle is True, otherwise the message will be discarded. *}
 lemma tsynmed_insert: "tsynMed\<cdot>msg\<cdot>ora = tsynProjFst\<cdot>(tsynFilter {x. snd x}\<cdot>(tsynZip\<cdot>msg\<cdot>ora))"
   by (simp add: tsynMed_def)
+
+text{* If the first element in the oracle is True then the current message will be transmitted. *}
+lemma tsynmed_sconc_msg_t:
+  assumes "msg \<noteq> \<epsilon>"
+    and " #ora = \<infinity>"
+  shows "tsynMed\<cdot>(\<up>(Msg m) \<bullet> msg)\<cdot>(\<up>True \<bullet> ora) = \<up>(Msg m) \<bullet> (tsynMed\<cdot>msg\<cdot>ora)"
+  proof -
+    have thesis_simple:
+      "tsynProjFst\<cdot>(tsynFilter {x. snd x}\<cdot>(tsynZip\<cdot>(\<up>(Msg m) \<bullet> msg)\<cdot>(\<up>True \<bullet> ora)))
+        = \<up>(Msg m) \<bullet> tsynProjFst\<cdot>(tsynFilter {x. snd x}\<cdot>(tsynZip\<cdot>msg\<cdot>ora))"
+        by (simp add: tsynzip_sconc_msg tsynfilter_sconc_msg_in tsynfilter_sconc_null 
+          tsynprojfst_sconc_null tsynprojfst_sconc_msg)
+    then show ?thesis
+      by (simp add: tsynmed_insert)
+  qed
+
+lemma tsynmed_tsyndom: assumes "#ora=\<infinity>" shows "tsynDom\<cdot>(tsynMed\<cdot>msg\<cdot>ora) \<subseteq> tsynDom\<cdot>msg"
+  proof (induction rule: oracases)
+    case bottom
+      then show ?case
+        using assms by simp
+    next
+      case (true as)
+      then show ?case
+        apply (simp add: assms)
+         sorry
+    next
+      case (false as)
+      then show ?case sorry
+    qed
+
 
 end
