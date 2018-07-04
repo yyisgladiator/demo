39eaafdaa25a175fcd88e47df1d73263c0976512
Annika Savelsberg <annika.savelsberg@rwth-aachen.de>
2018-07-04 23:36:26 +0200

    Completed proof of

--- timesyn/CaseStudies/ABP/Medium.thy
+++ timesyn/CaseStudies/ABP/Medium.thy
@@ -68,8 +68,8 @@ lemma tsynmed_sconc_msg_f:
   shows "tsynMed\<cdot>(\<up>(Msg m) \<bullet> msg)\<cdot>(\<up>False \<bullet> ora) = tsynMed\<cdot>msg\<cdot>ora"
 sorry
 
-text{* If the first element in the stream is a tick the oracle will not change. *}
-lemma tsynmed_sconc_tick:
+text{* If the first element in the stream is null the oracle will not change. *}
+lemma tsynmed_sconc_null:
   assumes "msg \<noteq> \<epsilon>" 
     and " #ora = \<infinity>" 
   shows "tsynMed\<cdot>(\<up>- \<bullet> msg)\<cdot>ora = \<up>- \<bullet> tsynMed\<cdot>msg\<cdot>ora"
@@ -96,30 +96,72 @@ lemma tsynmed_tsyndom: assumes ora_inf:"#ora=\<infinity>" shows "tsynDom\<cdot>(
         case (true as)
         then show ?thesis 
           proof (cases rule: scases [of s])
-            assume ora_t: "ora = \<up>True \<bullet> as"
             case bottom
-              have tsynzip_bot: "tsynZip\<cdot>(\<up>(Msg m))\<cdot>(\<up>True \<bullet> ora) = (\<up>(Msg (m,True)))\<bullet>tsynZip\<cdot>\<epsilon>\<cdot>ora"
-                by (metis sconc_snd_empty tsynzip_sconc_msg)
-              have tsynfilter_simp: "tsynFilter {x::'a \<times> bool. snd x}\<cdot>(\<up>(Msg (m, True))) = (\<up>(Msg (m, True)))"
-                by (metis mem_Collect_eq sconc_snd_empty snd_conv tsynfilter_sconc_msg_in tsynfilter_strict)
-              have thesis_simp:
-                "tsynProjFst\<cdot>(tsynFilter {x. snd x}\<cdot>(tsynZip\<cdot>(\<up>(Msg m))\<cdot>(\<up>True \<bullet> ora)))
-                 = \<up>(Msg m)"
-                by (simp add: tsynzip_bot tsynfilter_simp tsynprojfst_insert)
+            have tsynzip_bot: "tsynZip\<cdot>(\<up>(Msg m))\<cdot>(\<up>True \<bullet> ora) = (\<up>(Msg (m,True)))\<bullet>tsynZip\<cdot>\<epsilon>\<cdot>ora"
+              by (metis sconc_snd_empty tsynzip_sconc_msg)
+            have tsynfilter_simp: "tsynFilter {x::'a \<times> bool. snd x}\<cdot>(\<up>(Msg (m, True))) = (\<up>(Msg (m, True)))"
+              by (metis mem_Collect_eq sconc_snd_empty snd_conv tsynfilter_sconc_msg_in tsynfilter_strict)
+            have thesis_simp:
+              "tsynProjFst\<cdot>(tsynFilter {x. snd x}\<cdot>(tsynZip\<cdot>(\<up>(Msg m))\<cdot>(\<up>True \<bullet> ora)))
+               = \<up>(Msg m)"
+              by (simp add: tsynzip_bot tsynfilter_simp tsynprojfst_insert)
             then show ?thesis 
-              apply (simp add: tsynmed_insert thesis_simp)
-              by (metis bottom sconc_snd_empty set_eq_subset thesis_simp true tsynZip.simps(1) tsynzip_sconc_msg)
+              by (metis bottom tsynmed_insert thesis_simp sconc_snd_empty set_eq_subset thesis_simp 
+                  true tsynZip.simps(1) tsynzip_sconc_msg)
           next
-            case (scons a s)
-            then show ?thesis sorry
+            case (scons a t)
+            have s_not_empty: "s \<noteq> \<epsilon>"
+              by (simp add: scons)
+            have as_inf: "#as = \<infinity>"
+              using msg.prems true by simp
+            then show ?thesis 
+              by (simp add: s_not_empty true as_inf tsynmed_sconc_msg_t tsyndom_sconc_msg msg.IH 
+                  subset_insertI2)
           qed
       next
         case (false as)
-        then show ?thesis sorry
+        then show ?thesis
+          proof (cases rule: scases [of s])
+            case bottom
+            have tsynzip_bot: "tsynZip\<cdot>(\<up>(Msg m))\<cdot>(\<up>False \<bullet> ora) = (\<up>(Msg (m,False)))\<bullet>tsynZip\<cdot>\<epsilon>\<cdot>ora"
+              by (metis sconc_snd_empty tsynzip_sconc_msg)
+            have tsynfilter_simp: "tsynFilter {x::'a \<times> bool. snd x}\<cdot>(\<up>(Msg (m, False))) = \<up>null"
+              by (metis (full_types) mem_Collect_eq sconc_snd_empty snd_conv 
+                  tsynfilter_sconc_msg_nin tsynfilter_strict)
+            have thesis_simp:
+              "tsynProjFst\<cdot>(tsynFilter {x. snd x}\<cdot>(tsynZip\<cdot>(\<up>(Msg m))\<cdot>(\<up>False \<bullet> ora)))
+               = \<up>null"
+              by (metis lscons_conv sup'_def tsynZip.simps(1) tsynfilter_simp tsynprojfst_sconc_null 
+                  tsynprojfst_strict tsynzip_bot)
+            then show ?thesis 
+              by (metis bottom false order_refl sconc_snd_empty tsynZip.simps(1) 
+                  tsyndom_sconc_msg_sub tsyndom_sconc_null tsynmed_insert tsynzip_sconc_msg)
+          next
+            case (scons a t)
+            have s_not_empty: "s \<noteq> \<epsilon>"
+              by (simp add: scons)
+            have as_inf: "#as = \<infinity>"
+              using msg.prems false by simp
+            then show ?thesis
+              using false msg.IH s_not_empty tsyndom_sconc_msg_sub tsynmed_sconc_msg_f by fastforce
+          qed
       qed
   next
     case (null s)
-    then show ?case sorry
+    have tsynmed_null: "tsynMed\<cdot>(\<up>- \<bullet> s)\<cdot>ora = \<up>- \<bullet> tsynMed\<cdot>s\<cdot>ora"
+      proof (cases rule: scases [of s])
+        case bottom
+        then show ?thesis 
+          by (metis (no_types, hide_lams) Inf'_neq_0 null.prems sconc_snd_empty strict_slen 
+              tsynZip.simps(1) tsynfilter_sconc_null tsynmed_insert tsynprojfst_sconc_null 
+              tsynzip_sconc_null)
+      next
+        case (scons a s)
+        then show ?thesis 
+        by (simp add: null.prems tsynmed_sconc_null)
+      qed
+    then show ?case
+      by (simp add: null.IH null.prems tsyndom_sconc_null)
   qed
 
 end
