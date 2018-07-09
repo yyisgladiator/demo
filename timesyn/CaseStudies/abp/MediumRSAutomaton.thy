ff327605de36ecd12b7c5e0dc77c5ea6f011b662
Mathias Pfeiffer <mathias.pfeiffer@rwth-aachen.de>
2018-07-09 14:11:54 +0200

    State-Constructor heisst für jeden Automaten anders (Automatenname als Prefix). Auch Funktion getSubState heisst jetzt get<Name>SubState

--- timesyn/CaseStudies/abp/MediumRSAutomaton.thy
+++ timesyn/CaseStudies/abp/MediumRSAutomaton.thy
@@ -3,7 +3,7 @@
  * This file was generated from MediumRS.maa and will be overridden when changed. To change
  * permanently, consider changing the model itself.
  *
- * Generated on Jul 4, 2018 7:33:39 PM by transformer 1.0.0
+ * Generated on Jul 9, 2018 2:08:06 PM by transformer 1.0.0
  *)
 theory MediumRSAutomaton
 
@@ -15,29 +15,29 @@ begin
 datatype MediumRSSubstate = Single
 
 (* And these have also the variables *)
-datatype MediumRSState = State MediumRSSubstate (* c = *) "nat"
+datatype MediumRSState = MediumRSState MediumRSSubstate (* c = *) "nat"
 
-fun getSubState :: "MediumRSState \<Rightarrow> MediumRSSubstate" where
-"getSubState (State state_s automaton_c) = state_s"
+fun getMediumRSSubState :: "MediumRSState \<Rightarrow> MediumRSSubstate" where
+"getMediumRSSubState (MediumRSState state_s automaton_c) = state_s"
 
 fun getC :: "MediumRSState \<Rightarrow> nat" where
-"getC (State _ automaton_c) = automaton_c"
+"getC (MediumRSState _ automaton_c) = automaton_c"
     
 
 
 fun mediumRSTransitionH :: "(MediumRSState \<times> (abpMessage tsyn)) \<Rightarrow> ((MediumRSState \<times> abpMessage tsyn SB) set rev)" where
-    "mediumRSTransitionH (State Single automaton_c, ((*ar\<mapsto>*)Msg (Bool a))) = 
-       (if(automaton_c>0) then Rev {(State Single (automaton_c-1),(tsynbNull (\<C> ''as'')))}
-       else if(automaton_c=0) then Rev {(State Single (c),(createAsBundle (a)))| (c). True}
-       else Rev {(State Single automaton_c, ((tsynbNull (\<C> ''as''))))})"  |
+    "mediumRSTransitionH (MediumRSState Single automaton_c, ((*ar\<mapsto>*)Msg (Bool a))) = 
+       (if(automaton_c>0) then Rev {(MediumRSState Single (automaton_c-1),(tsynbNull (\<C> ''as'')))}
+       else if(automaton_c=0) then Rev {(MediumRSState Single (c),(createAsBundle (a)))| (c). True}
+       else Rev {(MediumRSState Single automaton_c, ((tsynbNull (\<C> ''as''))))})"  |
 
-    "mediumRSTransitionH (State Single automaton_c, ((*ar\<mapsto>*)null)) = 
-       (Rev {(State Single automaton_c,(tsynbNull (\<C> ''as'')))})"  
+    "mediumRSTransitionH (MediumRSState Single automaton_c, ((*ar\<mapsto>*)null)) = 
+       (Rev {(MediumRSState Single automaton_c,(tsynbNull (\<C> ''as'')))})"  
 
 fun mediumRSTransition :: "(MediumRSState \<times> (channel \<rightharpoonup> abpMessage tsyn)) \<Rightarrow> ((MediumRSState \<times> abpMessage tsyn SB) set rev)" where
 "mediumRSTransition (s,f) = (if dom(f) = {\<C> ''ar''} then mediumRSTransitionH (s,(f\<rightharpoonup>\<C> ''ar'')) else undefined)"
 
-lift_definition MediumRSAutomaton :: "(MediumRSState, abpMessage tsyn) ndAutomaton" is "(mediumRSTransition, Rev {(State Single c, (tsynbNull (\<C> ''as'')))| c. True}, Discr {\<C> ''ar''}, Discr {\<C> ''as''})"
+lift_definition MediumRSAutomaton :: "(MediumRSState, abpMessage tsyn) ndAutomaton" is "(mediumRSTransition, Rev {(MediumRSState Single c, (tsynbNull (\<C> ''as'')))| c. True}, Discr {\<C> ''ar''}, Discr {\<C> ''as''})"
   by simp
 
 definition MediumRSSPF :: "abpMessage tsyn SPS" where
--- timesyn/CaseStudies/abp/MediumSRAutomaton.thy
+++ timesyn/CaseStudies/abp/MediumSRAutomaton.thy
@@ -3,7 +3,7 @@
  * This file was generated from MediumSR.maa and will be overridden when changed. To change
  * permanently, consider changing the model itself.
  *
- * Generated on Jul 4, 2018 7:33:39 PM by transformer 1.0.0
+ * Generated on Jul 9, 2018 2:08:06 PM by transformer 1.0.0
  *)
 theory MediumSRAutomaton
 
@@ -15,29 +15,29 @@ begin
 datatype MediumSRSubstate = Single
 
 (* And these have also the variables *)
-datatype MediumSRState = State MediumSRSubstate (* c = *) "nat"
+datatype MediumSRState = MediumSRState MediumSRSubstate (* c = *) "nat"
 
-fun getSubState :: "MediumSRState \<Rightarrow> MediumSRSubstate" where
-"getSubState (State state_s automaton_c) = state_s"
+fun getMediumSRSubState :: "MediumSRState \<Rightarrow> MediumSRSubstate" where
+"getMediumSRSubState (MediumSRState state_s automaton_c) = state_s"
 
 fun getC :: "MediumSRState \<Rightarrow> nat" where
-"getC (State _ automaton_c) = automaton_c"
+"getC (MediumSRState _ automaton_c) = automaton_c"
     
 
 
 fun mediumSRTransitionH :: "(MediumSRState \<times> (abpMessage tsyn)) \<Rightarrow> ((MediumSRState \<times> abpMessage tsyn SB) set rev)" where
-    "mediumSRTransitionH (State Single automaton_c, ((*ds\<mapsto>*)Msg (Pair_nat_bool a))) = 
-       (if(automaton_c>0) then Rev {(State Single (automaton_c-1),(tsynbNull (\<C> ''dr'')))}
-       else if(automaton_c=0) then Rev {(State Single (c),(createDrBundle (a)))| (c). True}
-       else Rev {(State Single automaton_c, ((tsynbNull (\<C> ''dr''))))})"  |
+    "mediumSRTransitionH (MediumSRState Single automaton_c, ((*ds\<mapsto>*)Msg (Pair_nat_bool a))) = 
+       (if(automaton_c>0) then Rev {(MediumSRState Single (automaton_c-1),(tsynbNull (\<C> ''dr'')))}
+       else if(automaton_c=0) then Rev {(MediumSRState Single (c),(createDrBundle (a)))| (c). True}
+       else Rev {(MediumSRState Single automaton_c, ((tsynbNull (\<C> ''dr''))))})"  |
 
-    "mediumSRTransitionH (State Single automaton_c, ((*ds\<mapsto>*)null)) = 
-       (Rev {(State Single automaton_c,(tsynbNull (\<C> ''dr'')))})"  
+    "mediumSRTransitionH (MediumSRState Single automaton_c, ((*ds\<mapsto>*)null)) = 
+       (Rev {(MediumSRState Single automaton_c,(tsynbNull (\<C> ''dr'')))})"  
 
 fun mediumSRTransition :: "(MediumSRState \<times> (channel \<rightharpoonup> abpMessage tsyn)) \<Rightarrow> ((MediumSRState \<times> abpMessage tsyn SB) set rev)" where
 "mediumSRTransition (s,f) = (if dom(f) = {\<C> ''ds''} then mediumSRTransitionH (s,(f\<rightharpoonup>\<C> ''ds'')) else undefined)"
 
-lift_definition MediumSRAutomaton :: "(MediumSRState, abpMessage tsyn) ndAutomaton" is "(mediumSRTransition, Rev {(State Single c, (tsynbNull (\<C> ''dr'')))| c. True}, Discr {\<C> ''ds''}, Discr {\<C> ''dr''})"
+lift_definition MediumSRAutomaton :: "(MediumSRState, abpMessage tsyn) ndAutomaton" is "(mediumSRTransition, Rev {(MediumSRState Single c, (tsynbNull (\<C> ''dr'')))| c. True}, Discr {\<C> ''ds''}, Discr {\<C> ''dr''})"
   by simp
 
 definition MediumSRSPF :: "abpMessage tsyn SPS" where
