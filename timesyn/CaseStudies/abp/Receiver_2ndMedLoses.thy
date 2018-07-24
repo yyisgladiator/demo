4995a6506e4a724838a2afe11fd3024cd3d725cf
unknown <shi.chen.niu@rwth-aachen.de>
2018-07-24 23:18:34 +0200

    finished please review correctness and style

--- timesyn/CaseStudies/abp/Receiver_2ndMedLoses.thy
+++ timesyn/CaseStudies/abp/Receiver_2ndMedLoses.thy
@@ -1,11 +1,9 @@
-(*  Title:        Receiver.thy
-    Author:       Marlene Lutz, Dennis Slotboom
-    E-Mail:       marlene.lutz@rwth-aachen.de, dennis.slotboom@rwth-aachen.de
+(*  Title:        Receiver_2ndMedLoses.thy
+    Author:       Shi Chen Niu
+    E-Mail:       shi.chen.niu@rwth-aachen.de, dennis.slotboom@rwth-aachen.de
 
-    Description:  Theory for Receiver Lemmata on Time-synchronous Streams.
+    Description:  CaseStudy for Receiver; case: The second medium loses the first acknowledgement.
 *)
-
-chapter {* Theory for Receiver Lemmata on Time-synchronous Streams *}
 
 theory Receiver_2ndMedLoses
 imports Receiver
@@ -17,27 +15,29 @@ lemma natbool2abp2natbool: "abp2natbool\<cdot>(natbool2abp\<cdot>recTestInputStr
   apply (simp add: abp2natbool_def natbool2abp_def)
   by (simp add: tsynmap_sconc_msg tsynmap_singleton_msg)
 
-lemma tsynrec_ar: "bool2abp\<cdot>(tsynProjSnd\<cdot>(abp2natbool\<cdot>(recTestInputUbundleLoseAck  .  \<C> ''dr''))) = bool2abp\<cdot>recTestOutputStreamArLoseAck"
-  sorry
+lemma abp_inputChannel: "abp2natbool\<cdot>(recTestInputUbundleLoseAck  .  \<C> ''dr'') = recTestInputStreamLoseAck"
+  proof -
+    have h1: "recTestInputUbundleLoseAck  .  \<C> ''dr'' = natbool2abp\<cdot>recTestInputStreamLoseAck"
+      by (metis fun_upd_same option.sel recTestInputUbundleLoseAck.rep_eq ubgetch_insert)
+    then show ?thesis
+      using abp2natbool_def natbool2abp2natbool h1 recTestInputStreamLoseAck_def recTestInputUbundleLoseAck_def by auto
+  qed
 
-lemma secondMedLoses_Stream: "tsynRec\<cdot>recTestInputStreamLoseAck = recTestOutputStreamOLoseAck"
+lemma secondMedLoses_StreamO: "tsynRec\<cdot>recTestInputStreamLoseAck = recTestOutputStreamOLoseAck"
   apply (simp add: recTestInputStreamLoseAck_def recTestOutputStreamOLoseAck_def)
   apply (simp add: tsynrec_sconc_msg_t)
   done
 
-lemma tsynrec_nat: "nat2abp\<cdot>(tsynRec\<cdot>(abp2natbool\<cdot>(recTestInputUbundleLoseAck  .  \<C> ''dr''))) = nat2abp\<cdot>recTestOutputStreamOLoseAck"
-  proof -
-    have h0: "recTestInputUbundleLoseAck  .  \<C> ''dr'' = natbool2abp\<cdot>recTestInputStreamLoseAck"
-      by (metis fun_upd_same option.sel recTestInputUbundleLoseAck.rep_eq ubgetch_insert)
-    have h1: "abp2natbool\<cdot>(recTestInputUbundleLoseAck  .  \<C> ''dr'') = recTestInputStreamLoseAck"
-      using abp2natbool_def natbool2abp2natbool h0 recTestInputStreamLoseAck_def recTestInputUbundleLoseAck_def by auto
-    then show ?thesis
-      by (simp add: secondMedLoses_Stream)
-  qed
+lemma tsynrec_O: "nat2abp\<cdot>(tsynRec\<cdot>(abp2natbool\<cdot>(recTestInputUbundleLoseAck  .  \<C> ''dr''))) = nat2abp\<cdot>recTestOutputStreamOLoseAck"
+  by (simp add: abp_inputChannel secondMedLoses_StreamO)
+
+lemma tsynrec_ar: "bool2abp\<cdot>(tsynProjSnd\<cdot>(abp2natbool\<cdot>(recTestInputUbundleLoseAck  .  \<C> ''dr''))) = bool2abp\<cdot>recTestOutputStreamArLoseAck"
+  apply (simp add: abp_inputChannel recTestInputStreamLoseAck_def recTestOutputStreamArLoseAck_def)
+  by (metis lscons_conv sup'_def tsynprojsnd_sconc_msg tsynprojsnd_strict)
 
 lemma secondMedLoses_SB: "tsynbRec\<cdot>recTestInputUbundleLoseAck = Some recTestOutputUbundleLoseAck"
   apply (simp add: tsynbrec_insert)
-  by (simp add: fun_upd_twist recTestInputUbundleLoseAck.rep_eq recTestOutputUbundleLoseAck_def tsynrec_ar tsynrec_nat ubdom_insert)
+  by (simp add: fun_upd_twist recTestInputUbundleLoseAck.rep_eq recTestOutputUbundleLoseAck_def tsynrec_ar tsynrec_O ubdom_insert)
 
 lemma secondMedLoses_SPF: "RecSPF \<rightleftharpoons> recTestInputUbundleLoseAck = recTestOutputUbundleLoseAck"
   by (simp add: RecSPF_def secondMedLoses_SB)
