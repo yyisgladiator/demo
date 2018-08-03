af783e8d542511ef888b289bd94a1d18cadf24b6
Annika Savelsberg <annika.savelsberg@rwth-aachen.de>
2018-08-03 20:14:47 +0200

    Tidied up proof. #236 Still some work to do.

--- timesyn/CaseStudies/abp/Medium.thy
+++ timesyn/CaseStudies/abp/Medium.thy
@@ -155,14 +155,59 @@ lemma tsynmed_tsyndom: "tsynDom\<cdot>(tsynMed\<cdot>msg\<cdot>ora) \<subseteq> 
       by (metis tsyndom_sconc_null tsynmed_sconc_null tsynmed_strict(2))
   qed
 
-lemma tsynmed_sntimes_null: "ora \<noteq> \<epsilon> \<Longrightarrow> tsynMed\<cdot>((sntimes k (\<up>null)) \<bullet> s)\<cdot>ora = (sntimes k (\<up>null)) \<bullet> tsynMed\<cdot>s\<cdot>ora"
+lemma tsynmed_sntimes_null: 
+  "ora \<noteq> \<epsilon> \<Longrightarrow> tsynMed\<cdot>((k\<star>(\<up>null)) \<bullet> s)\<cdot>ora = (k\<star>(\<up>null)) \<bullet> tsynMed\<cdot>s\<cdot>ora"
   apply (induct k, simp_all)
-  apply (cases rule:oracases,simp)
+  apply (cases rule:oracases, simp)
   using tsynmed_sconc_null by auto
 
+(* nach tsynStream *)
 lemma tsynlen_sntimes_null: "tsynLen\<cdot>(k\<star>\<up>- \<bullet> s) = tsynLen\<cdot>s"
-  apply(induct k,simp)
+  apply (induct k, simp)
   by (simp add: tsynlen_sconc_null)
+
+(* nach tsynStream *)
+lemma slen_stakewhile_fin: 
+  assumes "tsynLen\<cdot>as > 0" 
+  obtains n where "#(stakewhile (\<lambda>x. x = null)\<cdot>as) = Fin n"
+  proof -
+    obtain k where "snth k as \<noteq> null"
+      using assms
+      apply (simp add: tsynlen_insert tsynabs_insert)
+      by (metis (mono_tags, lifting) ex_snth_in_sfilter_nempty lnsuc_neq_0 mem_Collect_eq slen_empty_eq)
+    then show ?thesis
+      by (metis (full_types) lncases notinfI3 stakewhile_slen that)
+  qed
+
+(* nach tsynStream *)
+lemma split_leading_null:
+  assumes "tsynLen\<cdot>as > 0"
+  obtains n where "as = (sntimes n (\<up>null)) \<bullet> (sdropwhile (\<lambda>x. x = null)\<cdot>as)"
+  proof -
+    obtain k where k_def: "#(stakewhile (\<lambda>x. x = null)\<cdot>as) = Fin k"
+      using assms slen_stakewhile_fin by blast
+    then have "(sntimes k (\<up>null)) = (stakewhile (\<lambda>x. x = null)\<cdot>as)"
+      proof (induction as arbitrary: k rule: tsyn_ind)
+        case adm
+        then show ?case 
+          apply (rule admI)
+          by (metis (no_types, lifting) Fin_neq_inf ch2ch_Rep_cfunR contlub_cfun_arg inf_chainl4 l42)
+      next
+        case bot
+        then show ?case
+          by simp
+      next
+        case (msg m s)
+        then show ?case 
+          by simp
+      next
+        case (null s)
+        then show ?case 
+          using infI slen_sconc_snd_inf slen_scons sntimes.simps(2) by fastforce
+      qed
+    then show ?thesis
+      by (metis stakewhileDropwhile that)
+  qed
 
 text{* The number of transmitted messages equals the number of True in ora. *}
 lemma tsynmed_tsynlen_ora: 
@@ -179,34 +224,51 @@ lemma tsynmed_tsynlen_ora:
       by simp
   next
     case msg:(3 a s)
-    obtain k1 where "snth k1 msg \<noteq> null"
-      using msg.prems apply (simp add: tsynlen_insert tsynabs_insert)
-      by (metis (mono_tags, lifting) Inf'_neq_0 ex_snth_in_sfilter_nempty mem_Collect_eq slen_empty_eq)
-    then obtain n where h1:"#(stakewhile (\<lambda>x. x = -)\<cdot>msg) = Fin n"
-      by (metis (full_types) lncases notinfI3 stakewhile_slen)
-    then have h2:"(sdropwhile (\<lambda>x. x=null)\<cdot>msg) \<noteq> \<epsilon>"
-      using msg.prems apply (simp add: tsynlen_insert tsynabs_insert)
-      by (metis Fin_neq_inf sconc_snd_empty sfilterl4 stakewhileDropwhile)
-    from h1 have "(sntimes n (\<up>null)) = (stakewhile (\<lambda>x. x = -)\<cdot>msg)"
-      apply (induct msg arbitrary: n rule: tsyn_ind)
-      apply (rule admI)
-      apply (smt ch2ch_Rep_cfunR contlub_cfun_arg finChainapprox lub_eq)
-      apply simp_all
-      by (metis (mono_tags) Fin_0 Fin_Suc gr0_implies_Suc gr_0 lnat.sel_rews(2) lnat_well_h1 sntimes.simps(2))
-    then have h3: " msg =(sntimes n (\<up>null))  \<bullet> (sdropwhile (\<lambda>x. x=null)\<cdot>msg)"
-      by (simp add: stakewhileDropwhile)
-    from h2 obtain b where "shd (sdropwhile (\<lambda>x. x=null)\<cdot>msg) = Msg b"
+    have tsynlen_nzero: "tsynLen\<cdot>msg > 0"
+      using Zero_lnless_infty msg.prems by simp
+    then obtain n where msg_def: "msg = (sntimes n (\<up>null)) \<bullet> (sdropwhile (\<lambda>x. x = null)\<cdot>msg)"
+      using split_leading_null by blast
+    then have slen_stakewhile_msg_fin: "#(stakewhile (\<lambda>x. x = null)\<cdot>msg) < \<infinity>"
+      by (metis Fin_neq_inf Zero_lnless_infty inf_less_eq leI msg.prems slen_stakewhile_fin)
+    have sdropwhile_nbot: "(sdropwhile (\<lambda>x. x = null)\<cdot>msg) \<noteq> \<epsilon>"
+      using msg.prems 
+      apply (simp add: tsynlen_insert tsynabs_insert slen_stakewhile_fin)
+      by (metis Fin_neq_inf tsynlen_nzero sconc_snd_empty sfilterl4 slen_stakewhile_fin 
+          stakewhileDropwhile)
+    from sdropwhile_nbot obtain b where b_def: "shd (sdropwhile (\<lambda>x. x = null)\<cdot>msg) = Msg b"
       by (metis (full_types) scases sdropwhile_resup shd1 tsynSnd.cases)
-    moreover have "tsynLen\<cdot>(srt\<cdot>(sdropwhile (\<lambda>x. x = -)\<cdot>msg)) = \<infinity>"
-      using msg.prems
-      by (simp add: tsynlen_insert tsynabs_insert h1 stakewhile_sdropwhilel1 slen_sfilter_sdrop)
+    moreover have tsynlen_inf: "tsynLen\<cdot>(srt\<cdot>(sdropwhile (\<lambda>x. x = null)\<cdot>msg)) = \<infinity>"
+      apply (simp add: tsynlen_insert tsynabs_insert)
+      apply (subst stakewhile_sdropwhilel1)
+(*      apply (simp add: slen_stakewhile_msg_fin)
+      apply (simp add: tsynlen_nzero slen_stakewhile_fin stakewhile_sdropwhilel1 slen_sfilter_sdrop)*)
+      sorry
     ultimately show ?case
-      apply (subst h3)
-      apply (subst tsynmed_sntimes_null,simp)
-      apply (simp add: tsynlen_sntimes_null)
-      apply (cases a,simp_all)
-      apply (metis (no_types, lifting) h2 msg.IH surj_scons tsynlen_sconc_msg tsynmed_sconc_msg_t)
-      by (metis h2 msg.IH tsynlen_sconc_null tsynmed_sconc_msg_f surj_scons)
+      proof -
+        have thesis_msg: "tsynLen\<cdot>(tsynMed\<cdot>msg\<cdot>(\<up>a \<bullet> s)) 
+          = tsynLen\<cdot>(tsynMed\<cdot>((sntimes n (\<up>null)) \<bullet> (sdropwhile (\<lambda>x. x = null))\<cdot>msg)\<cdot>(\<up>a \<bullet> s))"
+          using msg_def by simp
+        then have thesis_msg_consume_tick: 
+          "tsynLen\<cdot>(tsynMed\<cdot>(n\<star>\<up>- \<bullet> sdropwhile (\<lambda>x::(nat \<times> bool) tsyn. x = -)\<cdot>msg)\<cdot>(\<up>a \<bullet> s))
+          = tsynLen\<cdot>(tsynMed\<cdot>(sdropwhile (\<lambda>x. x = -)\<cdot>msg)\<cdot>(\<up>a \<bullet> s))"
+          by (simp add: tsynmed_sntimes_null tsynlen_sntimes_null)
+        then show ?thesis
+          proof (cases a)
+            case True
+            then show ?thesis
+              using thesis_msg thesis_msg_consume_tick
+              apply (simp add: True)
+              by (metis b_def msg.IH sdropwhile_nbot surj_scons tsynlen_inf tsynlen_sconc_msg 
+                  tsynmed_sconc_msg_t)
+          next
+            case False
+            then show ?thesis 
+              using thesis_msg thesis_msg_consume_tick
+              apply (simp add: False)
+              by (metis b_def msg.IH sdropwhile_nbot surj_scons tsynlen_inf tsynlen_sconc_null 
+                  tsynmed_sconc_msg_f)
+          qed
+      qed
    qed
 
 text{* If infinitely many messages are sent, infinitely many messages will be transmitted. *}
