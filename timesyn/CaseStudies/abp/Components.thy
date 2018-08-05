8f39fddfc6e8945266f6eb18ade9aa1f1ec8046e
unknown <shi.chen.niu@rwth-aachen.de>
2018-08-06 00:25:21 +0200

    started with help lemma

--- timesyn/CaseStudies/abp/Components.thy
+++ timesyn/CaseStudies/abp/Components.thy
@@ -59,32 +59,83 @@ definition abp2nat :: "abpMessage tsyn stream \<rightarrow> nat tsyn stream" whe
   section {* Datatype Conversion Lemmata *}
 (* ----------------------------------------------------------------------- *)
 
-lemma test1: "invPair (Pair_nat_bool n) = n"
-  by (metis invPair.simps(1) old.prod.exhaust)
+lemma tsynMaps2tsynMap: "tsynMap f\<cdot>(tsynMap g\<cdot>tsyns) = tsynMap (\<lambda> x. f (g x))\<cdot>tsyns"
+  proof (cases "tsyns = \<epsilon>")
+    case True
+    then show ?thesis
+      by simp
+  next
+    case False
+    (*have every_ele: "Fin n < #s \<Longrightarrow> snth n (smap (tsynApplyElem f)\<cdot>(smap (tsynApplyElem g)\<cdot>tsyns)) = (tsynApplyElem (\<lambda>x. f (g x)))\<cdot>(snth n tsyns)"
+    have every_ele: "Fin n < #s \<Longrightarrow> snth n (smap f\<cdot>s) = f (snth n s)"*)
+    have thesis_l: "smap (tsynApplyElem f)\<cdot>(smap (tsynApplyElem g)\<cdot>tsyns) = smap (tsynApplyElem (\<lambda>x::'c. f (g x)))\<cdot>tsyns"
+      sorry
+    then show ?thesis
+      using tsynmap_insert
+      by (simp add: tsynmap_insert)
+  qed
 
-lemma test2: "tsynMap f\<cdot>(tsynMap g\<cdot>s) = tsynMap (\<lambda> x. f (g x))\<cdot>s"
-  apply (simp add: tsynmap_insert)
-  sorry
+lemma tsynMap_id: "tsynMap (\<lambda>x. x)\<cdot>tsyns = tsyns"
+  proof -
+    have smap_id: "smap (\<lambda>x :: 'a tsyn. x)\<cdot> tsyns = tsyns"
+      by (simp add: smap_snth_lemma snths_eq)
+    have tsyn_id: "(tsynApplyElem (\<lambda>x :: 'a. x)) = (\<lambda>x :: 'a tsyn. x)"
+      by (metis tsynApplyElem.elims)
+    have smap_tsyn_id: "smap (tsynApplyElem (\<lambda>x :: 'a. x))\<cdot> tsyns = tsyns"
+      by (simp add: local.smap_id tsyn_id)
+    show ?thesis
+      by (simp add: smap_tsyn_id tsynMap_def)
+  qed
 
-lemma natbool2abp2natbool: "abp2natbool\<cdot>(natbool2abp\<cdot>s) = s"
+(* ----------------------------------------------------------------------- *)
+  subsection {* Pair_nat_bool *}
+(* ----------------------------------------------------------------------- *)
+
+lemma pair_nat_bool_inver: "(\<lambda>x::nat \<times> bool. invPair (Pair_nat_bool x)) nb = nb"
+  by (metis invPair.simps(1) surj_pair)                    
+
+lemma natbool2abp2natbool: "abp2natbool\<cdot>(natbool2abp\<cdot>tsyns) = tsyns"
   apply (simp add: abp2natbool_def natbool2abp_def)
-  apply (simp add: test2)
-  apply (simp add: tsynmap_insert)
-  sorry
+  apply (simp add: tsynMaps2tsynMap)
+  apply (simp add: pair_nat_bool_inver)
+  by (simp add: tsynMap_id)
 
-lemma abp2natbool2abp: "natbool2abp\<cdot>(abp2natbool\<cdot>s) = s"
-  sorry
+(*lemma invpair_inver: "(\<lambda>x::abpMessage. Pair_nat_bool (invPair x)) n = n"
+  sorry*)
 
-lemma bool2abp2bool: "abp2bool\<cdot>(bool2abp\<cdot>s) = s"
-  sorry
+(*lemma abp2natbool2abp: assumes "tsynDom\<cdot>tsyns \<subseteq> {null} \<union> (Msg ` range (Pair_nat_bool))"
+  shows "natbool2abp\<cdot>(abp2natbool\<cdot>tsyns) = tsyns"
+  sorry*)
 
-lemma abp2bool2abp: "bool2abp\<cdot>(abp2bool\<cdot>s) = s"
-  sorry
+(* ----------------------------------------------------------------------- *)
+  subsection {* Bool *}
+(* ----------------------------------------------------------------------- *)
 
-lemma nat2abp2nat: "abp2nat\<cdot>(nat2abp\<cdot>s) = s"
-  sorry
+lemma bool2abp2bool: "abp2bool\<cdot>(bool2abp\<cdot>tsyns) = tsyns"
+  apply (simp add: abp2bool_def bool2abp_def)
+  apply (simp add: tsynMaps2tsynMap)
+  by (simp add: tsynMap_id)
 
-lemma abp2nat2abp: "nat2abp\<cdot>(abp2nat\<cdot>s) = s"
+lemma invbool_inver: assumes "(m::abpMessage) = Bool b" 
+  shows "Bool (invBool m) = m"
+  using assms invBool.simps(1) by blast
+
+(*lemma abp2bool2abp: assumes "tsynDom\<cdot>tsyns \<subseteq> {null} \<union> (Msg Bool)"
+  shows "bool2abp\<cdot>(abp2bool\<cdot>tsyns) = tsyns"
+  apply (simp add: abp2bool_def bool2abp_def)
+  apply (simp add: tsynMaps2tsynMap)
+  sorry*)
+
+(* ----------------------------------------------------------------------- *)
+  subsection {* Nat *}
+(* ----------------------------------------------------------------------- *)
+
+lemma nat2abp2nat: "abp2nat\<cdot>(nat2abp\<cdot>tsyns) = tsyns"
+  apply (simp add: abp2nat_def nat2abp_def)
+  apply (simp add: tsynMaps2tsynMap)
+  by (simp add: tsynMap_id)
+
+lemma abp2nat2abp: "nat2abp\<cdot>(abp2nat\<cdot>tsyns) = tsyns"
   sorry  
 
 
