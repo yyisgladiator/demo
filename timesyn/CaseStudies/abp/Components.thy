7e3cfdd1fecdab872b8b3623dd64324defb967f5
JanikB <janik.benzin@rwth-aachen.de>
2018-08-06 18:43:55 +0200

    Added ctype to assumption of tsynbCases

--- timesyn/CaseStudies/abp/Components.thy
+++ timesyn/CaseStudies/abp/Components.thy
@@ -78,7 +78,7 @@ lemma tsynb_cases [case_names max_len not_ubleast numb_channel msg null]:
   assumes max_len: "ubMaxLen (Fin (1::nat)) x" 
     and not_ubleast: "x \<noteq> ubLeast (ubDom\<cdot>x)"
     and numb_channel: "(ubDom\<cdot>x) = {c}"
-    and msg: "\<And>m. P (createBundle (Msg m) c)"
+    and msg: "\<And>m. (Msg m) \<in> ctype c \<Longrightarrow> P (createBundle (Msg m) c)"
     and null: "P (tsynbNull c)"
   shows "P x"
 proof - 
@@ -107,40 +107,20 @@ proof -
     by metis
   have s_ubundle_eq_x: "x = Abs_ubundle ([c \<mapsto> s])"
     by (metis (mono_tags, lifting) dom_eq_singleton_conv fun_upd_same numb_channel s_def singletonI ubWell_single_channel ubdom_insert ubgetchE ubgetchI ubgetch_insert ubrep_ubabs)
-  have len_one_cases: "usclLen\<cdot>s = Fin 1 \<Longrightarrow> (\<exists>m. s = (\<up>(Msg m))) \<or>  (s = (\<up>null))" apply (simp add: usclLen_stream_def)
-  proof - 
-    assume len_one: "#s = Fin (Suc (0::nat))"
-    show "(\<exists>m. s = (\<up>(Msg m))) \<or>  (s = (\<up>null))"
-    proof - 
-      show ?thesis using tsyn.exhaust len_one len_one_stream
-        by (metis One_nat_def)
-    qed
-  qed
+  have len_one_cases: "usclLen\<cdot>s = Fin 1 \<Longrightarrow> (\<exists>m. s = (\<up>(Msg m))) \<or>  (s = (\<up>null))" using tsyn.exhaust One_nat_def len_one_stream usclLen_stream_def sledgehammer
+    by metis  
   have s_cases: "(\<exists>m. s = \<up>(Msg m)) \<or> (s = \<up>null)"
     using s_def assms x_singleton x_not_empty len_one_cases by blast
   have s_eq: "(\<exists>m. s = (createBundle (Msg m) c) . c ) \<or> (s = (tsynbNull c) . c)" 
-    apply (case_tac "\<exists>m. s = \<up>(Msg m)")
-     defer 
-    using s_cases apply auto[1]
-  proof - 
-    assume a0: " \<exists>m::'a. s = \<up>(\<M> m)"
-    show " (\<exists>m::'a. s = createBundle (\<M> m) c  .  c) \<or> s = tsynbNull c  .  c"
-    proof - 
-      obtain m where m_def: "s = \<up>(\<M> m)" 
-        using a0 by blast
-      have m_shd: "(\<M> m) = shd (x . c)"
-        by (simp add: m_def s_def)
-      have m_in_ctype: "(\<M> m) \<in> ctype c" 
-        using numb_channel usclOkay_stream_def UnI1 contra_subsetD s_ubundle_eq_x sdom2un singletonI surj_scons ubdom_channel_usokay ubgetch_insert x_not_empty
-        by (metis (no_types, lifting) m_shd)
-      have s4: "s = (createBundle (\<M> m) c) . c " 
-        using createbundle_stream_eq m_def m_in_ctype by force
-      show ?thesis using s4 
-        by blast
-    qed 
+  proof(case_tac "\<exists>m. s = \<up>(Msg m)")
+    show "\<exists>m::'a. s = \<up>(\<M> m) \<Longrightarrow> (\<exists>m::'a. s = createBundle (\<M> m) c  .  c) \<or> s = tsynbNull c  .  c"
+      by (metis contra_subsetD createbundle_stream_eq insertI1 insert_is_Un lscons_conv numb_channel s_def sdom2un sup'_def ubdom_channel_usokay ubgetch_insert usclOkay_stream_def)
+    show "\<nexists>m::'a. s = \<up>(\<M> m) \<Longrightarrow> (\<exists>m::'a. s = createBundle (\<M> m) c  .  c) \<or> s = tsynbNull c  .  c" 
+      using s_cases by simp
   qed
   have x_eq: "(\<exists>m. x = (createBundle (Msg m) c)) \<or> (x = (tsynbNull c))" using s_cases s_def assms s_eq by (metis singletonD ubgetchI x_dom_eq_createbundle x_dom_eq_tsynbnull)
-  show ?thesis using x_eq msg null by blast
+  show ?thesis using x_eq msg null
+    by (metis createBundle.rep_eq fun_upd_same option.sel ubgetch_insert x_not_empty)
 qed
 
 
