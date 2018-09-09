be299ed0ac947bdeae3b4cb8de38051ee8997d0f
ShiChenNiu <shi.chen.niu@rwth-aachen.de>
2018-09-09 15:23:18 +0200

    last subgoal still unsolved

--- timesyn/issue241.thy
+++ timesyn/issue241.thy
@@ -4,13 +4,17 @@ imports tsynStream
 
 begin
 
+lemma tsynabs_tsyndom: "tsynDom\<cdot>s = sdom\<cdot>(tsynAbs\<cdot>s)"
+  apply (induction s rule: tsyn_ind, simp_all)
+  apply (simp add: tsynabs_sconc_msg tsyndom_sconc_msg)
+  by (simp add: tsyndom_sconc_null tsynabs_sconc_null)
+
 lemma tsyndom_sdom: "tsynDom\<cdot>s = inverseMsg ` ((sdom\<cdot>s) - {null})"
   apply (simp add: tsyndom_insert image_def set_eq_iff)
   by (metis DiffE DiffI empty_iff insert_iff inverseMsg.simps(2) tsyn.exhaust tsyn.simps(3))
 
 lemma tsynabs_sdom: "sdom\<cdot>(tsynAbs\<cdot>s) = tsynDom\<cdot>s"
   apply (induction rule: tsyn_ind, simp_all)
-  apply (simp add: tsyndom_strict)
   apply (simp add: tsynabs_sconc_msg tsyndom_sconc_msg)
   by (simp add: tsynabs_sconc_null tsyndom_sconc_null)
 
@@ -32,13 +36,13 @@ lemma tsynprojsnd_sdom: "sdom\<cdot>(tsynProjSnd\<cdot>s) = tsynSnd ` sdom\<cdot
 lemma tsynremdups_h_sdom: "sdom\<cdot>(sscanlA tsynRemDups_h (\<M> m)\<cdot>s) \<subseteq> sdom\<cdot>s \<union> {null}"
   apply (induction s arbitrary: m rule: tsyn_ind, simp_all)
   apply (rule admI)
-  apply (simp add: contlub_cfun_arg contlub_cfun_fun lub_eq_Union)
+  apply (simp add: contlub_cfun_arg lub_eq_Union)
   by blast+
 
 lemma tsynremdups_sdom: "sdom\<cdot>(tsynRemDups\<cdot>s) \<subseteq> sdom\<cdot>s \<union> {null}"
   apply (induction rule: tsyn_ind, simp_all)
   apply (rule admI)
-  apply (simp add: contlub_cfun_arg contlub_cfun_fun lub_eq_Union, blast)
+  apply (simp add: contlub_cfun_arg lub_eq_Union, blast)
   apply (metis (no_types, hide_lams) Un_commute Un_insert_left insert_mono sdom2un 
          sup_bot.left_neutral tsynremdups_sconc_msg tsynremdups_h_sdom)
   by (simp add: tsynremdups_sconc_null)
@@ -51,23 +55,23 @@ lemma tsynfilter_sdom: "sdom\<cdot>(tsynFilter A\<cdot>s) \<subseteq> tsynFilter
 lemma tsyndropwhile_sdom: "sdom\<cdot>(tsynDropWhile f\<cdot>s) \<subseteq> sdom\<cdot>s \<union> {null}"
   apply (induction s arbitrary: f rule: tsyn_ind, simp_all)
   apply (rule admI)
-  apply (simp add: contlub_cfun_arg contlub_cfun_fun lub_eq_Union, blast)
+  apply (simp add: contlub_cfun_arg lub_eq_Union, blast)
   apply (metis (no_types, hide_lams) insert_commute insert_is_Un insert_subset sdom2un 
          subset_insertI2 subset_refl tsyndropwhile_sconc_msg_f tsyndropwhile_sconc_msg_t)
   by (simp add: tsyndropwhile_sconc_null)
 
-lemma tsynzip_sdom: "sdom\<cdot>(tsynZip\<cdot>as\<cdot>bs) \<subseteq> Msg ` (tsynDom\<cdot>as \<times> sdom\<cdot>bs) \<union> {null}"
+lemma tsynzip_sdom_msg2: "sdom\<cdot>(tsynZip\<cdot>as\<cdot>bs) \<subseteq> sdom\<cdot>(tsynZip\<cdot>(\<up>(\<M> a) \<bullet> as)\<cdot>(\<up>b \<bullet> bs))"
+  by (simp add: subset_insertI tsynzip_sconc_msg)
+
+lemma tsynzip_sdom_null: "sdom\<cdot>(tsynZip\<cdot>as\<cdot>bs) \<subseteq> sdom\<cdot>(tsynZip\<cdot>(\<up>- \<bullet> as)\<cdot>bs) \<union> {null} "
+  by (metis (no_types, hide_lams) Un_subset_iff sdom2un subset_refl tsynZip.simps(2) tsynzip_sconc_null)
+
+lemma tsynzip_sdom: "sdom\<cdot>(tsynZip\<cdot>as\<cdot>bs) \<subseteq> (Msg ` (tsynDom\<cdot>as \<times> sdom\<cdot>bs)) \<union> {null}"  
   apply (induction as arbitrary: bs rule: tsyn_ind, simp_all)
   apply (rule admI)
-    apply (simp add: contlub_cfun_arg contlub_cfun_fun lub_eq_Union, blast)
-   defer
-  using tsynzip_sconc_null
-   apply (smt insertI1 insert_is_Un insert_subset sdom2un tsynZip.simps(2) tsyndom_sconc_null)
-  using tsynzip_sconc_msg tsynzip_singleton_msg_first tsynzip_singleton_msg_second
-  (* apply (metis (no_types, hide_lams) Un_commute Un_insert_left insert_mono sdom2un
-         sup_bot.left_neutral tsynzip_sconc_msg tsynzip_sconc_null)*)
+  apply (simp add: contlub_cfun_arg contlub_cfun_fun lub_eq_Union, blast)
+  defer
+  apply (metis (no_types) insert_absorb2 insert_is_Un insert_mono sdom2un tsynZip.simps(2) tsynzip_sconc_null tsyndom_sconc_null)
   oops
-
-(* Skip tsynScanl, tsynScanlExt for now. *)
 
 end
