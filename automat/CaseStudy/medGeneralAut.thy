bd68347f039c67e6f7e9f5a99194e53fce83a8f5
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-09-19 08:03:05 +0200

    #202 medium with countdown limit

--- automat/CaseStudy/medGeneralAut.thy
+++ automat/CaseStudy/medGeneralAut.thy
@@ -7,38 +7,42 @@ begin
 
 (* The first parameter describes what happens in the drop-case *)
 (* Normally f is constant and returns "-" ... But it can also alter the message *)
-fun medGeneralTransitionH :: "('a \<Rightarrow> ('a tsyn set)) \<Rightarrow> medState \<Rightarrow> 'a tsyn \<Rightarrow> (medState \<times> 'a tsyn) set" where
-"medGeneralTransitionH F  state     -    = { (state,    -   )            }" |   (* Time *)
-"medGeneralTransitionH F (Suc n) (Msg m) = { (  n  ,   out  ) | out . out\<in>(F m)   }" |  (* Drop *)
-"medGeneralTransitionH F    0    (Msg m) = { (  n  , (Msg m)) | n . True }"     (* Pass *)
+fun medGeneralTransitionH :: "lnat \<Rightarrow> ('a \<Rightarrow> ('a tsyn set)) \<Rightarrow> medState \<Rightarrow> 'a tsyn \<Rightarrow> (medState \<times> 'a tsyn) set" where
+"medGeneralTransitionH _ F  state     -    = { (state,    -   )            }" |   (* Time *)
+"medGeneralTransitionH _ F (Suc n) (Msg m) = { (  n  ,   out  ) | out . out\<in>(F m)   }" |  (* Drop *)
+"medGeneralTransitionH MaxCounter F    0    (Msg m) = { (  n  , (Msg m)) | n. Fin n \<le> MaxCounter}"     (* Pass *)
 
 fun tsynDelay :: "nat \<Rightarrow> 'm tsyn SB \<Rightarrow> 'm tsyn SB" where
 "tsynDelay n tsb = undefined" (* ToDo: Should be -- ubConcEq (sbNTimes n (tsynNull (ubDom tsb))) tsb *)
 
 (* ToDo: use delayLimit to nondeterministically delay everything *)
-fun medGeneralTransition :: "lnat \<Rightarrow> ('a \<Rightarrow> ('a tsyn set)) \<Rightarrow> (medState \<times> 'a medMessage tsyn sbElem) \<Rightarrow> ((medState \<times> 'a medMessage tsyn SB) set rev)" where
-"medGeneralTransition delayLimit F (s,f) = (if sbeDom f = medInDom then 
-    Rev {(s, tsynDelay n (medOut out)) | n s out. Fin n \<le>delayLimit \<and> (s, out)\<in>(medGeneralTransitionH F s (medMessageTransform ((Rep_sbElem f)\<rightharpoonup>(\<C> ''in''))))} 
+fun medGeneralTransition :: "lnat \<Rightarrow> lnat \<Rightarrow> ('a \<Rightarrow> ('a tsyn set)) \<Rightarrow> (medState \<times> 'a medMessage tsyn sbElem) \<Rightarrow> ((medState \<times> 'a medMessage tsyn SB) set rev)" where
+"medGeneralTransition delayLimit maxCounter F (s,f) = (if sbeDom f = medInDom then 
+    Rev {(s, tsynDelay n (medOut out)) | n s out. Fin n \<le>delayLimit \<and> (s, out)\<in>(medGeneralTransitionH maxCounter F s (medMessageTransform ((Rep_sbElem f)\<rightharpoonup>(\<C> ''in''))))} 
   else undefined)"
 
-lift_definition medGeneralAut :: "lnat \<Rightarrow> ('a \<Rightarrow> ('a tsyn set)) \<Rightarrow> (medState, 'a medMessage tsyn) ndAutomaton" is 
-  "\<lambda>delayLimit F. (medGeneralTransition delayLimit F, Rev {(n, medOut - )| n. True}, Discr medInDom, Discr medOutDom)"
+lift_definition medGeneralAut :: "lnat\<Rightarrow>lnat \<Rightarrow> ('a \<Rightarrow> ('a tsyn set)) \<Rightarrow> (medState, 'a medMessage tsyn) ndAutomaton" is 
+  "\<lambda>delayLimit maxCounter F. (medGeneralTransition delayLimit maxCounter F, Rev {(n, medOut - )| n. Fin n \<le> maxCounter}, Discr medInDom, Discr medOutDom)"
   by (simp add: medInDom_def)
 
-definition medGeneral :: "lnat \<Rightarrow> ('a \<Rightarrow> ('a tsyn set)) \<Rightarrow> medState \<Rightarrow> 'a medMessage tsyn SPS" where
-"medGeneral delayLimit F n = nda_h (medGeneralAut delayLimit F) n"
+definition medGeneral :: "lnat \<Rightarrow> lnat \<Rightarrow> ('a \<Rightarrow> ('a tsyn set)) \<Rightarrow> medState \<Rightarrow> 'a medMessage tsyn SPS" where
+"medGeneral delayLimit maxCounter F n = nda_h (medGeneralAut delayLimit maxCounter F) n"
 
 
 
 definition medFair :: "medState \<Rightarrow> 'a medMessage tsyn SPS" where
-"medFair = medGeneral 0 (\<lambda>s. {-})"  (* No delay, normal drop *)
+"medFair = medGeneral 0 \<infinity> (\<lambda>s. {-})"  (* No delay, normal counter, normal drop *)
 
 definition medFairDelay :: "medState \<Rightarrow> 'a medMessage tsyn SPS" where
-"medFairDelay = medGeneral \<infinity> (\<lambda>s. {-})"  (* arbitrary but finite delay, normal drop *)
+"medFairDelay = medGeneral \<infinity> \<infinity> (\<lambda>s. {-})"  (* arbitrary but finite delay, normal counter, normal drop *)
 
 definition medFairDelayTupel :: "medState \<Rightarrow> ('a\<times>'b) medMessage tsyn SPS" where
-"medFairDelayTupel = medGeneral \<infinity> (\<lambda>(a,b). {Msg (undefined, b), -, Msg (a, undefined)})"  
-  (* arbitrary but finite delay, drop can alter elements and only delete part of the information *)
+"medFairDelayTupel = medGeneral \<infinity> \<infinity> (\<lambda>(a,b). {Msg (undefined, b), -, Msg (a, undefined)})"  
+  (* arbitrary but finite delay, normal counter, drop can alter elements and only delete part of the information *)
+
+definition medGurantee :: "medState \<Rightarrow> 'a medMessage tsyn SPS" where
+"medGurantee = medGeneral 0 (Fin 5) (\<lambda>s. {-})" 
+  (* No delay, Passes at least every 5th message, normal drop *)
 
 
 
