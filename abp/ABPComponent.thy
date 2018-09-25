9e32f36e3fdcd497bd040f6270083c359ed91fb0
Mathias Pfeiffer <mathias.pfeiffer@rwth-aachen.de>
2018-09-25 11:56:33 +0200

    #198 manual datatype definition and instantiation

---
+++ abp/ABPComponent.thy
@@ -0,0 +1,68 @@
+(*
+ * DO NOT MODIFY!
+ * This file was generated from ABP.maa and will be overridden when changed. To change
+ * permanently, consider changing the model itself.
+ *
+ *  1.0.0
+ *)
+theory ABPComponent
+  imports ReceiverAutomaton SenderAutomaton MediumAutomaton
+
+begin
+
+(* Dummy *)
+definition uspecComp :: "('m,'m) ufun uspec \<Rightarrow> ('m,'m) ufun uspec \<Rightarrow> ('m,'m) ufun uspec" (infixl "\<Otimes>" 50) where
+"uspecComp S1 S2 \<equiv> undefined"
+
+(* Dummy *)
+definition spf2Sps :: "('m,'m) SPF \<Rightarrow> 'm SPS" where
+"spf2Sps f = undefined"
+
+
+section \<open>Datatype definition\<close>
+
+datatype ('e::countable) abpMessage = AbpPair_E_Bool "('e\<times>bool)" | AbpBool "bool" | AbpE "'e"
+
+instance abpMessage :: (countable) countable
+  apply(intro_classes)
+  by(countable_datatype)
+
+instantiation abpMessage :: (countable) message
+begin
+  fun ctype_abpMessage :: "channel \<Rightarrow> ('e::countable) abpMessage set" where
+  "ctype_abpMessage c = (
+    if c = \<C> ''sender_i'' then range AbpE else
+    if c = \<C> ''sender_ds__mediumSr_ar'' then range AbpPair_E_Bool else
+    if c = \<C> ''mediumSr_as__receiver_dr'' then range AbpPair_E_Bool else
+    if c = \<C> ''receiver_ar__mediumRs_ar'' then range AbpBool else
+    if c = \<C> ''receiver_o'' then range AbpE else
+    if c = \<C> ''mediumRs_as__sender_as'' then range AbpBool else
+    undefined)"
+  instance
+    by(intro_classes)
+end
+
+
+(* TODO Channel renamen für die Lampe? *)
+section \<open>Instanzen der Sub-Komponenten\<close>
+
+definition sender :: "(('e::countable) abpMessage tsyn, ('e::countable) abpMessage tsyn) SPF" where
+"sender = undefined"
+
+definition mediumSr :: "('e::countable) abpMessage tsyn SPS" where
+"mediumSr = undefined"
+
+definition receiver :: "(('e::countable) abpMessage tsyn, ('e::countable) abpMessage tsyn) SPF" where
+"receiver = undefined"
+
+definition mediumRs :: "('e::countable) abpMessage tsyn SPS" where
+"mediumRs = undefined"
+
+
+section \<open>Die Komponente selbst\<close>
+
+definition abp :: "('e::countable) abpMessage tsyn SPS" where
+"abp = (spf2Sps(sender) \<Otimes>  mediumSr \<Otimes>  spf2Sps(receiver) \<Otimes>  mediumRs)"
+
+
+end
