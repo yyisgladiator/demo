3d3038da7915dc632a388d238dfbdc21ef9f1d4a
Annika Savelsberg <annika.savelsberg@rwth-aachen.de>
2018-10-10 19:07:30 +0200

    Rearranged lemmata. First attempt of proof of spf2sps. #240

--- automat/CaseStudy/MediumSPS.thy
+++ automat/CaseStudy/MediumSPS.thy
@@ -44,8 +44,29 @@ lemma medsps_uspecran[simp]: "uspecRan\<cdot>(MedSPS n) = medOutDom"
 subsection {* Medium State Lemmata *}
 (* ----------------------------------------------------------------------- *)
 
+lemma spf2sps: assumes "spfConcIn (sbe2SB sbe1)\<cdot>spf1 = spfConcOut (sbe2SB sbe2)\<cdot>spf2"
+(*  and "sbeDom sbe1 = uspecDom\<cdot>sps"
+  and "sbeDom sbe2 = uspecRan\<cdot>sps2" *)
+  and "uspecDom\<cdot>sps1 = uspecDom\<cdot>sps2"
+  and "uspecRan\<cdot>sps1 = uspecRan\<cdot>sps2"
+  shows "spsConcIn (sbe2SB sbe1) sps1 
+    = spsConcOut (sbe2SB sbe2) (uspecFlatten (uspecDom\<cdot>sps2)(uspecRan\<cdot>sps2)(Rev {sps2}))"
+  apply (subst spsconcin_insert)
+  apply (subst spsconcout_insert)
+  apply (rule uspec_eqI)
+  apply (subst uspecimage_useful_uspecrevset)
+  apply (simp add: ufclDom_ufun_def ufclRan_ufun_def)
+  apply (subst uspecimage_useful_uspecrevset)
+  apply (simp add: ufclDom_ufun_def ufclRan_ufun_def)
+  apply (simp add: uspecrevset_insert setrevImage_def)
+  apply (rule image_cong, simp_all)
+  defer 
+  defer
+  apply (simp add: ufclDom_ufun_def ufclRan_ufun_def assms)+
+sorry
+
 text{* If null comes in, it will be sent and Medium stays in its state. *}
-lemma "spsConcIn (medIn -)(MedSPS n) = spsConcOut (medOut -)(MedSPS n)"
+lemma  medsps_spsconc_null: "spsConcIn (medIn -)(MedSPS n) = spsConcOut (medOut -)(MedSPS n)"
   apply (subst spsconcin_insert)
   apply (subst spsconcout_insert)
   apply (rule uspec_eqI)
@@ -60,19 +81,14 @@ lemma "spsConcIn (medIn -)(MedSPS n) = spsConcOut (medOut -)(MedSPS n)"
 
 text{* If a message comes in and the counter is not zero, null will be sent and Medium stays in its 
   state. *}
-lemma "spsConcIn (medIn (Msg m)) (MedSPS (Suc n)) = spsConcOut (medOut -)(MedSPS n)"
+lemma medsps_spsconc_msg_nzero: "spsConcIn (medIn (Msg m)) (MedSPS (Suc n)) = spsConcOut (medOut -)(MedSPS n)"
+  apply (simp add: medIn_def medOut_def)
+  apply (subst spf2sps, simp_all)
   oops
-
-lemma spf2sps: assumes "spfConcIn (sbe2SB sbe1)\<cdot>spf1 = spfConcOut (sbe2SB sbe2)\<cdot>spf2"
-  and "sbeDom sbe1 = uspecDom\<cdot>sps"
-  and "sbeDom sbe2 = uspecRan\<cdot>sps2"
-  shows "spsConcIn (sbe2SB sbe1) sps 
-    = spsConcOut (sbe2SB sbe2) (uspecFlatten (uspecDom\<cdot>sps2)(uspecRan\<cdot>sps2)(Rev {sps2}))"
-sorry
 
 text{* If a message comes in and the counter is zero, the message will be sent and Medium changes 
   its state. *}
-lemma "spsConcIn (medIn (Msg m)) (MedSPS 0) 
+lemma medsps_spsconc_msg_zero:"spsConcIn (medIn (Msg m)) (MedSPS 0) 
   = spsConcOut (medOut (Msg m))(uspecFlatten medInDom medOutDom (Rev {MedSPS n | n. True}))"
   apply (simp add: medIn_def medOut_def)
   apply (subst spf2sps, simp_all)
@@ -88,10 +104,10 @@ lemma medsps_strict[simp]: "uspecIsStrict (MedSPS n)"
 sorry
 
 thm nda_h_final_back
-lemma medsps_medfair_eq: 
+lemma medsps_medfair_subeq:
   shows "nda_h medFairAut \<sqsubseteq> MedSPS"
   apply(subst nda_h_final_back, simp_all)
-  apply(subst spf2sps)  (* This should result in a proof over MedSPF *)
+  apply(subst spf2sps, simp_all)  (* This should result in a proof over MedSPF *)
 sorry
   
 end
