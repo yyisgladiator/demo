1df8cbbd0305c82d1ba3d9389d575539d6210695
Annika Savelsberg <annika.savelsberg@rwth-aachen.de>
2018-10-12 15:15:43 +0200

    Some work on sps2spf-ndaconcout-version #240. Some work on assumption for #249.

--- automat/CaseStudy/MediumSPS.thy
+++ automat/CaseStudy/MediumSPS.thy
@@ -65,6 +65,7 @@ lemma spf2sps: assumes "spfConcIn (sbe2SB sbe1)\<cdot>spf1 = spfConcOut (sbe2SB 
   apply (simp add: ufclDom_ufun_def ufclRan_ufun_def assms)+
 sorry
 
+(* step Lemmata *) 
 text{* If null comes in, it will be sent and Medium stays in its state. *}
 lemma  medsps_spsconc_null: "spsConcIn (medIn -)(MedSPS n) = spsConcOut (medOut -)(MedSPS n)"
   apply (subst spsconcin_insert)
@@ -93,21 +94,45 @@ lemma medsps_spsconc_msg_zero:"spsConcIn (medIn (Msg m)) (MedSPS 0)
   apply (simp add: medIn_def medOut_def)
   apply (subst spf2sps, simp_all)
   oops
+(**)
+
+lemma sps2spf_ndaconcoutflatten:
+  assumes "\<And>state. uspecDom\<cdot>(other state) = In"
+  and "\<And>state. uspecRan\<cdot>(other state) = Out"
+  shows "spsConcIn (sbe2SB sbe) (other state) = ndaConcOutFlatten In Out (currentTransitions) other"
+  apply (rule uspec_eqI)
+  apply (simp add: spsconcin_insert)
+  apply (subst uspecimage_useful_uspecrevset)
+  apply (simp add: ufclDom_ufun_def ufclRan_ufun_def)
+  apply (simp add: uspecrevset_insert)
+  defer
+  apply (subst spsconcin_dom)
+  apply (simp add: ndaConcOutFlatten_def assms)
+  apply (subst spsconcin_ran)
+  apply (simp add: ndaConcOutFlatten_def assms)
+sorry
 
 lemma ndatrans_nempty[simp]: "(ndaTransition\<cdot>medFairAut) (n, sbe) \<noteq> Rev {}"
+  apply (simp add: ndaTransition_def medFairAut.rep_eq)
 sorry
 
 lemma medsps_notuspecmax[simp]: "MedSPS n \<noteq> uspecMax medInDom medOutDom"
-sorry
+  by (metis (mono_tags, lifting) MedSPS.rep_eq all_not_in_conv empty_Collect_eq orafun_nempty 
+    prod.inject rev.inject uspecMax.rep_eq)
 
+thm nda_h_bottom_h
 lemma medsps_strict[simp]: "uspecIsStrict (MedSPS n)"
+  apply (simp add: uspecIsStrict_def)
+  apply (rule uspec_ballI)
+  apply (rule ufisstrictI)
+  apply (simp add: ubclDom_ubundle_def)
 sorry
 
 thm nda_h_final_back
 lemma medsps_medfair_subeq:
   shows "nda_h medFairAut \<sqsubseteq> MedSPS"
-  apply(subst nda_h_final_back, simp_all)
-  apply(subst spf2sps, simp_all)  (* This should result in a proof over MedSPF *)
+  apply (subst nda_h_final_back, simp_all)
+  apply (subst sps2spf_ndaconcoutflatten, simp_all) (* This should result in a proof over MedSPF *)
 sorry
   
 end
