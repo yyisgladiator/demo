d0818d512f818bf207cfff2891051a05ce54544d
Sebastian Stüber <sebastian.stuber@rwth-aachen.de>
2018-10-18 19:31:52 +0200

    #247 restructured and final proof

--- automat/daReachability.thy
+++ automat/daReachability.thy
@@ -3,7 +3,7 @@ theory daReachability
 imports  dAutomaton
   begin
   
-section \<open>reachability\<close>
+section \<open>Definitions\<close>
 
 (* Given an automaton, a set of sbElem, the initial state, the set of states to be considered
   and it returns the set of all reachable states *)
@@ -14,9 +14,29 @@ definition daReach_helper::"('s::type, 'm::message) dAutomaton \<Rightarrow> 'm 
 (* Given an automaton, a set of sbElem, the initial state,
   and it returns the set of all reachable states,
   calls daReach_helper*)
-definition daReach::"('s::type, 'm::message) dAutomaton \<Rightarrow> 'm sbElem set \<Rightarrow> 's set  \<Rightarrow> 's set" where
-"daReach da M S_init = fix\<cdot>(\<Lambda> S. daReach_helper da M S_init S)"
+definition daReach_h::"('s::type, 'm::message) dAutomaton \<Rightarrow> 'm sbElem set \<Rightarrow> 's set  \<Rightarrow> 's set" where
+"daReach_h da M S_init = fix\<cdot>(\<Lambda> S. daReach_helper da M S_init S)"
 
+(* Returns all reachable states from the initial state *)
+definition daReach::"('s::type, 'm::message) dAutomaton  \<Rightarrow> 's set" where
+"daReach da = daReach_h da {sbe | sbe. (sbeDom sbe = daDom da)} {daInitialState da}"
+
+
+
+(* Given an automaton, a set of sbElem, the initial state,
+  and it returns the set of all outputs that can be produced by reachable states *)
+definition daOutReach_h:: "('s::type, 'm::message)dAutomaton => 'm sbElem set \<Rightarrow> 's set=> 'm SB set" where 
+"daOutReach_h da M S_init = { daNextOutput da s m |s m. s\<in>(daReach_h da M S_init)  \<and> m\<in>M}"
+
+
+(* Returns all possible outputs from the initial state *)
+definition daOutReach:: "('s::type, 'm::message)dAutomaton => 'm SB set" where
+"daOutReach da  = daOutReach_h da {sbe | sbe. (sbeDom sbe = daDom da)} {daInitialState da}"
+
+
+
+
+section \<open>Lemma\<close>
 
 lemma dareach_helper_mono: "monofun (\<lambda>S. daReach_helper da M S_init S)"
   apply(rule monofunI)
@@ -33,14 +53,26 @@ lemma dareach_helper_cont: "cont (\<lambda>S. daReach_helper da M S_init S)"
   by(auto simp add: daReach_helper_def)
 
 
-lemma daReach_insert: "(daReach da M S_init) = (daReach da M S_init) \<union> { daNextState da s  input | input s. s\<in>((daReach da M S_init)\<union>S_init) \<and> input\<in>M \<and>
+lemma daReach_insert: "(daReach_h da M S_init) = (daReach_h da M S_init) \<union> { daNextState da s  input | input s. s\<in>((daReach_h da M S_init)\<union>S_init) \<and> input\<in>M \<and>
         (ubLen (ubRestrict (daRan da)\<cdot>(ubUp\<cdot>(daNextOutput da s input))) < \<infinity>)}"
-  apply(subst daReach_def)
-  by (metis (no_types) Abs_cfun_inverse2 daReach_def daReach_helper_def dareach_helper_cont fix_eq)
+  apply(subst daReach_h_def)
+  by (metis (no_types) Abs_cfun_inverse2 daReach_h_def daReach_helper_def dareach_helper_cont fix_eq)
 
-(* Given an automaton, a set of sbElem, the initial state,
-  and it returns the set of all outputs that can be produced by reachable states *)
-definition daOutReach:: "('s::type, 'm::message)dAutomaton => 'm sbElem set \<Rightarrow> 's set=> 'm SB set" where 
-"daOutReach da M S_init = { daNextOutput da s m |s m. s\<in>(daReach da M S_init)  \<and> m\<in>M}"
+
+
+
+(* The output of the SPF contains only messages that are also in "daOutReach" *)
+
+(* use this for induction *)
+lemma assumes "c\<in>daDom da" and "ubDom\<cdot>ub = daDom da"
+  shows "sdom\<cdot>(((da_h da state) \<rightleftharpoons> ub) . c) 
+    \<subseteq> \<Union>{sdom\<cdot>(outBundle . c) | outBundle. outBundle \<in> (daOutReach_h da {sbe | sbe. (sbeDom sbe = daDom da)} {state})}"
+  oops
+
+(* final proof *)
+lemma assumes "c\<in>daDom da" and "state \<in> daReach da" and "ubDom\<cdot>ub = daDom da"
+  shows "sdom\<cdot>(((da_h da state) \<rightleftharpoons> ub) . c) \<subseteq> \<Union>{sdom\<cdot>(outBundle . c) | outBundle. outBundle \<in> daOutReach da}"
+  oops
+
 
 end
