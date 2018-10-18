610d7d5af345dfee198cb340e1163bd2e320eae1
Sebastian Stüber <sebastian.stuber@rwth-aachen.de>
2018-10-18 19:56:44 +0200

    #248 simplified proofs with new step collection, prooved one lemma

--- automat/CaseStudy/PasswordAutomat/PasswordFun.thy
+++ automat/CaseStudy/PasswordAutomat/PasswordFun.thy
@@ -21,14 +21,18 @@ section\<open>UbConcEq lemmas\<close>
 (****************************************************)  
 
 lemma ubConcEq_associative: "ubConcEq (ubConcEq a\<cdot>b)\<cdot>ub = ubConcEq a\<cdot>(ubConcEq b\<cdot>ub)"
+  apply(rule ub_eq)
+  apply auto[1]
   sorry
 
-lemma ubConcEq_ubLeast: "ubConcEq (ubLeast cs)\<cdot>s = s"
-  apply (simp add:ubConcEq_def)
-  sorry
+lemma ubConcEq_ubLeast[simp]: "ubConcEq (ubLeast cs)\<cdot>s = s"
+  apply(rule ub_eq)
+  apply auto
+  apply (simp add: usclConc_leftbottom)
+  by (simp add: ubconc_getch usclConc_leftbottom)
 
-lemma ubConcEq_spf: "spf \<rightleftharpoons> ubConcEq (ubConcEq a\<cdot>b)\<cdot>ub = spf \<rightleftharpoons> ubConcEq a\<cdot>(ubConcEq b\<cdot>ub)" 
-  sorry
+lemma ubConcEq_spf: "spf \<rightleftharpoons> ubConcEq (ubConcEq a\<cdot>b)\<cdot>ub = spf \<rightleftharpoons> ubConcEq a\<cdot>(ubConcEq b\<cdot>ub)"
+  by (metis ubConcEq_associative) 
 
 
 (****************************************************)
@@ -112,17 +116,16 @@ lemma verif_rule_1:
 (* 2. f(a ) = null *)
 lemma  verif_rule_2:
 "spfConcIn (passwordIn_list_i [Msg a])\<cdot>(passwordStep (PasswordState Initial Buf)) = 
-      spfConcOut (passwordOut_list_o [-])\<cdot>((passwordStep( PasswordState Initial Buf)))"
+      spfConcOut (passwordOut_list_o [-])\<cdot>((passwordStep( PasswordState PasswortSaved a)))"
   apply (simp add: spfconcin_split spfconcout_split del: ubconceq_insert)
-  apply(simp add: passwordStep_0_0)
-  sorry
+  by(simp add: passwordStep_0_0)
 
 
 lemma (* SWS: so sieht das 2. lemma eigentlich aus. wenn man (spfConcIn) verwendet hat man immer den rekursiven aufruf drin *)
 "(passwordStep (PasswordState Initial Buf)) \<rightleftharpoons> (passwordIn_i (Msg a)) = 
       passwordOut_o -"
   apply (simp add: passwordStep_def)
-  sorry
+  oops
 
 (* 3. f(null x ) = null f(x) *)
 lemma  verif_rule_3:
@@ -137,29 +140,21 @@ lemma  verif_rule_4:
 "spfConcIn (passwordIn_list_i [(Msg a),(Msg a)])\<cdot>(passwordStep (PasswordState Initial Buf)) = 
       spfConcOut (passwordOut_list_o [-, Msg a])\<cdot>(passwordStep( PasswordState Initial ''''))" 
   apply (simp add: spfconcin_split spfconcout_split del: ubconceq_insert)
-  apply(simp add: passwordStep_0_0)
-  apply(simp add: spfconcin_out_switch)
-  by(simp add: passwordStep_0_0 passwordStep_2_0)
+  by(simp add: passwordSteps spfconcin_out_switch)
 
 (* 5. f(a b x ) = null f(b x) *)
 lemma  verif_rule_5:
 "a\<noteq>b \<Longrightarrow> spfConcIn (passwordIn_list_i [(Msg a), (Msg b)])\<cdot>(passwordStep (PasswordState Initial Buf)) = 
       spfConcOut (passwordOut_list_o [-])\<cdot>(spfConcIn (passwordIn_list_i [Msg b]) \<cdot>(passwordStep( PasswordState Initial Buf2)))"
   apply (simp add: spfconcin_split spfconcout_split del: ubconceq_insert)
-  apply(simp add: passwordStep_0_0)
-  apply(simp add: spfconcin_out_switch)
-  by(simp add: passwordStep_0_0 passwordStep_2_1)
+  by(simp add: passwordSteps spfconcin_out_switch)
 
 (* 6. f(a null a x ) = null null a f(x) *)
 lemma  verif_rule_6:
 "spfConcIn (passwordIn_list_i [(Msg a), null, (Msg a)])\<cdot>(passwordStep (PasswordState Initial Buf)) = 
       spfConcOut (passwordOut_list_o [-,-, Msg a])\<cdot>(passwordStep( PasswordState Initial ''''))"
   apply (simp add: spfconcin_split spfconcout_split del: ubconceq_insert)
-  apply(simp add: passwordStep_0_0)
-  apply(simp add: spfconcin_out_switch)
-  apply(simp add: passwordStep_3_0)
-  apply(simp add: spfconcin_out_switch)
-  by(simp add: passwordStep_0_0 passwordStep_4_1)
+  by(simp add: passwordSteps spfconcin_out_switch)
 
 
 (* 7. f(a null b x ) = null null f(b x) *)
@@ -167,11 +162,7 @@ lemma  verif_rule_7:
 "a\<noteq>b \<Longrightarrow> spfConcIn (passwordIn_list_i [(Msg a), null, (Msg b)])\<cdot>(passwordStep (PasswordState Initial Buf)) = 
       spfConcOut (passwordOut_list_o [-,-])\<cdot>(spfConcIn (passwordIn_list_i [Msg b]) \<cdot>(passwordStep( PasswordState Initial Buf2)))"
   apply (simp add: spfconcin_split spfconcout_split del: ubconceq_insert)
-  apply(simp add: passwordStep_0_0)
-  apply(simp add: spfconcin_out_switch)
-  apply(simp add: passwordStep_3_0)
-  apply(simp add: spfconcin_out_switch)
-  by(simp add: passwordStep_0_0 passwordStep_4_0)
+  by(simp add: passwordSteps spfconcin_out_switch)
 
 
 (* 8. f(a null null x ) = null null null f(x) *)
@@ -179,10 +170,7 @@ lemma  verif_rule_8:
 "spfConcIn (passwordIn_list_i [(Msg a), null, null])\<cdot>(passwordStep (PasswordState Initial Buf)) = 
       spfConcOut (passwordOut_list_o [-,-,-])\<cdot>((passwordStep( PasswordState Initial '''')))"
   apply (simp add: spfconcin_split spfconcout_split del: ubconceq_insert)
-  apply(simp add: passwordStep_0_0)
-  apply(simp add: spfconcin_out_switch)
-  apply(simp add: passwordStep_3_0)
-  apply(simp add: spfconcin_out_switch)
-  by(simp add: passwordStep_0_0 passwordStep_5_0)
+  by(simp add: passwordSteps spfconcin_out_switch)
+
 
 end
