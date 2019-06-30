9aa99e304867ae90fef7a4b6e06bf149838b08bd
Hendrik Kausch <hendrik.kausch@rwth-aachen.de>
2019-06-30 18:05:22 +0200

    definition of daComp and some stuff for weak and strong spfs

---
+++ src/automatons/dAutomaton_comp.thy
@@ -0,0 +1,43 @@
+(*<*)
+theory dAutomaton_comp
+  imports dAutomaton_causal (*SPF_comp*)
+begin
+(*>*)
+section \<open>Automaton Composition\<close>
+
+subsection \<open>Composition of strong automatons\<close>
+
+subsubsection \<open>Definition\<close>
+
+(*TODO?:'incomp = ('in1 \<union> 'in2), 'outcomp = ('out1 \<union> 'out2),'buf = ('out1 \<union> 'out2) - ('in1 \<union> 'in2)*)
+
+fun compTransition::"('s1::type \<Rightarrow> 'in1\<^sup>\<surd> \<Rightarrow> ('s1 \<times> 'out1\<^sup>\<surd>)) \<Rightarrow> ('s2::type \<Rightarrow> 'in2\<^sup>\<surd> \<Rightarrow> ('s2 \<times> 'out2\<^sup>\<surd>)) \<Rightarrow> (('s1\<times>'s2 \<times>'buf\<^sup>\<surd>) \<Rightarrow> 'incomp\<^sup>\<surd> \<Rightarrow> (('s1\<times>'s2 \<times>'buf\<^sup>\<surd>) \<times> 'outcomp\<^sup>\<surd>))"where
+"compTransition t1 t2 (s1,s2,buf) sbe = (let (s1o,sbe1o) = (t1 s1 (sbeUnion sbe buf));
+                                             (s2o,sbe2o) = (t2 s2 (sbeUnion sbe buf))  in
+                                        ((s1o,s2o, sbeUnion sbe1o sbe2o), sbeUnion sbe1o sbe2o))"
+text\<open>The function @{term compTransition} maps two Transitoinfunctions to there equivalent composed 
+     Transitionfunction.\<close>
+
+(*TODO:'incomp = ('in1 \<union> 'in2), 'outcomp = ('out1 \<union> 'out2), 'buf = ('out1 \<union> 'out2) - ('in1 \<union> 'in2)*)
+definition daComp::"('s1::type,'in1,'out1)dAutomaton_strong \<Rightarrow> ('s2::type,'in2,'out2)dAutomaton_strong \<Rightarrow> (('s1\<times>'s2\<times>'buf\<^sup>\<surd>),'incomp,'outcomp)dAutomaton_strong" where
+"daComp \<equiv> \<lambda>aut1 aut2. (| dawTransition = compTransition (dawTransition aut1) (dawTransition aut2), 
+                        dawInitState =((dawInitState aut1),(dawInitState aut2), sbeUnion (dawInitOut aut1)(dawInitOut aut2)),
+                        dawInitOut = sbeUnion(dawInitOut aut1) (dawInitOut aut2) |)"
+text\<open>@{term daComp} composes two strong automatons. The State of the composed automaton has an 
+     \<open>sbElem\<close> as a buffer. The buffer contains all elements on the internal channels of the
+     automatons.\<close>
+
+subsubsection\<open>Strong composition lemmas \<close>
+
+lemma final_eval_autcomp:"semantik_strong(daComp aut1 aut2) \<sqsubseteq> spfcomp_s\<cdot>(semantik_strong aut1)\<cdot>(semantik_strong aut2)"
+  oops
+
+(*TODO: something else than Rep_spfs_fun\<cdot>(semantik_strong..) *)
+lemma final_eval_autcomp_eq:assumes "sbLen sb = \<infinity>"
+                            shows"(Rep_spfs_fun\<cdot>(semantik_strong(daComp aut1 aut2)))\<cdot>sb 
+                                  = Rep_spfs_fun\<cdot>((spfcomp_s\<cdot>(semantik_strong aut1))\<cdot>(semantik_strong aut2))\<cdot>sb"
+  oops
+
+(*<*)
+end
+(*>*)
--- src/spf/SPF.thy
+++ src/spf/SPF.thy
@@ -1,15 +1,11 @@
 theory SPF
 
-imports bundle.SB
+imports bundle.SB SPFcomp
 
 begin
 
 section \<open> Causal SPFs\<close>
 
-(*
-definition strong_weak_ex::"('I\<^sup>\<Omega> \<rightarrow> 'O\<^sup>\<Omega>)" where
-"strong_weak_ex = (\<Lambda> sb. Abs_sb(\<lambda>c. sinftimes(\<up>eps)))" (*doesnt work, because of ctype*)
-*)
 subsection \<open>Weak SPF\<close>
 
 subsubsection \<open>Weak SPF definition \<close>
@@ -17,23 +13,72 @@ subsubsection \<open>Weak SPF definition \<close>
 definition weak_well::"('I\<^sup>\<Omega> \<rightarrow> 'O\<^sup>\<Omega>) \<Rightarrow> bool" where
 "weak_well spf = (\<forall>sb. sbLen sb \<le> sbLen (spf\<cdot>sb))"
 
-typedef ('I::chan,'O::chan)spfw = "{f::('I\<^sup>\<Omega> \<rightarrow> 'O\<^sup>\<Omega>) . weak_well f}"
+definition sometimesspfw::"('I\<^sup>\<Omega> \<rightarrow> 'O\<^sup>\<Omega>)"where
+"sometimesspfw = (\<Lambda> sb. Abs_sb(\<lambda>c. sinftimes (\<up>(SOME a. a \<in> ctype (Rep c)))))"
+
+lemma sblen_leadm:"adm (\<lambda>sb. k \<le> sbLen sb)"
+  oops
+
+lemma sblen_ladm:"adm (\<lambda>sb. k <\<^sup>l sbLen sb)"
+  oops
+
+cpodef ('I::chan,'O::chan)spfw = "{f::('I\<^sup>\<Omega> \<rightarrow> 'O\<^sup>\<Omega>) . weak_well f}"
   apply(simp add: weak_well_def)
+  apply(cases "chIsEmpty TYPE('O)")
+  apply(subgoal_tac "\<forall>sb::'O\<^sup>\<Omega>. sbLen sb = \<infinity>",auto)
+  defer(*lemma for sbLen*)
+  apply(rule_tac x="sometimesspfw" in exI)
+  defer (*lemma sbLen (sometimesspfw\<cdot>sb) = \<infinity> with assumption*)
+  apply(simp add: weak_well_def)
+  apply(rule adm_all)(*lemma sblen_leadm*)
+  apply(rule admI)
   sorry
+
+lemma [simp, cont2cont]:"cont Rep_spfw"
+  using cont_Rep_spfw cont_id by blast
+
+lift_definition Rep_spfw_fun::"('I::chan,'O::chan)spfw \<rightarrow> ('I\<^sup>\<Omega> \<rightarrow>'O\<^sup>\<Omega>)"is
+"\<lambda> spfs. Rep_spfw( spfs)"
+  by(intro cont2cont)
+
+subsubsection \<open>Weak SPF functions \<close>
+
+definition spfcomp_w::"('I::chan,'O::chan)spfw \<rightarrow>  ('I2::chan,'O2::chan)spfw \<rightarrow> ('I3::chan,'O3::chan)spfw"where
+"spfcomp_w \<equiv> (\<Lambda> spf1 spf2. Abs_spfw(genComp\<cdot>(Rep_spfw spf1)\<cdot>(Rep_spfw spf2)))"
 
 subsubsection \<open>Weak SPF lemmas\<close>
 
 subsection \<open>Strong SPF \<close>
 
 subsubsection \<open>Strong SPF definition\<close>
-
-
+(*If 'O is empty (then there is no weak function*)
 definition strong_well::"('I\<^sup>\<Omega> \<rightarrow> 'O\<^sup>\<Omega>) \<Rightarrow> bool" where
 "strong_well spf = (\<forall>sb. lnsuc\<cdot>(sbLen sb) \<le> sbLen (spf\<cdot>sb))"
 
-typedef ('I::chan,'O::chan)spfs = "{f::('I,'O)spfw . strong_well (Rep_spfw f)}"
+cpodef ('I::chan,'O::chan)spfs = "{f::('I,'O)spfw . strong_well (Rep_spfw f)}"
   apply(simp add: strong_well_def)
+  apply(cases "chIsEmpty TYPE('O)")
+  apply(subgoal_tac "\<forall>sb::'O\<^sup>\<Omega>. sbLen sb = \<infinity>",auto)
+  defer(*lemma for sbLen*)
+  apply(rule_tac x="Abs_spfw(sometimesspfw)" in exI)
+  defer (*lemma spfw_well sometimesspfw with assumption*)
+  apply(simp add: strong_well_def)
+  apply(rule adm_all)(*lemma sblen_ladm*)
+  apply(rule admI)
   sorry
+
+lemma [simp, cont2cont]:"cont Rep_spfs"
+  using cont_Rep_spfs cont_id by blast
+
+subsubsection \<open>Strong SPF functions\<close>
+
+lift_definition Rep_spfs_fun::"('I::chan,'O::chan)spfs \<rightarrow> ('I\<^sup>\<Omega> \<rightarrow>'O\<^sup>\<Omega>)"is
+"\<lambda> spfs. Rep_spfw_fun\<cdot>(Rep_spfs spfs)"
+  by(intro cont2cont)
+
+definition spfcomp_s::"('I::chan,'O::chan)spfs \<rightarrow>  ('I2::chan,'O2::chan)spfs \<rightarrow> ('I3::chan,'O3::chan)spfs"where
+"spfcomp_s \<equiv> (\<Lambda> spf1 spf2. Abs_spfs(spfcomp_w\<cdot>(Rep_spfs spf1)\<cdot>(Rep_spfs spf2)))"
+
 
 subsubsection \<open>Strong SPF lemmas\<close>
 
