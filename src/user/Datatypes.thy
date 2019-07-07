ff2fd3bd290a012e78a220d1bac624f68b53dd83
Hendrik Kausch <hendrik.kausch@rwth-aachen.de>
2019-07-07 12:42:29 +0200

    ...

--- src/user/Datatypes.thy
+++ src/user/Datatypes.thy
@@ -13,8 +13,7 @@ text \<open>The channel-datatype is fixed before the proof begins.
 
 datatype channel = DummyChannel nat
 
-
-
+hide_const DummyChannel
 
 section \<open>Message Definition\<close>
 
@@ -31,11 +30,8 @@ instance M :: countable
 
 text \<open>Then one describes the types of each channel. Only Messages included are allowed to be
   transmitted\<close>
+
 definition ctype :: "channel \<Rightarrow> M set" where 
-"ctype = (\<lambda>c. if c= DummyChannel (Abs_Nat undefined) then {} else undefined)"
-
-hide_const DummyChannel
-
-
+"ctype = (\<lambda>c. if c= undefined then {} else undefined)"
 
 end
