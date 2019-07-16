e7c4650027bd16ab7f8a1892941673aa810bac10
Sebastian Stüber <sebastian.stuber@rwth-aachen.de>
2019-07-16 16:50:34 +0200

    less magic abbreviations, spfcomp

--- src/spf/SPFcomp.thy
+++ src/spf/SPFcomp.thy
@@ -13,8 +13,11 @@ definition genComp :: "('I1\<^sup>\<Omega> \<rightarrow> 'O1\<^sup>\<Omega>) \<r
 "genComp \<equiv> \<Lambda> spf1 spf2 sbIn . fix\<cdot>(\<Lambda> sbOut. spf1\<cdot>(sbIn \<uplus>\<^sub>\<star> sbOut) \<uplus>\<^sub>\<star> spf2\<cdot>(sbIn \<uplus>\<^sub>\<star> sbOut))"
 
 subsection \<open>General composition abbreviation\<close>
+abbreviation genComp_abbr ::  "('I1\<^sup>\<Omega> \<rightarrow> 'O1\<^sup>\<Omega>) \<Rightarrow> ('I2\<^sup>\<Omega> \<rightarrow> 'O2\<^sup>\<Omega>) \<Rightarrow> 'E\<^sup>\<Omega> \<rightarrow> 'F\<^sup>\<Omega>" (infixr "\<otimes>\<^sub>\<star>" 70) where 
+"spf1 \<otimes>\<^sub>\<star> spf2 \<equiv> genComp\<cdot>spf1\<cdot>spf2"
+
  (* inifxr \<otimes> ... without magic*)
-abbreviation genComp_abbr :: "('I1\<^sup>\<Omega> \<rightarrow> 'O1\<^sup>\<Omega>) \<Rightarrow> ('I2\<^sup>\<Omega> \<rightarrow> 'O2\<^sup>\<Omega>) \<Rightarrow> ((('I1 \<union> 'I2) - ('O1 \<union> 'O2))\<^sup>\<Omega> \<rightarrow> ('O1 \<union> 'O2)\<^sup>\<Omega>)" (infixr "\<otimes>" 70) where 
+abbreviation genComp_magicabbr :: "('I1\<^sup>\<Omega> \<rightarrow> 'O1\<^sup>\<Omega>) \<Rightarrow> ('I2\<^sup>\<Omega> \<rightarrow> 'O2\<^sup>\<Omega>) \<Rightarrow> ((('I1 \<union> 'I2) - ('O1 \<union> 'O2))\<^sup>\<Omega> \<rightarrow> ('O1 \<union> 'O2)\<^sup>\<Omega>)" (infixr "\<otimes>" 70) where 
 "spf1 \<otimes> spf2 \<equiv> genComp\<cdot>spf1\<cdot>spf2"
 
 
@@ -26,59 +29,6 @@ lemma spfcomp_eql[simp]: "genComp\<cdot>f\<cdot>g = f"
   apply(rule cfun_eqI, simp)
   apply(rule fix_eqI)
   by simp+
-
-
-(* TODO; Move *)
-definition chDom::"'cs itself \<Rightarrow> channel set" where
-"chDom a = (range (Rep::'cs \<Rightarrow> channel)) - cEmpty"
-
-lemma chdom_minus: "chDom (TYPE('cs1 - 'cs2)) = chDom (TYPE ('cs1)) - chDom (TYPE('cs2))"
-  apply(simp add: chDom_def Rep_minus_def)
-  apply auto
-  apply (meson Diff_iff Rep_minus)
-  apply (metis DiffE Rep_minus repinrange)
-proof -
-  fix xa :: 'cs1
-  assume a1: "Rep xa \<notin> range (\<lambda>x. Rep (x::'cs2))"
-  then have f2: "\<not> range (Rep::'cs1 \<Rightarrow> channel) \<subseteq> range (Rep::'cs2 \<Rightarrow> channel)"
-    by (metis repinrange subsetD)
-  have "range (Rep_minus::'cs1 - 'cs2 \<Rightarrow> channel) = (if range (Rep::'cs1 \<Rightarrow> channel) \<subseteq> range (Rep::'cs2 \<Rightarrow> channel) then cEmpty else range (Rep::'cs1 \<Rightarrow> channel) - range (Rep::'cs2 \<Rightarrow> channel))"
-    using type_definition.Rep_range type_definition_minus by blast
-  then show "Rep xa \<in> range (\<lambda>m. Rep_minus (m::'cs1 - 'cs2))"
-    using f2 a1 by (metis (full_types) DiffI repinrange)
-qed
-
-
-lemma chdom_union: "chDom (TYPE('cs1 \<union> 'cs2)) = chDom (TYPE ('cs1)) \<union> chDom (TYPE('cs2))"
-  apply(simp add: chDom_def Rep_union_def)
-  apply auto
-  apply (meson DiffD1 Rep_union UnE)
-proof -
-  fix xa :: 'cs1
-  assume "Rep xa \<notin> cEmpty"
-then have f1: "\<not> chIsEmpty (TYPE('cs1)::'cs1 itself)"
-  by (metis cempty_rule)
-  have "type_definition (Rep_union::'cs1 \<union> 'cs2 \<Rightarrow> channel) Abs_union (if range (Rep::'cs1 \<Rightarrow> channel) \<subseteq> cEmpty \<and> range (Rep::'cs2 \<Rightarrow> channel) \<subseteq> cEmpty then cEmpty else range (Rep::'cs1 \<Rightarrow> channel) \<union> range (Rep::'cs2 \<Rightarrow> channel) - cEmpty) = type_definition (Rep_union::'cs1 \<union> 'cs2 \<Rightarrow> channel) Abs_union (if \<not> range (Rep::'cs1 \<Rightarrow> channel) \<subseteq> cEmpty \<or> \<not> range (Rep::'cs2 \<Rightarrow> channel) \<subseteq> cEmpty then range (Rep::'cs1 \<Rightarrow> channel) \<union> range (Rep::'cs2 \<Rightarrow> channel) - cEmpty else cEmpty)"
-by presburger
-  then have "type_definition (Rep_union::'cs1 \<union> 'cs2 \<Rightarrow> channel) Abs_union (if \<not> range (Rep::'cs1 \<Rightarrow> channel) \<subseteq> cEmpty \<or> \<not> range (Rep::'cs2 \<Rightarrow> channel) \<subseteq> cEmpty then range (Rep::'cs1 \<Rightarrow> channel) \<union> range (Rep::'cs2 \<Rightarrow> channel) - cEmpty else cEmpty)"
-    by (meson type_definition_union)
-  then show "Rep xa \<in> range (\<lambda>u. Rep_union (u::'cs1 \<union> 'cs2))"
-using f1 by (simp add: chIsEmpty_def type_definition.Rep_range)
-next
-  fix xa :: 'cs2
-  assume "Rep xa \<notin> cEmpty"
-then have f1: "\<not> chIsEmpty (TYPE('cs2)::'cs2 itself)"
-  by (metis cempty_rule)
-  have "type_definition (Rep_union::'cs1 \<union> 'cs2 \<Rightarrow> channel) Abs_union (if range (Rep::'cs1 \<Rightarrow> channel) \<subseteq> cEmpty \<and> range (Rep::'cs2 \<Rightarrow> channel) \<subseteq> cEmpty then cEmpty else range (Rep::'cs1 \<Rightarrow> channel) \<union> range (Rep::'cs2 \<Rightarrow> channel) - cEmpty) = type_definition (Rep_union::'cs1 \<union> 'cs2 \<Rightarrow> channel) Abs_union (if \<not> range (Rep::'cs1 \<Rightarrow> channel) \<subseteq> cEmpty \<or> \<not> range (Rep::'cs2 \<Rightarrow> channel) \<subseteq> cEmpty then range (Rep::'cs1 \<Rightarrow> channel) \<union> range (Rep::'cs2 \<Rightarrow> channel) - cEmpty else cEmpty)"
-by presburger
-  then have "type_definition (Rep_union::'cs1 \<union> 'cs2 \<Rightarrow> channel) Abs_union (if \<not> range (Rep::'cs1 \<Rightarrow> channel) \<subseteq> cEmpty \<or> \<not> range (Rep::'cs2 \<Rightarrow> channel) \<subseteq> cEmpty then range (Rep::'cs1 \<Rightarrow> channel) \<union> range (Rep::'cs2 \<Rightarrow> channel) - cEmpty else cEmpty)"
-    by (meson type_definition_union)
-  then show "Rep xa \<in> range (\<lambda>u. Rep_union (u::'cs1 \<union> 'cs2))"
-    using f1 by (simp add: chIsEmpty_def type_definition.Rep_range)
-qed
-
-lemma "chDom (TYPE('cs1 - 'cs2)) \<inter> chDom (TYPE ('cs2)) = {}"
-  by(auto simp add: chdom_minus )
 
 lemma sbgetch_empty[simp]: "Rep c\<in>cEmpty \<Longrightarrow> sb \<^enum>\<^sub>\<star> c = \<epsilon>"
   apply(auto simp add: sbGetCh.rep_eq)
@@ -92,9 +42,7 @@ lemma ubunion_commu:
   apply(rule sb_eqI)
   apply(rename_tac c)
   apply(case_tac "Rep c \<in> chDom (TYPE ('cs1))"; case_tac "Rep c \<in> chDom (TYPE ('cs2))"; case_tac "Rep c \<in> chDom (TYPE ('cs3))")
-  using assms apply auto
-  apply(auto simp add: chDom_def sbGetCh.rep_eq sbunion_rep_eq)[5]
-  by (simp add: chDom_def)
+  using assms  by(auto simp add: chDom_def sbGetCh.rep_eq sbunion_rep_eq)
 
 lemma ubunion_fst[simp]:
   fixes sb1 ::"'cs1\<^sup>\<Omega>"
@@ -104,9 +52,8 @@ lemma ubunion_fst[simp]:
   apply(rule sb_eqI)  (* Exakt gleicher beweis wie "ubunion_commu" ... *)
   apply(rename_tac c)
   apply(case_tac "Rep c \<in> chDom (TYPE ('cs1))"; case_tac "Rep c \<in> chDom (TYPE ('cs2))"; case_tac "Rep c \<in> chDom (TYPE ('cs3))")
-  using assms apply auto
-  apply(auto simp add: chDom_def sbGetCh.rep_eq sbunion_rep_eq)[5]
-  by (simp add: chDom_def)
+  apply(case_tac "Rep c \<in> chDom (TYPE ('cs1))"; case_tac "Rep c \<in> chDom (TYPE ('cs2))"; case_tac "Rep c \<in> chDom (TYPE ('cs3))")
+  using assms  by(auto simp add: chDom_def sbGetCh.rep_eq sbunion_rep_eq)
 
 
 (* Solange output disjunkt *)
@@ -143,47 +90,99 @@ lemma "genComp\<cdot>f\<cdot>g = spfConvert\<cdot>(f \<otimes> g)"
   apply(rule sb_eqI)
   oops 
 
-lemma ubunion_id[simp]: "out\<star> \<uplus> (out\<star>) = out"
-  apply(rule sb_eqI)
-  apply(auto simp add: Abs_sb_inverse sbunion_insert sbgetch_insert)
-  using chdom_union chDom_def 
-  sorry
+lemma chdom_in: fixes c::"'cs"
+  assumes "chDom TYPE('cs) \<noteq> {}"
+    shows "Rep c \<in> chDom TYPE('cs)"
+  by (metis Diff_eq_empty_iff Diff_triv assms chDom_def chan_botsingle rangeI)
 
-lemma  fixes f::"'fIn\<^sup>\<Omega> \<rightarrow> 'fOut\<^sup>\<Omega>"
+lemma ubunion_id[simp]: "out\<star>\<^sub>1 \<uplus> (out\<star>\<^sub>2) = out"
+proof(rule sb_eqI)
+  fix c::"'a \<union> 'b"
+  assume as:"Rep c \<in> chDom TYPE('a \<union> 'b)"
+  have "Rep c \<in> chDom (TYPE ('a)) \<Longrightarrow> out\<star> \<uplus> (out\<star>) \<^enum> c = out \<^enum> c"
+    by (metis DiffE chDom_def sbgetch_insert2 sbunion_getch sbunion_rep_eq sbunion_sbconvert_eq)
+  moreover have "Rep c \<in> chDom (TYPE ('b)) \<Longrightarrow> out\<star> \<uplus> (out\<star>) \<^enum> c = out \<^enum> c"
+    by (metis DiffE chDom_def sbconv_eq sbunion_getch sbunion_rep_eq sbunion_sbconvert_eq)
+  moreover have "(Rep c \<in> chDom (TYPE ('a))) \<or> (Rep c \<in> chDom (TYPE ('b)))"
+    using as chdom_in by fastforce
+  ultimately show  "out\<star> \<uplus> (out\<star>) \<^enum> c = out \<^enum> c" by fastforce
+qed
+
+lemma spfcomp_belowI: fixes f::"'fIn\<^sup>\<Omega> \<rightarrow> 'fOut\<^sup>\<Omega>"
         and g::"'gIn\<^sup>\<Omega> \<rightarrow> 'gOut\<^sup>\<Omega>"
       assumes "chDom (TYPE ('fOut)) \<inter> chDom (TYPE ('gOut)) = {}"
 
       fixes out::"('fOut \<union> 'gOut)\<^sup>\<Omega>"
-assumes "f\<cdot>(sb \<uplus>\<^sub>\<star> out) = out\<star>"
-    and "g\<cdot>(sb \<uplus>\<^sub>\<star> out) = out\<star>"
+assumes "f\<cdot>(sb \<uplus>\<^sub>\<star> out) = out\<star>\<^sub>1"
+    and "g\<cdot>(sb \<uplus>\<^sub>\<star> out) = out\<star>\<^sub>2"
       shows "((f\<otimes>g)\<cdot>sb) \<sqsubseteq> out"
   apply(subst genComp_def)
   apply(simp add: spfConvert_def)
   apply(rule fix_least)
   by (simp add: assms)
 
-lemma spfcomp2gencomp  [simp]: "spfConvert\<cdot>(f \<otimes> g) = genComp\<cdot>f\<cdot>g"
-  oops
+lemma spfcomp_belowI: fixes f::"'fIn\<^sup>\<Omega> \<rightarrow> 'fOut\<^sup>\<Omega>"
+        and g::"'gIn\<^sup>\<Omega> \<rightarrow> 'gOut\<^sup>\<Omega>"
+      assumes "chDom (TYPE ('fOut)) \<inter> chDom (TYPE ('gOut)) = {}"
+assumes "f\<cdot>(sb \<uplus>\<^sub>\<star> out) = out\<star>"
+    and "g\<cdot>(sb \<uplus>\<^sub>\<star> out) = out\<star>"
+  shows "((f \<otimes>\<^sub>\<star> g)\<cdot>sb) \<sqsubseteq> out"
+  apply(subst genComp_def)
+  apply(simp add: spfConvert_def)
+  apply(rule fix_least)
+  apply auto
+  apply(rule sb_eqI)
+  oops  (* gilt vermutlich nicht *)
 
-lemma spfcomp_surj_h: "(spfConvert\<cdot>(f)) \<otimes> (spfConvert\<cdot>(f)) = f"
+lemma spfcomp2gencomp  [simp]: 
+      fixes f::"'fIn\<^sup>\<Omega> \<rightarrow> 'fOut\<^sup>\<Omega>"
+        and g::"'gIn\<^sup>\<Omega> \<rightarrow> 'gOut\<^sup>\<Omega>"
+      assumes "chDom (TYPE ('fOut)) \<inter> chDom (TYPE ('a)) = {}"
+      assumes "chDom (TYPE ('gOut)) \<inter> chDom (TYPE ('a)) = {}"
+      shows "spfConvert\<cdot>(f \<otimes> g) = (genComp\<cdot>f\<cdot>g::('a\<^sup>\<Omega> \<rightarrow> 'b\<^sup>\<Omega>))"
+  apply(rule cfun_eqI)
+  apply(simp add: spfConvert_def genComp_def)
+  sorry
+
+
+lemma spfcomp_surj_h: 
+  fixes  f :: "(('a \<union> 'b) - 'c \<union> 'd)\<^sup>\<Omega> \<rightarrow> ('c \<union> 'd)\<^sup>\<Omega>"
+      assumes "chDom (TYPE ('c)) \<inter> chDom (TYPE ('d)) = {}"
+
+  shows "(spfConvert\<cdot>(f)) \<otimes> (spfConvert\<cdot>(f)) = f"
   apply(subst genComp_def)
   apply(simp add: spfConvert_def)
   apply(rule cfun_eqI, simp)
   apply(rule fix_eqI)
    apply auto
+   apply(rule sb_eqI)
+   apply auto
   oops  (* TODO: Wichtig *)
+(* Ist aber sehr komisch, gilt glaube ich nicht ... *)
 
 lemma spfcomp_surj: obtains f g where "f \<otimes> g = x"
+  oops
+
+
+
+lemma sercomp:fixes f::"'fIn\<^sup>\<Omega> \<rightarrow> 'fOut\<^sup>\<Omega>"
+        and g::"'gIn\<^sup>\<Omega> \<rightarrow> 'gOut\<^sup>\<Omega>"
+      assumes "chDom (TYPE ('fOut)) \<subseteq> chDom (TYPE ('gIn))"
+      shows "(f \<otimes> g)\<cdot>sb = f\<cdot>(sb\<star>) \<uplus> g\<cdot>(f\<cdot>(sb\<star>)\<star>)"
   oops
 
 definition spsComp::"('I1\<^sup>\<Omega> \<rightarrow> 'O1\<^sup>\<Omega>) set \<Rightarrow> ('I2\<^sup>\<Omega> \<rightarrow> 'O2\<^sup>\<Omega>) set \<Rightarrow> ((('I1 \<union> 'I2) - ('O1 \<union> 'O2))\<^sup>\<Omega> \<rightarrow> ('O1 \<union> 'O2)\<^sup>\<Omega>) set"  where
 "spsComp F G = {f \<otimes> g | f g. f\<in>F \<and> g\<in>G }"
 
-lemma fixes P::"('I1\<^sup>\<Omega> \<rightarrow> 'O1\<^sup>\<Omega>) \<Rightarrow> bool"
-        and H::"('I2\<^sup>\<Omega> \<rightarrow> 'O2\<^sup>\<Omega>) \<Rightarrow> bool"
+lemma fixes P::"'I1\<^sup>\<Omega> \<Rightarrow> 'O1\<^sup>\<Omega> \<Rightarrow> bool"
+        and H::"'I2\<^sup>\<Omega> \<Rightarrow> 'O2\<^sup>\<Omega> \<Rightarrow> bool"
       assumes "chDom (TYPE ('O1)) \<inter> chDom (TYPE ('O2)) = {}"
-      shows  "spsComp {x. P x} {x. H x} = {x. P (spfConvert\<cdot>x) \<and> H (spfConvert\<cdot>x)}"
-  apply (auto simp add: spsComp_def)
+      shows  "spsComp {p . \<forall>sb. P sb (p\<cdot>sb)} {h . \<forall>sb. H sb (h\<cdot>sb)} =   
+            {g. \<forall>sb. 
+                  let all = sb \<uplus> g\<cdot>sb in 
+                    P (all\<star>) (all\<star>) \<and> H (all\<star>) (all\<star>)
+            }"
+  apply (auto simp add: spsComp_def Let_def)
   oops
 (*  by (metis spfcomp2gencomp spfcomp_eql spfcomp_eqr spfcomp_surj) *)
 (* Gegenbeispiel ... soweit ich sehe: 
