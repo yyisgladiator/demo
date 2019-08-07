d114e1d3ef9be6fb5546b335eccf02186f631e8b
Hendrik Kausch <hendrik.kausch@rwth-aachen.de>
2019-08-07 02:44:13 +0200

    some broy stuff

--- src/demo/fairmerge/FMAutomat.thy
+++ src/demo/fairmerge/FMAutomat.thy
@@ -58,6 +58,61 @@ lift_definition FairMergelift::"(S_fm,inFM,outFM) ndAutomaton" is
   apply (metis (mono_tags)old.prod.exhaust)+
   by(simp add: fairmergeinit_def,auto)
 
+section \<open>Broy specification\<close>
+text\<open>Assumes that stream domains are disjoint, non-strict if we use timed streams\<close>
+definition FM_broy::"('a::countable stream \<rightarrow> 'a stream \<rightarrow> 'a stream) set" where
+"FM_broy = {f | f. \<forall>s1 s2. (#s1=\<infinity> \<and> #s2=\<infinity> \<and> (sValues\<cdot>s1 \<inter> sValues\<cdot>s2 = {})) 
+            \<longrightarrow> (s1=(sValues\<cdot>s1) \<ominus> (f\<cdot>s1\<cdot>s2) \<and> s2=(sValues\<cdot>s2) \<ominus> (f\<cdot>s1\<cdot>s2))}"
+
+text \<open>Case we want to merge nat and bool stream:\<close>
+
+datatype NB= \<N> nat | \<B> bool
+
+instance NB::countable
+  by(countable_datatype)
+
+definition FM_broynb::"(nat stream \<rightarrow> bool stream \<rightarrow> NB stream)set" where
+"FM_broynb = {f | f. \<exists>g. \<forall>s1 s2. f\<cdot>s1\<cdot>s2 = g\<cdot>(smap \<N>\<cdot>s1)\<cdot>(smap \<B>\<cdot>s2) \<and> g \<in> FM_broy}"
+
+lemma assumes "f\<in>FM_broynb" and "#s1 = \<infinity>" and "#s2 = \<infinity>" shows "(\<N> `(sValues\<cdot>s1)) \<ominus> (f\<cdot>s1\<cdot>s2) = smap \<N>\<cdot>s1"
+  apply(insert assms, simp add: FM_broynb_def FM_broy_def,auto)
+  apply(subgoal_tac "sValues\<cdot>(smap NB.\<N>\<cdot>s1)\<inter> sValues\<cdot>(smap NB.\<B>\<cdot>s2) = {}",auto)
+  apply (metis slen_smap smap_sValues)
+  by(auto simp add: smap_sValues)
+
+lemma assumes "f\<in>FM_broynb" and "#s1 = \<infinity>" and "#s2 = \<infinity>" shows "(\<B> `(sValues\<cdot>s2)) \<ominus> (f\<cdot>s1\<cdot>s2) = smap \<B>\<cdot>s2"
+  apply(insert assms, simp add: FM_broynb_def FM_broy_def,auto)
+  apply(subgoal_tac "sValues\<cdot>(smap NB.\<N>\<cdot>s1)\<inter> sValues\<cdot>(smap NB.\<B>\<cdot>s2) = {}",auto)
+  apply (metis slen_smap smap_sValues)
+  by(auto simp add: smap_sValues)
+
+
+fixrec
+smerge :: "'a::countable stream \<rightarrow> 'a stream \<rightarrow> 'a stream"
+where
+"smerge\<cdot>\<bottom>\<cdot>l2 = \<bottom>"|
+"smerge\<cdot>l1\<cdot>\<bottom> = \<bottom>"|
+"smerge\<cdot>((up\<cdot>l1)&&ls1)\<cdot>((up\<cdot>l2)&&ls2) = (up\<cdot>l1)&&((up\<cdot>l2)&&(smerge\<cdot>ls1\<cdot>ls2))"
+
+lemma "smerge\<cdot>(\<up>a \<bullet> as)\<cdot>(\<up>b \<bullet>bs) = \<up>a \<bullet> \<up>b \<bullet> (smerge\<cdot>as\<cdot>bs)"
+  unfolding smerge_def
+  sorry
+
+lemma "snth (2*n) (smerge\<cdot>(s1)\<cdot>(s2)) = snth n s1"
+  sorry
+
+lemma "snth (2*n +1) (smerge\<cdot>(s1)\<cdot>(s2)) = snth (n+1) s2"
+  sorry
+
+lemma smerge_well1:"#s1 = \<infinity> \<Longrightarrow> #s2 = \<infinity> \<Longrightarrow> sValues\<cdot>s1 \<inter> sValues\<cdot>s2 = {} \<Longrightarrow> s1 = sValues\<cdot>s1 \<ominus> smerge\<cdot>s1\<cdot>s2"
+  sorry
+
+lemma smerge_well2:"#s1 = \<infinity> \<Longrightarrow> #s2 = \<infinity> \<Longrightarrow> sValues\<cdot>s1 \<inter> sValues\<cdot>s2 = {} \<Longrightarrow> s2 = sValues\<cdot>s2 \<ominus> smerge\<cdot>s1\<cdot>s2"
+  sorry
+
+lemma "\<exists>f. f\<in>FM_broy"
+  apply(rule_tac x="smerge"in exI)
+  by(simp add: FM_broy_def smerge_well1 smerge_well2)
 
 section \<open>Rumpe specification\<close>
 
